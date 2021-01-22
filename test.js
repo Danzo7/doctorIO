a1c046388778b5ce9b80d5d1c8461c77f6812610
Danzo7 <aymenmiro@gmail.com>
2021-01-22 00:00:18 +0000

    remove test junk

--- test.js
+++
@@ -1,8 +0,0 @@
-var sass = require('sass');
-
-result = sass.renderSync({
-  file: 'src/components/QueueItem/style/index.scss',
-  includePaths: ['src/assets/styles'],
-  outFile: 'style.css',
-});
-console.log(result.css.toString());
