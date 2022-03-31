e339c7beceeca7ca1ccaac4b98ec2e624871cb24
Danzo7 <aymenmiro@gmail.com>
2022-03-31 18:57:07 +0200

    ⚡️ improvement

--- src/renderer/components/components/mini_stats/MiniStats.tsx
+++ src/renderer/components/components/mini_stats/MiniStats.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import './style/index.scss';
-import Clinic from 'toSvg/clinic.svg';
+import clinic from 'toSvg/clinic.svg';
+import home from 'toSvg/home.svg';
+import stats from 'toSvg/stats.svg';
+const arr = [home, clinic, stats];
 interface MiniStatsProps {
   value?: string;
   text?: string;
+  state?: number;
 }
 
-function MiniStats({ value, text }: MiniStatsProps) {
+function MiniStats({ value, text, state }: MiniStatsProps) {
+  const Comp = arr[state ?? 0];
   return (
     <div className="mini-stats">
       <span>{value}</span>
       <span>{text}</span>
-      <Clinic></Clinic>
+      <Comp></Comp>
     </div>
   );
 }
