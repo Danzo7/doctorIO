bb21c21634f66f6d512ed3350fc9adf0979314c6
Danzo7 <aymenmiro@gmail.com>
2022-04-03 03:50:47 +0100

    📱 make component responsive and add features

--- src/renderer/components/components/buttons/iconic_button/IconicButton.tsx
+++ src/renderer/components/components/buttons/iconic_button/IconicButton.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
+import { css } from '@emotion/css';
 import './style/index.scss';
 interface IconicButtonProps {
   Icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  backgroundColor: string;
+  backgroundColor?: string;
+  afterColor?: string;
+  size?: number;
+  width?: number;
 }
-function IconicButton({ Icon, backgroundColor }: IconicButtonProps) {
+function IconicButton({
+  Icon,
+  backgroundColor,
+  size,
+  width,
+  afterColor,
+}: IconicButtonProps) {
   return (
     <div
-      className={`iconic-button`}
-      style={{ backgroundColor: backgroundColor }}
+      className={`iconic-button ${css`
+        background-color: ${backgroundColor};
+        padding: ${size}%;
+        width: ${width}px;
+        height: ${width}px;
+        &:hover {
+          background-color: ${afterColor};
+        }
+      `}`}
     >
-      {<Icon width={17} height={17} />}
+      <Icon />
     </div>
   );
 }
