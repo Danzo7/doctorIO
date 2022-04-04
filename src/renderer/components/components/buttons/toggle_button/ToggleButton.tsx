882ce84450368b04cc25f8e3f20877786106f2b8
Danzo7 <aymenmiro@gmail.com>
2022-04-04 23:28:08 +0100

    ⚡️ Dynamic size

--- src/renderer/components/components/buttons/toggle_button/ToggleButton.tsx
+++ src/renderer/components/components/buttons/toggle_button/ToggleButton.tsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import './style/index.scss';
 import XMark from 'toSvg/x_mark.svg';
 import GoodMark from 'toSvg/good_mark.svg';
+import { css } from '@emotion/css';
 interface ToggleButtonProps {
   disabled: boolean;
   isChecked?: boolean;
+  size?: number;
 }
-function ToggleButton({ disabled, isChecked }: ToggleButtonProps) {
+function ToggleButton({ disabled, isChecked, size }: ToggleButtonProps) {
   const [checked, setChecked] = useState(isChecked ?? false);
 
   const Svg = checked ? GoodMark : XMark;
@@ -17,10 +19,19 @@ function ToggleButton({ disabled, isChecked }: ToggleButtonProps) {
     <div
       className={`toggle-button ${checked ? 'on' : 'off'} ${
         disabled ? 'disabled' : ''
-      }`}
+      }
+      ${css`
+        width: ${size ?? 40}px;
+      `}
+       `}
       onClick={switchToggle}
     >
-      <div className={`circle `}>
+      <div
+        className={`circle  ${css`
+          width: ${(size ?? 40) / 2}px;
+          height: ${(size ?? 40) / 2}px;
+        `}`}
+      >
         <Svg />
       </div>
     </div>
