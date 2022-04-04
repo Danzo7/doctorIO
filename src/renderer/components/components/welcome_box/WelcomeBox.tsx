b76f1ea8a60630a3a107e62a72f6fb6b2da2ddfb
Danzo7 <aymenmiro@gmail.com>
2022-04-04 18:07:50 +0100

    ✨ welcome component

--- src/renderer/components/components/welcome_box/WelcomeBox.tsx
+++ src/renderer/components/components/welcome_box/WelcomeBox.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import './style/index.scss';
+import Svg from 'toSvg/doctor_face.svg';
+import Arrow from 'toSvg/arrow.svg';
+
 interface WelcomeBoxProps {}
 function WelcomeBox({}: WelcomeBoxProps) {
-  return <div className="welcome-box"></div>;
+  return (
+    <div className="welcome-box">
+      <div className="content">
+        <Svg />
+        <span>Welcome to marely</span>
+      </div>
+      <div className="hide-btn">
+        <Arrow />
+      </div>
+      {/* <Pattern /> */}
+    </div>
+  );
 }
 
 export default WelcomeBox;
