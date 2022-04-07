e2a708128413d67b3ac6e81ec5ce19b80f347f0b
Danzo7 <aymenmiro@gmail.com>
2022-04-07 17:03:54 +0100

    🐛 fix return empty div

--- src/renderer/components/components/welcome_box/WelcomeBox.tsx
+++ src/renderer/components/components/welcome_box/WelcomeBox.tsx
@@ -2,32 +2,33 @@ import React, { useState } from 'react';
 import './style/index.scss';
 import Svg from 'toSvg/doctor_face.svg';
 import Arrow from 'toSvg/arrow.svg';
-
 interface WelcomeBoxProps {}
 function WelcomeBox({}: WelcomeBoxProps) {
   const [close, setClose] = useState({ isHidding: false, isHidden: false });
-  return !close.isHidden ? (
-    <div
-      className={`welcome-box ${close.isHidding ? 'animate' : ''}`}
-      onAnimationEnd={() => {
-        setClose({ isHidding: true, isHidden: true });
-      }}
-    >
-      <div className="content">
-        <Svg />
-        <span>Welcome to marely</span>
-      </div>
-      <div
-        className="hide-btn"
-        onClick={() => {
-          setClose({ isHidding: true, isHidden: false });
-        }}
-      >
-        <Arrow />
-      </div>
-    </div>
-  ) : (
-    <div></div>
+  return (
+    <>
+      {!close.isHidden && (
+        <div
+          className={`welcome-box ${close.isHidding ? 'animate' : ''}`}
+          onAnimationEnd={() => {
+            setClose({ isHidding: true, isHidden: true });
+          }}
+        >
+          <div className="content">
+            <Svg />
+            <span>Welcome to marely</span>
+          </div>
+          <div
+            className="hide-btn"
+            onClick={() => {
+              setClose({ isHidding: true, isHidden: false });
+            }}
+          >
+            <Arrow />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
