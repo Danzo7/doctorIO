d0397772c9c30eb19b2e019394600d52d007a3b4
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-04-09 00:04:51 +0100

    💄 MembersPanel (need a lot of work )

--- src/renderer/components/components/members_panel/MembersPanel.stories.tsx
+++ src/renderer/components/components/members_panel/MembersPanel.stories.tsx
@@ -1,6 +1,7 @@
 import React, { ComponentProps } from 'react';
 import type { Story } from '@storybook/react';
 import MembersPanel from './MembersPanel';
+import test from 'toPng/test.png';
 //👇 This default export determines where your story goes in the story list
 export default {
   title: 'MembersPanel',
@@ -10,4 +11,28 @@ const Template: Story<ComponentProps<typeof MembersPanel>> = (args) => (
   <MembersPanel {...args} />
 );
 export const FirstStory = Template.bind({});
-FirstStory.args = {};
+FirstStory.args = {
+  membersList: [
+    {
+      fullName: 'Amanda clarck',
+      imgSrc: test,
+      memberID: '100',
+      status: 'Online',
+      roleArray: ['Gamer', 'Assistant', 'Assistant', 'Assistant'],
+    },
+    {
+      fullName: 'Amanda clarck',
+      imgSrc: test,
+      memberID: '100',
+      status: 'Online',
+      roleArray: ['Gamer', 'Assistant', 'Assistant', 'Assistant'],
+    },
+    {
+      fullName: 'Amanda clarck',
+      imgSrc: test,
+      memberID: '100',
+      status: 'Online',
+      roleArray: ['Gamer', 'Assistant', 'Assistant', 'Assistant'],
+    },
+  ],
+};
--- src/renderer/components/components/members_panel/MembersPanel.tsx
+++ src/renderer/components/components/members_panel/MembersPanel.tsx
@@ -1,8 +1,36 @@
+import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
+import MembersPreview from '@components/members_preview';
 import React from 'react';
 import './style/index.scss';
-interface MembersPanelProps {}
-function MembersPanel({}: MembersPanelProps) {
-  return <div className="members-panel"></div>;
+interface MembersPanelProps {
+  membersList: Array<any>;
+}
+function MembersPanel({ membersList = [] }: MembersPanelProps) {
+  return (
+    <div className="members-panel">
+      <div className="header">
+        <span>Members</span>
+        <DarkLightCornerButton title="Members ..." />
+      </div>
+      <div className="members-list">
+        {membersList.length != 0 ? (
+          membersList.map(
+            ({ fullName, imgSrc, memberID, status, roleArray }) => (
+              <MembersPreview
+                fullName={fullName}
+                imgSrc={imgSrc}
+                memberID={memberID}
+                status={status}
+                roleArray={roleArray}
+              />
+            ),
+          )
+        ) : (
+          <div>No Members</div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default MembersPanel;
