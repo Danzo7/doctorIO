3db904bcc5593bf8175deb6af6dad499a50fd357
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-04-12 02:18:11 +0100

    💄 LastDmMessage

--- src/renderer/components/components/last_dm_message/LastDmMessage.stories.tsx
+++ src/renderer/components/components/last_dm_message/LastDmMessage.stories.tsx
@@ -1,6 +1,7 @@
 import React, { ComponentProps } from 'react';
 import type { Story } from '@storybook/react';
 import LastDmMessage from './LastDmMessage';
+import client from '@assets/pictures/test.png';
 export default {
   title: 'LastDmMessage',
   component: LastDmMessage,
@@ -9,4 +10,8 @@ const Template: Story<ComponentProps<typeof LastDmMessage>> = (args) => (
   <LastDmMessage {...args} />
 );
 export const FirstStory = Template.bind({});
-FirstStory.args = {};
+FirstStory.args = {
+  imgSrc: client,
+  status: true,
+  lastMessage: 'hello there!',
+};
--- src/renderer/components/components/last_dm_message/LastDmMessage.tsx
+++ src/renderer/components/components/last_dm_message/LastDmMessage.tsx
@@ -1,6 +1,23 @@
+import UserProfileStatus from '@components/user_profile_status';
 import React from 'react';
 import './style/index.scss';
-interface LastDmMessageProps {}
-export default function LastDmMessage({}: LastDmMessageProps) {
-  return <div className="last-dm-message"></div>;
+interface LastDmMessageProps {
+  imgSrc: string;
+  status: boolean;
+  lastMessage: string;
+}
+export default function LastDmMessage({
+  imgSrc,
+  status,
+  lastMessage,
+}: LastDmMessageProps) {
+  return (
+    <div className="last-dm-message">
+      <UserProfileStatus imgSrc={imgSrc} status={status} width={35} />
+      <div className="info-container">
+        <span>Mahmoud Benaissa</span>
+        <span>{`You: ${lastMessage ? lastMessage : ''}`}</span>
+      </div>
+    </div>
+  );
 }
