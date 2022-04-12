947df310bfd18422588aea088045853f3dff9ef8
Danzo7 <aymenmiro@gmail.com>
2022-04-12 16:37:07 +0100

    feat: 🏷️ add strict values to interface(read) Note: Be careful using Types/interfaces in TS, JS know nothing about interface/types they are only valable before compilation and syntax checking, on runtime you are on your one. -[Defferent between interface & classes] (https://stackoverflow.com/a/65948871/6552992).

--- src/renderer/components/components/booked_item/BookedItem.tsx
+++ src/renderer/components/components/booked_item/BookedItem.tsx
@@ -3,10 +3,11 @@ import './style/index.scss';
 import Panding from 'toSvg/pending.svg';
 import InQueue from 'toSvg/in_queue.svg';
 import Menu from 'toSvg/menu.svg';
+
 interface BookedItemProps {
   name: string;
   bookTime: string;
-  state: string;
+  state: 'panding' | 'in queue'; //This will force ts to accept only those values with their types.
 }
 function BookedItem({ name, bookTime, state }: BookedItemProps) {
   return (
