8f03dde2c49531f3aad66574609ab9d643bdb5da
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-04-16 03:32:33 +0100

    💄 MemberItem (still not done yet)

--- src/renderer/components/components/member_item/MemberItem.tsx
+++ src/renderer/components/components/member_item/MemberItem.tsx
@@ -18,30 +18,32 @@ export default function MemberItem({
   const addRole = () => {};
   return (
     <div className="container">
-      <div className="member-item">
-        <div className="member-Info">
-          <UserProfileStatus
-            imgSrc={memberImgSrc}
-            status={memberStatus}
-            width={30}
-          />
-          <div className="id-container">
-            <span>Aymen Daouadji</span>
-            <span>@mohamed#2000</span>
+      <div className="content">
+        <div className="member-item">
+          <div className="member-Info">
+            <UserProfileStatus
+              imgSrc={memberImgSrc}
+              status={memberStatus}
+              width={30}
+            />
+            <div className="id-container">
+              <span>Aymen Daouadji</span>
+              <span>@mohamed#2000</span>
+            </div>
+            <IsOwner />
           </div>
-          <IsOwner />
-        </div>
-        <div className="roles-container">
-          {roleArray.length > 0 &&
-            roleArray.map((rollName) => (
-              <SmallRolePreview roleName={rollName} />
-            ))}
-          <div onClick={addRole} className="add-btn-container">
-            <AddButton />
+          <div className="roles-container">
+            {roleArray.length > 0 &&
+              roleArray.map((rollName) => (
+                <SmallRolePreview roleName={rollName} />
+              ))}
+            <div onClick={addRole} className="add-btn-container">
+              <AddButton />
+            </div>
           </div>
-        </div>
-        <div className="date-container">
-          <span>28 Feb 2021</span>
+          <div className="date-container">
+            <span>28 Feb 2021</span>
+          </div>
         </div>
       </div>
       <div className="option-menu">
