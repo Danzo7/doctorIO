697741886bdeb4ec23b21e49dea5fc6650a9eba1
Danzo7 <aymenmiro@gmail.com>
2022-04-16 06:37:52 +0100

    ✨  add minimizable sidebar

--- src/renderer/components/containers/AppSidebar/AppSidebar.tsx
+++ src/renderer/components/containers/AppSidebar/AppSidebar.tsx
@@ -1,14 +1,23 @@
 import BookedList from '@components/booked_list';
 import QueueList from '@components/queue_list';
-import React from 'react';
+import React, { useState } from 'react';
+import Arrow from 'toSvg/arrow.svg?icon';
 import './index.scss';
 interface AppSidebarProps {}
 
 function AppSidebar({}: AppSidebarProps) {
+  const [isMini, setMini] = useState(false);
   return (
-    <div className="AppSidebar">
-      <QueueList></QueueList>
-      <BookedList></BookedList>
+    <div className={`AppSidebar${isMini ? ' minimize' : ''}`}>
+      <div className="sidebar">
+        <div className="content">
+          <QueueList></QueueList>
+          <BookedList></BookedList>
+        </div>
+      </div>
+      <div className="control" onClick={() => setMini(!isMini)}>
+        <Arrow width={10} height={10} />
+      </div>
     </div>
   );
 }
