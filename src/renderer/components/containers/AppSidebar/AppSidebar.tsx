371df4513eaa4f268bd2e5d304a71e82209c588a
Danzo7 <aymenmiro@gmail.com>
2022-04-16 17:46:42 +0100

    💫 Improve animation

--- src/renderer/components/containers/AppSidebar/AppSidebar.tsx
+++ src/renderer/components/containers/AppSidebar/AppSidebar.tsx
@@ -2,23 +2,32 @@ import BookedList from '@components/booked_list';
 import QueueList from '@components/queue_list';
 import React, { useState } from 'react';
 import Arrow from 'toSvg/arrow.svg?icon';
+import { Transition } from 'react-transition-group';
 import './index.scss';
 interface AppSidebarProps {}
 
 function AppSidebar({}: AppSidebarProps) {
   const [isMini, setMini] = useState(false);
   return (
-    <div className={`AppSidebar${isMini ? ' minimize' : ''}`}>
-      <div className="sidebar">
-        <div className="content">
-          <QueueList></QueueList>
-          <BookedList></BookedList>
-        </div>
-      </div>
-      <div className="control" onClick={() => setMini(!isMini)}>
-        <Arrow width={10} height={10} />
-      </div>
-    </div>
+    <Transition in={!isMini} timeout={200}>
+      {(state) => {
+        return (
+          <div className={`AppSidebar${isMini ? ' minimize' : ''}`}>
+            {state != 'exited' && (
+              <div className="sidebar">
+                <div className={`content ${state}`}>
+                  <QueueList></QueueList>
+                  <BookedList></BookedList>
+                </div>
+              </div>
+            )}
+            <div className="control" onClick={() => setMini(!isMini)}>
+              <Arrow width={10} height={10} />
+            </div>
+          </div>
+        );
+      }}
+    </Transition>
   );
 }
 
