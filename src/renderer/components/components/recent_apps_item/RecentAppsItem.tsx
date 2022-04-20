e70eeb8f5e7e6382c5e076a4f7ebfc2e79a57601
Danzo7 <aymenmiro@gmail.com>
2022-04-20 03:28:49 +0100

    🎨 upate styling

--- src/renderer/components/components/recent_apps_item/RecentAppsItem.tsx
+++ src/renderer/components/components/recent_apps_item/RecentAppsItem.tsx
@@ -1,6 +1,7 @@
 import TextButton from '@components/buttons/text_button';
 import React from 'react';
 import './style/index.scss';
+
 import colors from '@colors';
 interface RecentAppsItemProps {
   fullName: string;
@@ -9,23 +10,17 @@ interface RecentAppsItemProps {
 export default function RecentAppsItem({ fullName, age }: RecentAppsItemProps) {
   return (
     <div className="recent-apps-item">
-      <div className="container">
-        <div className="info-container">
-          <span>{fullName}</span>
-          <span>Age {age}</span>
-        </div>
-        <div className="controls-container">
-          <TextButton
-            text="Run diagnosis..."
-            backgroundColor={colors.cold_blue}
-            radius={7}
-          />
-          <TextButton
-            text="Add"
-            backgroundColor={colors.good_green}
-            radius={7}
-          />
-        </div>
+      <div className="info-container">
+        <span>{fullName}</span>
+        <span>Age {age}</span>
+      </div>
+      <div className="controls-container">
+        <TextButton
+          text="Run diagnosis..."
+          backgroundColor={colors.cold_blue}
+          radius={7}
+        />
+        <TextButton text="Add" backgroundColor={colors.good_green} radius={7} />
       </div>
     </div>
   );
