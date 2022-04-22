ae207179d19725f11f35ad12492c14036861fd96
Danzo7 <aymenmiro@gmail.com>
2022-04-22 07:10:56 +0100

    🧪 Test overlay

--- src/renderer/ui/components/small_clinic_status/SmallClinicStatus.tsx
+++ src/renderer/ui/components/small_clinic_status/SmallClinicStatus.tsx
@@ -1,6 +1,8 @@
 import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
 import ToggleButton from '@components/buttons/toggle_button';
+import QueueAddSearchModal from '@components/modals/queue_add_search_modal';
 import Timer from '@components/small_clinic_status/timer';
+import { Overlay } from '@components/overlay';
 import React, { useState } from 'react';
 import './style/index.scss';
 interface SmallClinicStatusProps {}
@@ -9,7 +11,15 @@ export default function SmallClinicStatus({}: SmallClinicStatusProps) {
   const [isAccept, setIsAccept] = useState(true);
   return (
     <div className="small-clinic-status">
-      <DarkLightCornerButton title=" View clinic..." />
+      <DarkLightCornerButton
+        title=" View clinic..."
+        onPress={() =>
+          Overlay.showModal(<QueueAddSearchModal />, {
+            clickThrough: false,
+            isDimmed: true,
+          })
+        }
+      />
       <div className="content">
         <Timer isActive={isAccept} />
         <div className="switch">
