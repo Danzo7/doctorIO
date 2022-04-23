317d9ea2e345db4dda7a077e1ecc0882091356c4
Danzo7 <aymenmiro@gmail.com>
2022-04-23 03:14:21 +0100

    🔥 Disable locking for now

--- src/renderer/ui/components/queue_list/queue_item/queue_item_wide/buttons_hover_lock/ButtonsHoverLock.tsx
+++ src/renderer/ui/components/queue_list/queue_item/queue_item_wide/buttons_hover_lock/ButtonsHoverLock.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './style/index.scss';
-import invite from 'toSvg/invitePatientIn.svg';
-import view from 'toSvg/view-test.svg';
+import invite from 'toSvg/invitePatientIn.svg?icon';
+import view from 'toSvg/view-test.svg?icon';
 import TextIconButton from '@components/buttons/text_icon_button';
 import colors from '@colors';
 
@@ -10,15 +10,14 @@ const btnList = [
   { icon: view, text: 'View tests', color: colors.cold_blue },
 ];
 function ButtonsHoverLock() {
-  const [currentActive, setActive] = useState(1);
+  //  const [currentActive, setActive] = useState(1);
 
   return (
     <div className="buttons-hover-lock">
       {...btnList.map((e, i) => (
         <TextIconButton
-          startFocus={currentActive === i}
           id={i}
-          onMouseOver={setActive}
+          //onMouseOver={setActive}
           Icon={e.icon}
           text={e.text}
           color={e.color}
