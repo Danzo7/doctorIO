ec0d097f3308a47428c7260460cf24286568f831
Danzo7 <aymenmiro@gmail.com>
2022-04-24 00:57:58 +0100

    ✨ add button

--- src/renderer/ui/components/overlay/OverlayContainer.tsx
+++ src/renderer/ui/components/overlay/OverlayContainer.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/css';
 import { Overlay } from './overlay';
 import React, { ReactNode, useEffect, useRef } from 'react';
 import './style/index.scss';
+import BackButton from '@components/buttons/back_button';
 interface OverlayContainerProps {}
 
 export function OverlayContainer({}: OverlayContainerProps) {
@@ -13,9 +14,17 @@ export function OverlayContainer({}: OverlayContainerProps) {
 
   return <div className="overlay-container" ref={overlayRef}></div>;
 }
+
 export interface OverlayOptions {
   isDimmed?: boolean;
   clickThrough?: boolean;
+  backdropColor?: string;
+  width?: string;
+  //draggable?: boolean;
+  closeBtn?: {
+    placement: 'inner' | 'outer' | 'above';
+    component?: ReactNode;
+  };
   //bgColor,position,draggable,closeBtn
 }
 type OverlayItemProps = OverlayOptions & {
@@ -26,12 +35,15 @@ export function OverlayItem({
   children,
   isDimmed = true,
   clickThrough = false,
+  backdropColor,
+  width = '50%',
+  closeBtn,
 }: OverlayItemProps) {
   return (
     <>
       <div
         className={`backdrop ${css`
-          background-color: ${isDimmed ? '#000000d9' : ''};
+          background-color: ${isDimmed ? backdropColor ?? '#000000d9' : ''};
           pointer-events: ${clickThrough ? 'none' : 'all'};
         `}`}
         onClick={(e) => {
@@ -39,7 +51,20 @@ export function OverlayItem({
           e.stopPropagation();
         }}
       ></div>
-      <div className="layer" children={children} />
+      <div
+        className={`layer ${css`
+          width: ${width};
+        `}`}
+      >
+        {closeBtn && (
+          <div className={`close-btn ${closeBtn.placement}`}>
+            {closeBtn.component ?? (
+              <BackButton onClick={() => Overlay.closeModal()} />
+            )}
+          </div>
+        )}
+        {children}
+      </div>
     </>
   );
 }
