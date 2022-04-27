60720f48cd0a72f406a14d7c4474491e77d005f8
Danzo7 <aymenmiro@gmail.com>
2022-04-27 04:55:51 +0100

    ⚡️ Remove unnecessary wrapper & emotionCss and use outline to avoid shodow glitch

--- src/renderer/ui/components/role_list/role_item/RoleItem.tsx
+++ src/renderer/ui/components/role_list/role_item/RoleItem.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './style/index.scss';
 import Icon from 'toSvg/link.svg?icon';
 import { css } from '@emotion/css';
-import color from '@assets/styles/color';
 interface RoleItemProps {
   roleName: string;
   selected: boolean;
@@ -18,22 +17,17 @@ function RoleItem({
   return (
     <div
       onClick={onClick}
-      className={`role-item ${css`
-        background: ${selected ? color.darkersec_color : 'none'};
-        box-shadow: 0px 0px 0px ${selected ? 1 : 0}px ${color.border_color};
-      `} `}
+      className={`role-item${selected ? ' selected' : ''}`}
     >
-      <div className="role-item-wrapper">
-        <span>{roleName}</span>
-        <div
-          className={`linked  ${css`
-            visibility: ${linked ? 'visible' : 'hidden'};
-          `} `}
-        >
-          <Icon />
-          <div className="linked-role-container">
-            <span>@Rythm#3722</span>
-          </div>
+      <span>{roleName}</span>
+      <div
+        className={`linked  ${css`
+          visibility: ${linked ? 'visible' : 'hidden'};
+        `} `}
+      >
+        <Icon />
+        <div className="linked-role-container">
+          <span>@Rythm#3722</span>
         </div>
       </div>
     </div>
