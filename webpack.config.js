4b335b4ad14fd16c1815bbdb78e5380f81a7d2f1
Danzo7 <aymenmiro@gmail.com>
2022-05-01 03:17:00 +0100

    🎨 organize code & remove unused code

--- webpack.config.js
+++ webpack.config.js
@@ -5,97 +5,112 @@ const tsconfig = require('./tsconfig.json');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = ({ mode } = { mode: process.env.mode }) => {
   const isDevelopment = mode === 'development';
+  const isProduction = mode === 'production';
   return {
     mode: mode,
     entry: ['./src/renderer/index.tsx'],
     output: {
       path: `${__dirname}/build/renderer`,
       publicPath: '/',
-      filename: 'index.js',
-      assetModuleFilename: 'assets/[hash][ext][query]',
+      filename: isProduction
+        ? '[name].[contenthash:8].js'
+        : isDevelopment && '[name].bundle.js',
+      chunkFilename: isProduction
+        ? '[name].[contenthash:8].chunk.js'
+        : isDevelopment && '[name].chunk.js',
+      assetModuleFilename: 'assets/[contenthash][ext][query]',
     },
     module: {
       rules: [
+        //Type Script
         {
           test: /\.tsx?$/,
           use: [
             {
               loader: 'babel-loader',
             },
-
             //   { loader: 'ts-loader' },disabled for now.(testing babel instead)
           ],
           exclude: /node_modules/,
         },
+        ...[
+          //SCSS files
+          {
+            test: /\.s(a|c)ss$/,
+            sideEffects: true,
+            exclude: /\.module.(s(a|c)ss)$/,
+            use: [
+              isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: { sourceMap: true, importLoaders: 1 },
+              },
+
+              isDevelopment && 'postcss-loader',
+
+              { loader: 'sass-loader', options: { sourceMap: true } },
+            ].filter(Boolean),
+          },
+          {
+            test: /\.module\.s(a|c)ss$/,
+            sideEffects: true,
+            use: [
+              isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: { sourceMap: true, importLoaders: 1, modules: true },
+              },
+
+              isProduction && 'postcss-loader',
+
+              { loader: 'sass-loader', options: { sourceMap: true } },
+            ].filter(Boolean),
+          },
+        ],
+
+        ...[
+          //SVG
+          {
+            test: /\.svg$/i,
+            type: 'asset',
+            resourceQuery: /url/, // *.svg ?url
+          },
+          {
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/, // only work ib jsx/tsx file
+            resourceQuery: { not: [/url/, /icon/] },
+            use: ['@svgr/webpack'],
+          },
+          {
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            resourceQuery: /icon/,
+            use: [
+              {
+                loader: '@svgr/webpack',
+                options: {
+                  icon: true,
+                },
+              },
+            ],
+          },
+        ],
 
         {
-          test: /\.scss$/,
-          sideEffects: true,
-          exclude: /\.module.(s(a|c)ss)$/,
-          use: [
-            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: { sourceMap: true, importLoaders: 1 },
-            },
-
-            mode !== 'development' ? 'postcss-loader' : undefined,
-
-            { loader: 'sass-loader', options: { sourceMap: true } },
-          ].filter((r) => r != undefined),
-        },
-        {
-          test: /\.module\.s(a|c)ss$/,
-          sideEffects: true,
-          use: [
-            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: { sourceMap: true, importLoaders: 1, modules: true },
-            },
-
-            !isDevelopment ? 'postcss-loader' : undefined,
-
-            { loader: 'sass-loader', options: { sourceMap: true } },
-          ].filter((r) => r != undefined),
+          test: /\.png/,
+          type: 'asset/resource',
         },
         {
           test: [/\.(woff|tff)$/],
           type: 'asset/resource',
           generator: {
-            filename: '/font/[name][ext]',
+            filename: 'font/[hash][ext][query]',
           },
-        },
-        {
-          test: /\.svg$/i,
-          type: 'asset',
-          resourceQuery: /url/, // *.svg ?url
-        },
-        {
-          test: /\.svg$/i,
-          issuer: /\.[jt]sx?$/, // only work ib jsx/tsx file
-          resourceQuery: { not: [/url/, /icon/] },
-          use: ['@svgr/webpack'],
-        },
-        {
-          test: /\.svg$/i,
-          issuer: /\.[jt]sx?$/,
-          resourceQuery: /icon/,
-          use: [
-            {
-              loader: '@svgr/webpack',
-              options: {
-                icon: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.png/,
-          type: 'asset/resource',
         },
       ],
     },
@@ -118,8 +133,8 @@ module.exports = ({ mode } = { mode: process.env.mode }) => {
         template: 'public/index.html',
       }),
       new MiniCssExtractPlugin({
-        filename: isDevelopment ? '[name].css' : '[name].[hash].css',
-        chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
+        filename: isDevelopment ? '[name].css' : '[name].[contenthash].css',
+        chunkFilename: isDevelopment ? '[id].css' : '[id].[contenthash].css',
       }),
     ],
 
@@ -129,12 +144,17 @@ module.exports = ({ mode } = { mode: process.env.mode }) => {
       port: 4000,
       hot: true,
       historyApiFallback: true,
-      // writeToDisk: true,
     },
     devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     target: isDevelopment ? 'web' : 'browserslist',
+    optimization: {
+      minimize: isProduction,
+      minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+      nodeEnv: mode,
+    },
   };
 };
+
 const aliasResolver = (alias) => {
   const resAlias = {};
   Object.entries(alias).forEach((en) => {
