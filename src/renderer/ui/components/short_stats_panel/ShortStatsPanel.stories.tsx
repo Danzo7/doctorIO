64cb1f9acb2a954f2e911113767a38c4772f2659
Danzo7 <aymenmiro@gmail.com>
2022-05-07 01:03:19 +0100

    🔥 remove props

--- src/renderer/ui/components/short_stats_panel/ShortStatsPanel.stories.tsx
+++ src/renderer/ui/components/short_stats_panel/ShortStatsPanel.stories.tsx
@@ -1,8 +1,7 @@
 import { ComponentProps } from 'react';
 import type { Story } from '@storybook/react';
 import ShortStatsPanel from './ShortStatsPanel';
-import exclamation from 'toSvg/exclamation.svg?icon';
-import colors from '@colors';
+
 export default {
   title: 'ShortStatsPanel',
   component: ShortStatsPanel,
@@ -11,30 +10,4 @@ const Template: Story<ComponentProps<typeof ShortStatsPanel>> = (args) => (
   <ShortStatsPanel {...args} />
 );
 export const FirstStory = Template.bind({});
-FirstStory.args = {
-  miniStatsList: [
-    {
-      text: 'Revenue',
-      value: 1.4,
-      Icon: exclamation,
-      percentage: 5,
-    },
-    {
-      text: 'Visitors',
-      value: 24,
-      Icon: exclamation,
-      percentage: 10,
-    },
-    {
-      text: 'Messages',
-      value: 19,
-      Icon: exclamation,
-      backgroundColor: colors.hot_red,
-    },
-    {
-      text: 'Queue',
-      value: 18,
-      Icon: exclamation,
-    },
-  ],
-};
+FirstStory.args = {};
--- src/renderer/ui/components/short_stats_panel/ShortStatsPanel.tsx
+++ src/renderer/ui/components/short_stats_panel/ShortStatsPanel.tsx
@@ -1,13 +1,36 @@
 import MultiOptionSwitcher from '@components/buttons/multi_option_switcher';
 import MiniStats from '@components/mini_stats';
 import './style/index.scss';
-interface ShortStatsPanelProps {
-  miniStatsList: Array<any>;
-}
-export default function ShortStatsPanel({
-  miniStatsList = [],
-}: ShortStatsPanelProps) {
+import exclamation from 'toSvg/exclamation.svg?icon';
+import colors from '@colors';
+interface ShortStatsPanelProps {}
+export default function ShortStatsPanel({}: ShortStatsPanelProps) {
   const timeSortList = ['Today', 'Monthly'];
+  const miniStatsList = [
+    {
+      text: 'Revenue',
+      value: 1.4,
+      Icon: exclamation,
+      percentage: 5,
+    },
+    {
+      text: 'Visitors',
+      value: 24,
+      Icon: exclamation,
+      percentage: 10,
+    },
+    {
+      text: 'Messages',
+      value: 19,
+      Icon: exclamation,
+      backgroundColor: colors.hot_red,
+    },
+    {
+      text: 'Queue',
+      value: 18,
+      Icon: exclamation,
+    },
+  ];
   return (
     <div className="short-stats-panel">
       <div className="header">
@@ -18,6 +41,7 @@ export default function ShortStatsPanel({
         {miniStatsList.map(
           ({ text, Icon, value, backgroundColor, percentage }) => (
             <MiniStats
+              key={text + value}
               text={text}
               Icon={Icon}
               value={value}
