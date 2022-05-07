2a8b9a2a9e7f5d94449b4edf282654a65df1bd36
Danzo7 <aymenmiro@gmail.com>
2022-05-07 01:02:14 +0100

    🎨 fix bug & more readable config

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,8 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 module.exports = ({ mode } = { mode: process.env.mode }) => {
   const isDevelopment = mode === 'development';
   const isProduction = mode === 'production';
+  console.log(isDevelopment + ' and ' + isProduction);
+
   return {
     mode: mode,
     entry: ['./src/renderer/index.tsx'],
@@ -38,8 +40,10 @@ module.exports = ({ mode } = { mode: process.env.mode }) => {
           ],
           exclude: /node_modules/,
         },
+        //scss|sass file rules
         ...[
           //SCSS files
+          //load sass files and ignore module extension
           {
             test: /\.s(a|c)ss$/,
             sideEffects: true,
@@ -51,11 +55,13 @@ module.exports = ({ mode } = { mode: process.env.mode }) => {
                 options: { sourceMap: true, importLoaders: 1 },
               },
 
-              isDevelopment && 'postcss-loader',
+              isProduction && 'postcss-loader',
 
               { loader: 'sass-loader', options: { sourceMap: true } },
             ].filter(Boolean),
           },
+          //module style files
+          //load module style files only
           {
             test: /\.module\.s(a|c)ss$/,
             sideEffects: true,
@@ -72,20 +78,25 @@ module.exports = ({ mode } = { mode: process.env.mode }) => {
             ].filter(Boolean),
           },
         ],
+        //SVG rules
 
         ...[
-          //SVG
+          //load *.svg?url as svg file
           {
             test: /\.svg$/i,
             type: 'asset',
             resourceQuery: /url/, // *.svg ?url
           },
+
+          //load *.svg as react components
           {
             test: /\.svg$/i,
             issuer: /\.[jt]sx?$/, // only work ib jsx/tsx file
             resourceQuery: { not: [/url/, /icon/] },
             use: ['@svgr/webpack'],
           },
+          //load *.svg?icon as react components with viewbox.
+
           {
             test: /\.svg$/i,
             issuer: /\.[jt]sx?$/,
@@ -126,6 +137,7 @@ module.exports = ({ mode } = { mode: process.env.mode }) => {
     },
 
     plugins: [
+      //Ts error
       new ForkTsCheckerWebpackPlugin(),
 
       new HtmlWebpackPlugin({
