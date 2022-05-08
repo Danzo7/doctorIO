d116cd5eebe61aa3169f2c84ae3949270038d7f7
Danzo7 <aymenmiro@gmail.com>
2022-05-08 04:51:06 +0100

    🧪 test react router

--- src/renderer/ui/containers/AppContent/AppContent.tsx
+++ src/renderer/ui/containers/AppContent/AppContent.tsx
@@ -1,11 +1,29 @@
 import HomeContent from './pages/home';
 import './index.scss';
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+const Messages = lazy(() => import('./pages/messages'));
+
 interface AppContentProps {}
 
 function AppContent({}: AppContentProps) {
   return (
     <div className="AppContent">
-      <HomeContent></HomeContent>
+      <Routes>
+        {/* lazy route example */}
+
+        <Route
+          path="messages"
+          element={
+            <Suspense fallback={<>Loading</>}>
+              <Messages />
+            </Suspense>
+          }
+        />
+        {/* route example */}
+
+        <Route path="/" element={<HomeContent />} />
+      </Routes>
     </div>
   );
 }
