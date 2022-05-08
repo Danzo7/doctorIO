37c55bb6acedf481dff4c9db6bdab8cd2197d5b9
Danzo7 <aymenmiro@gmail.com>
2022-05-08 02:43:07 +0100

    💄 build home page

--- src/renderer/ui/containers/AppContent/pages/home/Home.tsx
+++ src/renderer/ui/containers/AppContent/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import MembersPanel from '@components/members_panel';
 import ShortStatsPanel from '@components/short_stats_panel';
 import SmallClinicStatus from '@components/small_clinic_status';
 import WelcomeBox from '@components/welcome_box';
@@ -6,9 +7,12 @@ interface HomeProps {}
 export default function Home({}: HomeProps) {
   return (
     <>
-      <WelcomeBox />
-      <ShortStatsPanel />
       <div>
+        <WelcomeBox message="Welcome Boy" />
+        <ShortStatsPanel />
+      </div>
+      <div className="home-body">
+        <MembersPanel />
         <SmallClinicStatus />
       </div>
     </>
