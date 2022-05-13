bfe640647da9455aa7b0d873f46c31780ab59cad
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-05-13 16:23:36 +0100

    💄 add transtion and onClick Prop to MessagesCategoryItem

--- src/renderer/ui/components/messages_side_bar/messages_categories/messages_category_item/MessagesCategoryItem.tsx
+++ src/renderer/ui/components/messages_side_bar/messages_categories/messages_category_item/MessagesCategoryItem.tsx
@@ -5,16 +5,19 @@ interface MessagesCategoryItemProps {
   Icon: FunctionComponent<SVGProps<SVGSVGElement>>;
   categoryName: string;
   selected: boolean;
+  onSelect: () => void;
 }
 export default function MessagesCategoryItem({
   Icon,
   categoryName,
   selected,
+  onSelect,
 }: MessagesCategoryItemProps) {
   return (
     <div
       className="messages-category-item"
       css={{ backgroundColor: selected ? color.border_color : undefined }}
+      onClick={onSelect}
     >
       <Icon width={18} height={21} />
       <span>{categoryName}</span>
