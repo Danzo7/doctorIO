847e7f2ca97088733cb88c49c0d7886513e1fa51
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-05-14 16:56:06 +0100

    💄 Contact page

--- src/renderer/ui/containers/AppContent/pages/messages/pages/contact/Contact.tsx
+++ src/renderer/ui/containers/AppContent/pages/messages/pages/contact/Contact.tsx
@@ -1,5 +1,96 @@
+import ContactTabMenu from './contact_tab_menu';
 import './style/index.scss';
+import Doctor from 'toSvg/doctor_icon.svg?icon';
+import ContactList from './contact_list';
+import profile from '@assets/pictures/test.png';
+
+const contactList = [
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe1',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe2',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe3',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe4',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe5',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe6',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe7',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe8',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe9',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe10',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe11',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe12',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe13',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe14',
+  },
+  {
+    status: true,
+    imgSrc: profile,
+    fullName: 'John Doe15',
+  },
+];
+
 interface ContactProps {}
 export default function Contact({}: ContactProps) {
-  return <div className="contact"></div>;
+  return (
+    <div className="contact">
+      <ContactTabMenu
+        messagesCategoryName="Clinic"
+        messagesCategoryIcon={Doctor}
+      />
+      <ContactList contactType="ONLINE" contactList={contactList} />
+    </div>
+  );
 }
