6e2347c060ea540573876c8874a01fbd7ca8eb96
Danzo7 <aymenmiro@gmail.com>
2022-05-20 17:19:00 +0100

    💄  add Header

--- src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/ClinicSettings.tsx
+++ src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/ClinicSettings.tsx
@@ -1,3 +1,5 @@
+import colors from '@assets/styles/color';
+import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
 import RolesTab from '@components/roles_tab';
 import TabMenu from '@components/tab_menu';
 import { Route, Routes, useNavigate } from 'react-router-dom';
@@ -7,7 +9,18 @@ export default function ClinicSettings({}: ClinicSettingsProps) {
   const navigate = useNavigate();
   return (
     <div className="clinic-settings">
-      <span>Clinic Settings</span>
+      <div className="header">
+        <span>Clinic Settings</span>
+        <DarkLightCornerButton
+          title="view all"
+          onPress={() => navigate('all', { replace: false })}
+        />
+        <DarkLightCornerButton
+          title="disconnect"
+          textColor={colors.hot_red}
+          isActive={true}
+        />
+      </div>
       <TabMenu
         items={[
           'Overview',
