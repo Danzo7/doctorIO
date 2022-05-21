68f419dad513b5613383105922ba60bd1edab652
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-05-21 18:14:34 +0100

    ➕ border prop to CircleAvatar

--- src/renderer/ui/components/avatars/circle_avatar/CircleAvatar.tsx
+++ src/renderer/ui/components/avatars/circle_avatar/CircleAvatar.tsx
@@ -3,11 +3,24 @@ interface CircleAvatarProps {
   src: string;
   width: number;
   alt?: string;
-  radius?: number;
+  radius?: number | string;
+  border?: string;
 }
-function CircleAvatar({ src, width, alt, radius }: CircleAvatarProps) {
+function CircleAvatar({
+  src,
+  width,
+  alt,
+  radius = '100%',
+  border,
+}: CircleAvatarProps) {
   return (
-    <div className="circle-avatar" css={{ borderRadius: radius }}>
+    <div
+      className="circle-avatar"
+      css={{
+        borderRadius: radius,
+        border: border,
+      }}
+    >
       <img
         css={{
           width: width,
