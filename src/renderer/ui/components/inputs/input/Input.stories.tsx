9cf514f64872166574cb1d91ae239c581e5d966a
Danzo7 <aymenmiro@gmail.com>
2022-05-26 00:42:50 +0100

    📝 add story

--- src/renderer/ui/components/inputs/input/Input.stories.tsx
+++ src/renderer/ui/components/inputs/input/Input.stories.tsx
@@ -1,6 +1,10 @@
 import { ComponentProps } from 'react';
 import type { Story } from '@storybook/react';
 import Input from './Input';
+import IconicButton from '@components/buttons/iconic_button';
+import search from 'toSvg/search.svg?icon';
+import { color } from '@assets/styles/color';
+import Select from '../select';
 export default {
   title: 'inputs/Input',
   component: Input,
@@ -9,4 +13,18 @@ const Template: Story<ComponentProps<typeof Input>> = (args) => (
   <Input {...args} />
 );
 export const FirstStory = Template.bind({});
-FirstStory.args = {};
+FirstStory.args = {
+  hint: 'hello',
+  label: 'name',
+  placeholder: 'username',
+  type: 'url',
+  leading: (
+    <IconicButton
+      Icon={search}
+      backgroundColor={color.cold_blue}
+      width={25}
+      iconSize={10}
+    />
+  ),
+  trailing: <Select options={['.exe', '.svg']} placeholder="type" width={80} />,
+};
