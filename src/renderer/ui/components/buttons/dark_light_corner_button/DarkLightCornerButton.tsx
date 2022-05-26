b4d2dd208a4ff3bb3395b9ef11a5c4f80e1d1050
Danzo7 <aymenmiro@gmail.com>
2022-05-26 01:08:30 +0100

    💄 Add blend styling(similar to sidebar) to use on LighterBackground

--- src/renderer/ui/components/buttons/dark_light_corner_button/DarkLightCornerButton.tsx
+++ src/renderer/ui/components/buttons/dark_light_corner_button/DarkLightCornerButton.tsx
@@ -5,6 +5,7 @@ interface DarkLightCornerButtonProps {
   isActive?: boolean;
   onPress?: () => void;
   textColor?: string;
+  blend?: true;
   type?: 'button' | 'submit' | 'reset';
 }
 //the reason why i put borderColor={colors.background} is to eliminate the height change after hover //
@@ -14,6 +15,7 @@ function DarkLightCornerButton({
   onPress,
   type,
   textColor = colors.white,
+  blend,
 }: DarkLightCornerButtonProps) {
   return (
     <TextButton
@@ -21,10 +23,10 @@ function DarkLightCornerButton({
       text={title}
       fontColor={textColor}
       fontSize={14}
-      backgroundColor={isActive ? colors.darkersec_color : undefined}
-      borderColor={isActive ? colors.border_color : undefined}
-      afterBgColor={colors.darkersec_color}
-      afterBorderColor={colors.border_color}
+      backgroundColor={isActive && !blend ? colors.darkersec_color : undefined}
+      borderColor={isActive && !blend ? colors.border_color : undefined}
+      afterBgColor={!blend ? colors.darkersec_color : colors.secondary_color}
+      afterBorderColor={!blend ? colors.border_color : undefined}
       radius={7}
       type={type}
     />
