444b5a381bb41048355a525d2e6fba2aaf1940ae
Danzo7 <aymenmiro@gmail.com>
2022-05-28 01:37:08 +0100

    🐛 fix ts warning when exporting types

--- src/renderer/libs/smart_link/index.ts
+++ src/renderer/libs/smart_link/index.ts
@@ -1,3 +1,3 @@
 import SmartLink, { RouteExact, ToRoute, ToWithInclude } from './SmartLink';
 export default SmartLink;
-export { RouteExact, ToRoute, ToWithInclude };
+export type { RouteExact, ToRoute, ToWithInclude };
