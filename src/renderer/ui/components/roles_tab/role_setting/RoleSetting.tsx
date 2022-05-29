e32d9154d640b3588c7598f7ff2b82fc9d1bffb7
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-05-29 18:03:05 +0100

    ✨  add Routes to Role Tab

--- src/renderer/ui/components/roles_tab/role_setting/RoleSetting.tsx
+++ src/renderer/ui/components/roles_tab/role_setting/RoleSetting.tsx
@@ -1,14 +1,72 @@
-import TabMenu from '@components/tab_menu';
-import PermissionList from './permission_list';
+import TabMenu, { NavTabMenu } from '@components/tab_menu';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import PermissionList from './miniTabs/permission_list';
+import RoleSettingGeneral from './miniTabs/role_setting_general';
 import './style/index.scss';
-interface RoleSettingProps {
-  permissionArray: any[];
-}
-export default function RoleSetting({ permissionArray }: RoleSettingProps) {
+
+const permissionArray = [
+  {
+    permissionName: 'administrator',
+    permissionDescription: 'Can access to database and edit clinic information',
+    disabled: true,
+  },
+  {
+    permissionName: 'Assistant',
+    permissionDescription:
+      '   people with this role will assist another role, mean they can only access to the dependent role permission',
+    linkedPermission: ' @Rythm#3722',
+  },
+  {
+    permissionName: 'Access to patients List',
+    permissionDescription: 'This is an example permission',
+  },
+  {
+    permissionName: 'Assistant',
+    permissionDescription:
+      '   people with this role will assist another role, mean they can only access to the dependent role permission',
+    linkedPermission: ' @Rythm#3722',
+  },
+  {
+    permissionName: 'Add patients',
+    permissionDescription: '  This is an example permission',
+  },
+  {
+    permissionName: 'Edit patients data',
+    permissionDescription: ' This is an example permission',
+  },
+  {
+    permissionName: 'Edit patients data',
+    permissionDescription: ' This is an example permission',
+  },
+  {
+    permissionName: 'Edit patients data',
+    permissionDescription: ' This is an example permission',
+  },
+  {
+    permissionName: 'Edit patients data',
+    permissionDescription: ' This is an example permission',
+  },
+];
+
+interface RoleSettingProps {}
+export default function RoleSetting({}: RoleSettingProps) {
   return (
     <div className="role-setting">
-      <TabMenu items={['General', 'Permissions', 'Members']} />
-      <PermissionList permissionArray={permissionArray} />
+      <NavTabMenu
+        items={[
+          { name: 'General', route: { to: '', include: 'General' } },
+          'Permissions',
+          'Members',
+        ]}
+      />
+      <Routes>
+        <Route path="" element={<RoleSettingGeneral />} />
+        <Route path="General" element={<RoleSettingGeneral />} />
+        <Route
+          path="Permissions"
+          element={<PermissionList permissionArray={permissionArray} />}
+        />
+      </Routes>
     </div>
   );
 }
