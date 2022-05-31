2d86aedfe126e4b13078f144e8703912ac64c19d
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-05-31 17:20:35 +0100

    ✅  update RecentAppsItem

--- src/renderer/ui/components/recent_apps_item/RecentAppsItem.tsx
+++ src/renderer/ui/components/recent_apps_item/RecentAppsItem.tsx
@@ -1,26 +1,19 @@
 import TextButton from '@components/buttons/text_button';
-import './style/index.scss';
-
-import colors from '@colors';
+import { color } from '@colors';
+import PresentationItem from '@components/presentation_item';
 interface RecentAppsItemProps {
   fullName: string;
   age: number;
 }
 export default function RecentAppsItem({ fullName, age }: RecentAppsItemProps) {
   return (
-    <div className="recent-apps-item">
-      <div className="info-container">
-        <span>{fullName}</span>
-        <span>Age {age}</span>
-      </div>
-      <div className="controls-container">
-        <TextButton
-          text="Run diagnosis..."
-          backgroundColor={colors.cold_blue}
-          radius={7}
-        />
-        <TextButton text="Add" backgroundColor={colors.good_green} radius={7} />
-      </div>
-    </div>
+    <PresentationItem primaryText={fullName} secondaryText={`Age ${age}`}>
+      <TextButton
+        text="Run diagnosis..."
+        backgroundColor={color.cold_blue}
+        radius={7}
+      />
+      <TextButton text="Add" backgroundColor={color.good_green} radius={7} />
+    </PresentationItem>
   );
 }
