390d1e637649cb22accedb1c092dff41e3e4a6d0
Danzo7 <aymenmiro@gmail.com>
2022-05-31 03:16:26 +0100

    🏗️  add borderColor props

--- src/renderer/ui/components/wide_card/WideCard.tsx
+++ src/renderer/ui/components/wide_card/WideCard.tsx
@@ -3,15 +3,25 @@ import { ReactNode } from 'react';
 import './style/index.scss';
 interface WideCardProps {
   children: ReactNode[];
+  borderColor?: string;
 }
-export default function WideCard({ children }: WideCardProps) {
+export default function WideCard({ borderColor, children }: WideCardProps) {
   return (
-    <div className="wide-card">
+    <div
+      className="wide-card"
+      css={{
+        border: borderColor && `1px solid ${borderColor}`,
+        boxShadow:
+          borderColor &&
+          `0 0px 1px ${borderColor} inset, 0 0 3px 
+      ${borderColor}`,
+      }}
+    >
       {children.map((child, index) => (
         <>
           {child}
           {index != children.length - 1 && (
-            <BorderSeparator direction="vertical" />
+            <BorderSeparator color={borderColor} direction="vertical" />
           )}
         </>
       ))}
