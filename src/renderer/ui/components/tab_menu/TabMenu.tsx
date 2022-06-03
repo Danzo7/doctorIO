3a766fc23d133476c464d97c0d8a6ba346c8412f
Danzo7 <aymenmiro@gmail.com>
2022-06-03 18:38:05 +0100

    ✨ Add children for static navigation

--- src/renderer/ui/components/tab_menu/TabMenu.tsx
+++ src/renderer/ui/components/tab_menu/TabMenu.tsx
@@ -1,18 +1,20 @@
 import colors from '@assets/styles/color';
 import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 import './style/index.scss';
 interface TabMenuProps {
   items: Array<string>;
   defaultSelected?: number;
   onChanged?: ({ item, index }: { item: string; index: number }) => void;
   borderBottom?: boolean;
+  children?: ReactNode[];
 }
 export default function TabMenu({
   items,
   defaultSelected = 0,
   onChanged,
   borderBottom = true,
+  children,
 }: TabMenuProps) {
   const [selected, setSelected] = useState(defaultSelected);
   function setTab(index: number) {
@@ -20,24 +22,27 @@ export default function TabMenu({
     onChanged?.({ item: items[index], index: index });
   }
   return (
-    <div
-      className="tab-menu"
-      css={
-        borderBottom
-          ? { borderBottom: `1px solid ${colors.border_color}` }
-          : undefined
-      }
-    >
-      <div className="menu-items">
-        {items.map((text, index) => (
-          <DarkLightCornerButton
-            key={index}
-            title={text}
-            isActive={selected == index}
-            onPress={() => setTab(index)}
-          />
-        ))}
+    <div className="tab-menu">
+      <div
+        className="tab-menu-bar"
+        css={
+          borderBottom
+            ? { borderBottom: `1px solid ${colors.border_color}` }
+            : undefined
+        }
+      >
+        <div className="menu-items">
+          {items.map((text, index) => (
+            <DarkLightCornerButton
+              key={index}
+              title={text}
+              isActive={selected == index}
+              onPress={() => setTab(index)}
+            />
+          ))}
+        </div>
       </div>
+      {children?.find((child, index) => index == selected)}
     </div>
   );
 }
