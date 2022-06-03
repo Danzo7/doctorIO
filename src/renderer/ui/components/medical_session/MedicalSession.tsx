ed0192b264250d40c908773bec9dd1b0e7d067df
Danzo7 <aymenmiro@gmail.com>
2022-06-03 18:39:50 +0100

    ⚡️ Use static navigation

--- src/renderer/ui/components/medical_session/MedicalSession.tsx
+++ src/renderer/ui/components/medical_session/MedicalSession.tsx
@@ -1,19 +1,23 @@
-import { NavTabMenu } from '@components/tab_menu';
-import { Route, Routes } from 'react-router-dom';
+import BorderSeparator from '@components/border_separator';
+import TabMenu from '@components/tab_menu';
 import MedicalSessionSideBar from './medical_session_side_bar';
 import PrescriptionTab from './pages/prescription_tab';
+import SessionParameter from './pages/session_parameter';
 import './style/index.scss';
 interface MedicalSessionProps {}
 export default function MedicalSession({}: MedicalSessionProps) {
   return (
+    //todo:TabMenu
     <div className="medical-session">
       <MedicalSessionSideBar />
       <div className="content-container">
         <span>Session</span>
-        <NavTabMenu items={['prescription', 'notice']} />
-        <Routes>
-          <Route path="" element={<PrescriptionTab />} />
-        </Routes>
+        <TabMenu items={['prescription', 'notice']}>
+          <PrescriptionTab />
+          <SessionParameter />
+        </TabMenu>
+        <BorderSeparator direction="horizontal" />
+        <SessionParameter />
       </div>
     </div>
   );
