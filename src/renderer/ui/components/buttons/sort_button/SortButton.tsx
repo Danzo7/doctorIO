e8dfbd952ea5656c0392e4f04bb6430a61284822
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-03 18:12:18 +0100

    💄 SortButton

---
+++ src/renderer/ui/components/buttons/sort_button/SortButton.tsx
@@ -0,0 +1,40 @@
+import color from '@assets/styles/color';
+import TextButton from '../text_button';
+import Arrow from 'toSvg/arrow.svg?icon';
+import './style/index.scss';
+import { useState } from 'react';
+interface SortButtonProps {
+  title: string;
+}
+export default function SortButton({ title }: SortButtonProps) {
+  const [direction, setDirection] = useState('up');
+  const changeDirection = () => {
+    if (direction == 'up') {
+      setDirection('down');
+    } else {
+      setDirection('up');
+    }
+  };
+  return (
+    <TextButton
+      text={title}
+      fontColor={color.silver_gray}
+      fontSize={12}
+      fontWeight={400}
+      backgroundColor={color.darkersec_color}
+      padding={'5px 10px'}
+      itemsDirection="row-reverse"
+      alignment="center"
+      onPress={() => {
+        changeDirection();
+      }}
+    >
+      <Arrow
+        css={{ transform: direction == 'up' ? 'rotate(180deg)' : 'unset' }}
+        className="arrow"
+        width={12}
+        height={8}
+      />
+    </TextButton>
+  );
+}
