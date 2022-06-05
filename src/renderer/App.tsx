c5663d572efe4d377bee9e7b74f441622aed8902
Danzo7 <aymenmiro@gmail.com>
2022-06-05 04:28:08 +0100

    🐛 update imports

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,7 +2,7 @@ import './App.scss';
 import AppMenu from '@containers/AppMenu';
 import AppContent from '@containers/AppContent';
 import AppSidebar from '@containers/AppSidebar';
-import { OverlayContainer } from '@components/overlay';
+import { OverlayContainer } from '@libs/overlay';
 
 interface AppProps {}
 
