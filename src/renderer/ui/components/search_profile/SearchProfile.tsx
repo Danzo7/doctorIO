4ed7da8efb2879f5194ef05c3d0202265b374622
Danzo7 <aymenmiro@gmail.com>
2022-06-06 04:07:28 +0100

    ✨ add overlay example

--- src/renderer/ui/components/search_profile/SearchProfile.tsx
+++ src/renderer/ui/components/search_profile/SearchProfile.tsx
@@ -3,10 +3,14 @@ import UserProfileStatus from '@components/user_profile_status';
 import Search from 'toSvg/search.svg?icon';
 import colors from '@colors';
 import './style/index.scss';
+import { useOverlay } from '@libs/overlay/useOverlay';
+import QueueAddSearchModal from '@components/modals/queue_add_search_modal';
 interface SearchProfileProps {
   imgSrc: string;
 }
 export default function SearchProfile({ imgSrc }: SearchProfileProps) {
+  const { open } = useOverlay();
+  //todo:remove imgSrc
   return (
     <div className="search-profile">
       <UserProfileStatus width={40} status={true} imgSrc={imgSrc} />
@@ -17,6 +21,14 @@ export default function SearchProfile({ imgSrc }: SearchProfileProps) {
           iconSize={15}
           backgroundColor={colors.silver_gray}
           radius={7}
+          onPress={() =>
+            open(<QueueAddSearchModal />, {
+              closeOnClickOutside: true,
+              clickThrough: false,
+              position: { top: '30%' },
+              width: '50%',
+            })
+          }
         />
       </div>
     </div>
