5e9ce776d1366b815e97103ea86dc9c4bc007b2e
Danzo7 <aymenmiro@gmail.com>
2022-06-08 16:11:09 +0100

    👔 Add logic

--- src/renderer/ui/components/medical_session/pages/notice_tab/NoticeTab.tsx
+++ src/renderer/ui/components/medical_session/pages/notice_tab/NoticeTab.tsx
@@ -1,34 +1,29 @@
-import color from '@assets/styles/color';
-import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
-import TextButton from '@components/buttons/text_button';
-import Input from '@components/inputs/input';
-import { ChangeEvent, useState } from 'react';
+import TextArea from '@components/inputs/text_area';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import './style/index.scss';
 interface NoticeTabProps {}
+type Data = {
+  notice: string;
+};
 export default function NoticeTab({}: NoticeTabProps) {
-  const [textAreaValue, setTextAreaValue] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Data>({ mode: 'onChange' });
+
+  const onSubmit: SubmitHandler<Data> = (data) => console.log(data);
+
   return (
     <div className="notice-tab">
-      <div className="textarea-container">
-        <textarea
-          onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
-            console.log(e.target.value);
-            setTextAreaValue(e.target.value);
-          }}
-          placeholder="write something..."
-        ></textarea>
-        {textAreaValue.length > 0 && (
-          <div className="save-btn-wrapper">
-            <TextButton
-              text="Save"
-              backgroundColor={color.secondary_color}
-              fontSize={14}
-              fontWeight={600}
-              padding="5px 15px"
-            />
-          </div>
-        )}
-      </div>
+      <TextArea
+        fillContainer
+        onSubmit={handleSubmit(onSubmit)}
+        errorMessage={errors.notice?.message}
+        {...register('notice', {
+          required: { value: true, message: 'dam Ass' },
+        })}
+      />
     </div>
   );
 }
