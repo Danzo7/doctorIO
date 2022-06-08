d73dda3123810fc98404a10049efa82939f9cdb9
Danzo7 <aymenmiro@gmail.com>
2022-06-08 16:05:06 +0100

    ✨ TextArea

---
+++ src/renderer/ui/components/inputs/text_area/TextArea.tsx
@@ -0,0 +1,69 @@
+import { color } from '@assets/styles/color';
+import TextButton from '@components/buttons/text_button';
+import { forwardRef, useState } from 'react';
+import { FormHookProps } from '../input';
+import InputContainer from '../input_container';
+import InputWrapper from '../input_wrapper';
+import './style/index.scss';
+interface TextAreaProps {
+  label?: string;
+  errorMessage?: string;
+  hint?: string;
+  fillContainer?: true;
+  onSubmit?: () => void;
+}
+export default forwardRef(function TextArea(
+  {
+    label,
+    errorMessage,
+    hint,
+    fillContainer,
+    onChange,
+    onSubmit,
+    ...others
+  }: TextAreaProps & FormHookProps,
+  ref,
+) {
+  const [changed, setChanged] = useState(false);
+  return (
+    <InputContainer
+      label={label}
+      errorMessage={errorMessage}
+      hint={hint}
+      fillContainer={fillContainer}
+    >
+      <InputWrapper
+        height={'fit-content'}
+        errorMessage={errorMessage}
+        fillContainer={fillContainer}
+      >
+        <div className="text-area">
+          <textarea
+            placeholder="write something..."
+            {...others}
+            onChange={(e) => {
+              onChange?.(e);
+              setChanged(true);
+            }}
+            ref={ref as any}
+          />
+
+          {changed && (
+            <div className="save-btn-wrapper">
+              <TextButton
+                text="Save"
+                backgroundColor={color.secondary_color}
+                fontSize={14}
+                disabled={!!errorMessage}
+                onPress={() => {
+                  onSubmit?.();
+                  if (!errorMessage) setChanged(false);
+                }}
+              />
+            </div>
+          )}
+        </div>
+      </InputWrapper>
+    </InputContainer>
+  );
+});
