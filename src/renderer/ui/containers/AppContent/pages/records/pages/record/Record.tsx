3ff1531cfff48a68a8aea9a0edbba91f309f2aea
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-12 19:45:42 +0100

    ➕  add Search on Record

--- src/renderer/ui/containers/AppContent/pages/records/pages/record/Record.tsx
+++ src/renderer/ui/containers/AppContent/pages/records/pages/record/Record.tsx
@@ -4,6 +4,10 @@ import Input from '@components/inputs/input';
 import MedicalHistory from '@components/medical_session/medical_session_side_bar/medical_history';
 import PatientCard from '@components/patient_card';
 import MiniPatientCard from '@components/patient_card/mini_patient_card';
+import RecordInfoItem from '@components/record_info_item';
+import useNavigation from '@libs/hooks/useNavigation';
+import useSearchPatient from '@libs/hooks/useSearchPatient';
+import { useForm } from 'react-hook-form';
 import Search from 'toSvg/search.svg?icon';
 import './style/index.scss';
 
@@ -13,7 +17,7 @@ interface SearchInput {
 const usersData = [
   {
     fullName: 'brahim aymen',
-    id: '#123456789',
+    id: '123456789',
     medicalHistoryList: [
       {
         medicalDescription: 'had a noise pain',
@@ -99,7 +103,7 @@ const usersData = [
   },
   {
     fullName: 'daouadji aymen',
-    id: '#12345689',
+    id: '12345689',
     medicalHistoryList: [],
     documentList: [],
     recordData: {
@@ -113,7 +117,7 @@ const usersData = [
   },
   {
     fullName: 'amine bou',
-    id: '#45689745',
+    id: '45689745',
     medicalHistoryList: [],
     documentList: [],
     recordData: {
@@ -127,7 +131,7 @@ const usersData = [
   },
   {
     fullName: 'John Doe',
-    id: '#12344689',
+    id: '12344689',
     medicalHistoryList: [],
     documentList: [],
     recordData: {
@@ -143,43 +147,63 @@ const usersData = [
 
 interface RecordProps {}
 export default function Record({}: RecordProps) {
+  const { register, watch, setValue } = useForm<SearchInput>();
+  const watchSearch = watch('searchField');
+  const matches = useSearchPatient(watchSearch, usersData);
+  const { navigate } = useNavigation();
   return (
     <div className="record">
       <Input
         placeholder="Enter patient Id"
-        trailing={
-          <Search
-            onClick={() => {
-              //      onChangeHandler(watchSearch);
-            }}
-          />
-        }
+        trailing={<Search />}
         type={'search'}
-        //      register={register('searchField')}
+        {...register('searchField')}
       />
-      <div className="record-content">
-        <div className="record-infos">
-          <PatientCard
-            data={usersData[0].recordData}
-            LeftComp={
-              <MiniPatientCard
-                patientFullName="John Doe"
-                patientId="#123468"
-                numPostAppointment={18}
-                nextAppointmentDate="25 jan 2028"
+      {matches ? (
+        matches.length > 0 ? (
+          <div className="records-suggestions-container">
+            {matches?.map(({ fullName, id }, index) => (
+              <RecordInfoItem
+                fullName={fullName}
+                id={id}
+                key={index}
+                onViewRecord={() => {
+                  setValue('searchField', '');
+                  navigate(`/records/${id}`);
+                }}
               />
-            }
-          />
-          <PatientCard data={usersData[0].recordData} />
-          <BookingTimeline />
+            ))}
+          </div>
+        ) : (
+          <div className="not-found">
+            <span>No patient found ! </span>
+          </div>
+        )
+      ) : (
+        <div className="record-content">
+          <div className="record-infos">
+            <PatientCard
+              data={usersData[0].recordData}
+              LeftComp={
+                <MiniPatientCard
+                  patientFullName="John Doe"
+                  patientId="#123468"
+                  numPostAppointment={18}
+                  nextAppointmentDate="25 jan 2028"
+                />
+              }
+            />
+            <PatientCard data={usersData[0].recordData} />
+            <BookingTimeline />
+          </div>
+          <div className="record-side-info">
+            <MedicalHistory
+              medicalHistoryList={usersData[0].medicalHistoryList}
+            />
+            <DocumentPreviewPanel documentList={usersData[0].documentList} />
+          </div>
         </div>
-        <div className="record-side-info">
-          <MedicalHistory
-            medicalHistoryList={usersData[0].medicalHistoryList}
-          />
-          <DocumentPreviewPanel documentList={usersData[0].documentList} />
-        </div>
-      </div>
+      )}
     </div>
   );
 }
