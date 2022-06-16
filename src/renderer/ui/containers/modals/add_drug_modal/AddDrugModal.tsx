61d799990ea4e7360957088d10b456cb1bfb16af
Danzo7 <aymenmiro@gmail.com>
2022-06-16 23:26:56 +0100

    ✨ use ModalContainer

--- src/renderer/ui/containers/modals/add_drug_modal/AddDrugModal.tsx
+++ src/renderer/ui/containers/modals/add_drug_modal/AddDrugModal.tsx
@@ -1,10 +1,8 @@
 import { color } from '@assets/styles/color';
-import SquareIconButton from '@components/buttons/square_icon_button/SquareIconButton';
 import TextButton from '@components/buttons/text_button';
-import Header from '@components/header';
 import Input from '@components/inputs/input';
+import ModalContainer from '@components/modal_container';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import './style/index.scss';
 
 type Inputs = {
   drugName: string;
@@ -19,45 +17,47 @@ export default function AddDrugModal({ onAdd }: AddDrugModalProps) {
   const { register, handleSubmit } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="add-drug-modal">
-      <Header
-        title="Add drug"
-        buttonNode={<SquareIconButton />}
-        titleFontSize={20}
-      />
+    <ModalContainer
+      title="Add drug"
+      onSubmit={handleSubmit(onSubmit)}
+      controls={
+        <TextButton
+          text="Add"
+          backgroundColor={color.good_green}
+          width="100%"
+          fontColor={color.white}
+          fontSize={13}
+          fontWeight={700}
+          blank
+        />
+      }
+    >
       <Input
         {...register('drugName', {
           required: { value: true, message: 'Drug name is required' },
         })}
         label="Drug name"
         type={'text'}
+        fillContainer
       />
-      <div className="pick-container">
-        <Input
-          {...register('duration')}
-          label="Duration"
-          fillContainer
-          type={{ type: 'numeric', min: 1, step: 1, unit: 'Day' }}
-        />
-        <Input
-          {...register('qts')}
-          label="Qts"
-          fillContainer
-          type={{ type: 'numeric', min: 1, step: 1, unit: '' }}
-        />
-      </div>
-      <Input {...register('comment', {})} label="Comment" type={'text'} />
-      <TextButton
-        text="Add"
-        backgroundColor={color.good_green}
-        width="100%"
-        fontColor={color.white}
-        fontSize={13}
-        fontWeight={700}
-        onPress={() => {
-          handleSubmit(onSubmit)();
-        }}
+      <Input
+        {...register('duration')}
+        label="Duration"
+        type={{ type: 'numeric', min: 1, step: 1, unit: 'Day' }}
+        inputFill
       />
-    </form>
+      <Input
+        {...register('qts')}
+        label="Qts"
+        type={{ type: 'numeric', min: 1, step: 1, unit: '' }}
+        inputFill
+      />
+      <Input
+        {...register('comment', {})}
+        label="Comment"
+        type={'text'}
+        fillContainer
+      />
+    </ModalContainer>
   );
 }
