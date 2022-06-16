60e795c190dcbc4aab6272c0d6ed20010d065179
Danzo7 <aymenmiro@gmail.com>
2022-06-16 23:07:50 +0100

    💄 Use inputContainer

--- src/renderer/ui/containers/modals/diagnosis_modal/DiagnosisModal.tsx
+++ src/renderer/ui/containers/modals/diagnosis_modal/DiagnosisModal.tsx
@@ -1,9 +1,8 @@
 import colors from '@assets/styles/color';
-import SquareIconButton from '@components/buttons/square_icon_button';
 import TextButton from '@components/buttons/text_button';
-import Header from '@components/header';
 import Input from '@components/inputs/input';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import ModalContainer from '@components/modal_container';
 import './style/index.scss';
 
 interface Inputs {
@@ -30,32 +29,33 @@ export default function DiagnosisModal({
   const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
   console.log(errors);
   return (
-    <div className="diagnosis-modal">
-      <Header title="Diagnosis" />
-      <div className="diagnosis-inputs-container">
-        {inputArray.map(({ label, type }, index) => (
-          <div className="input-width" key={index}>
-            <Input
-              type={type}
-              label={label}
-              {...register(label, {
-                required: { value: true, message: `${label} is required` },
-              })}
-              errorMsg={errors[label]?.message}
-            />
-          </div>
-        ))}
-      </div>
-      <TextButton
-        text="Add to queue"
-        backgroundColor={colors.good_green}
-        radius={7}
-        fontSize={14}
-        width={'60%'}
-        onPress={() => {
-          handleSubmit(onSubmit)();
-        }}
-      />
-    </div>
+    <ModalContainer
+      title="Diagnosis"
+      onSubmit={handleSubmit(onSubmit)}
+      controls={
+        <>
+          <TextButton
+            text="Add to queue"
+            backgroundColor={colors.good_green}
+            radius={7}
+            fontSize={14}
+            width={'60%'}
+            blank
+          />
+        </>
+      }
+    >
+      {inputArray.map(({ label, type }, index) => (
+        <Input
+          key={index}
+          type={type}
+          label={label}
+          {...register(label, {
+            required: { value: true, message: `${label} is required` },
+          })}
+          errorMsg={errors[label]?.message}
+        />
+      ))}
+    </ModalContainer>
   );
 }
