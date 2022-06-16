e2320646666ee3645d16d04e549180daf0e6ad8c
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-16 22:33:55 +0100

    ✅ update SmallRolePreview

--- src/renderer/ui/components/members_preview/small_role_preview/SmallRolePreview.tsx
+++ src/renderer/ui/components/members_preview/small_role_preview/SmallRolePreview.tsx
@@ -1,12 +1,15 @@
 import './style/index.scss';
+import Close from 'toSvg/x_mark.svg?icon';
 interface SmallRolePreviewProps {
   roleName: string;
+  onClick?: () => void;
 }
 
-function SmallRolePreview({ roleName }: SmallRolePreviewProps) {
+function SmallRolePreview({ roleName, onClick }: SmallRolePreviewProps) {
   return (
-    <div className="small-roll">
+    <div onClick={onClick} className="small-roll">
       <span>{roleName}</span>
+      <Close css={{ width: 7, height: 7 }} />
     </div>
   );
 }
