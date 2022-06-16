37090828b671c1a6a7ae9403654dc9c32387f673
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-16 19:59:26 +0100

    💄 AddSearchToBooked

---
+++ src/renderer/ui/containers/modals/add_search_to_booked/AddSearchToBooked.stories.tsx
@@ -0,0 +1,12 @@
+import { ComponentProps } from 'react';
+import type { Story } from '@storybook/react';
+import AddSearchToBooked from './AddSearchToBooked';
+export default {
+  title: 'Modals/AddSearchToBooked',
+  component: AddSearchToBooked,
+};
+const Template: Story<ComponentProps<typeof AddSearchToBooked>> = (args) => (
+  <AddSearchToBooked {...args} />
+);
+export const FirstStory = Template.bind({});
+FirstStory.args = {};
