58cfb800f60f5d5c81c5f915caf87b944604784b
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-17 21:48:33 +0100

    ⚡️ implemnte MemberFooter controls

--- src/renderer/ui/components/members_preview/member_footer/MemberFooter.tsx
+++ src/renderer/ui/components/members_preview/member_footer/MemberFooter.tsx
@@ -4,11 +4,17 @@ import colors from '@assets/styles/color';
 import IdCard from 'toSvg/id_card.svg?icon';
 import Messages from 'toSvg/messages_small.svg?icon';
 import Call_Icon from 'toSvg/phone.svg?icon';
+import { useOverlay } from '@libs/overlay/useOverlay';
+import MemberBigCard from '@containers/modals/member_big_card';
+import profile from '@assets/pictures/test.png';
+import useNavigation from '@libs/hooks/useNavigation';
 interface MemberFooterProps {
   memberID: string;
   status: string;
 }
 function MemberFooter({ memberID, status }: MemberFooterProps) {
+  const { open } = useOverlay();
+  const { navigate } = useNavigation();
   return (
     <div className="member-footer">
       <div className="member-container">
@@ -25,18 +31,48 @@ function MemberFooter({ memberID, status }: MemberFooterProps) {
           afterBgColor={colors.secondary_color}
           width={40}
           iconSize={15}
+          onPress={(e) => {
+            open(
+              <MemberBigCard
+                fullName="Aymen Daouadji"
+                age={23}
+                AddedBy="Brahim Aymen"
+                Address="Blida"
+                JoinDate="02/02/1999"
+                PhoneNumber="051549726"
+                id="123456789"
+                imgSrc={profile}
+                gender="Men"
+                status={true}
+                roleArray={['Gamer', 'Cool', 'Assistant']}
+              />,
+              {
+                closeOnClickOutside: true,
+                clickThrough: false,
+                width: '30%',
+                closeOnBlur: true,
+                popperTarget: e?.currentTarget,
+              },
+            );
+          }}
         />
         <IconicButton
           Icon={Messages}
           afterBgColor={colors.secondary_color}
           width={40}
           iconSize={15}
+          onPress={() => {
+            navigate(`messages/@clinic/${memberID}`);
+          }}
         />
         <IconicButton
           Icon={Call_Icon}
           afterBgColor={colors.good_green}
           width={40}
           iconSize={15}
+          onPress={() => {
+            //TODO? open(callModel)}//
+          }}
         />
       </div>
     </div>
