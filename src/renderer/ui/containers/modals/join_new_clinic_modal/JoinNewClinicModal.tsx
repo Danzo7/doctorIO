4bf81948c3c1e2b4723696b53708c5edaf07feb2
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-17 17:40:11 +0100

    ✅ update JoinNewClinicModal

--- src/renderer/ui/containers/modals/join_new_clinic_modal/JoinNewClinicModal.tsx
+++ src/renderer/ui/containers/modals/join_new_clinic_modal/JoinNewClinicModal.tsx
@@ -1,28 +1,30 @@
-import SquareIconButton from '@components/buttons/square_icon_button/SquareIconButton';
-import Header from '@components/header';
 import Input from '@components/inputs/input';
-import './style/index.scss';
 import Key from 'toSvg/key.svg?icon';
 import TextButton from '@components/buttons/text_button';
 import { color } from '@assets/styles/color';
+import ModalContainer from '@components/modal_container';
+
 interface JoinNewClinicModalProps {}
 export default function JoinNewClinicModal({}: JoinNewClinicModalProps) {
   return (
-    <div className="join-new-clinic-modal">
-      <Header title="Join a new clinic" buttonNode={<SquareIconButton />} />
+    <ModalContainer
+      title="Join a new clinic"
+      controls={
+        <TextButton
+          text="Validate"
+          backgroundColor={color.cold_blue}
+          fontSize={13}
+          fontWeight={700}
+          alignSelf="center"
+          padding={5}
+        />
+      }
+    >
       <Input
         leading={<Key />}
         type="text"
         hint="The invite key should be provided by a clinic member"
       />
-      <TextButton
-        text="Validate"
-        backgroundColor={color.cold_blue}
-        fontSize={13}
-        fontWeight={700}
-        alignSelf="center"
-        padding={5}
-      />
-    </div>
+    </ModalContainer>
   );
 }
