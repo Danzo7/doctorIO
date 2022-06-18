edce764a6b7c62c9c7241c2f9d5d4cb900a09250
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-18 19:54:41 +0100

    ✨ open AddSearchToBooked function on BookedAppointmentPanel

--- src/renderer/ui/components/booked_appointment_panel/BookedAppointmentPanel.tsx
+++ src/renderer/ui/components/booked_appointment_panel/BookedAppointmentPanel.tsx
@@ -1,4 +1,7 @@
+import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
 import PreviewList from '@components/preview_list';
+import AddSearchToBooked from '@containers/modals/add_search_to_booked';
+import { useOverlay } from '@libs/overlay/useOverlay';
 import BookedAppointmentItem from './booked_appointment_item';
 interface BookedAppointmentPanelProps {
   bookedAppointmentData: any[];
@@ -6,8 +9,27 @@ interface BookedAppointmentPanelProps {
 export default function BookedAppointmentPanel({
   bookedAppointmentData = [],
 }: BookedAppointmentPanelProps) {
+  const { open } = useOverlay();
   return (
-    <PreviewList title="Booked Appointment" noBorder maxHeight={400}>
+    <PreviewList
+      title="Booked Appointment"
+      buttonNode={
+        <DarkLightCornerButton
+          title="Add"
+          onPress={() => {
+            open(<AddSearchToBooked />, {
+              closeOnClickOutside: true,
+              isDimmed: true,
+              clickThrough: false,
+              width: '30%',
+              closeBtn: 'inner',
+            });
+          }}
+        />
+      }
+      noBorder
+      maxHeight={400}
+    >
       {bookedAppointmentData.map(
         ({ BookedByFullName, patientFullName, BookedInDate }) => (
           <BookedAppointmentItem
