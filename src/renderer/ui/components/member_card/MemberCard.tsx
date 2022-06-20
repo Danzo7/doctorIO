5d8d27678d32ba9bfeb0d3d89817bc829feb2c28
Danzo7 <aymenmiro@gmail.com>
2022-06-20 20:22:09 +0100

    🎨 remove RoleList and improve styling

--- src/renderer/ui/components/member_card/MemberCard.tsx
+++ src/renderer/ui/components/member_card/MemberCard.tsx
@@ -1,32 +1,17 @@
 import UserProfileStatus from '@components/user_profile_status';
 import './style/index.scss';
 import MemberActionControls from '@components/member_action_controls';
-import SmallRoleList from '@components/members_preview/small_role_list';
 
 interface MemberCardProps {
   fullName: string;
   imgSrc: string;
   roleArray: string[];
 }
-export default function MemberCard({
-  fullName,
-  imgSrc,
-  roleArray = [],
-}: MemberCardProps) {
+export default function MemberCard({ fullName, imgSrc }: MemberCardProps) {
   return (
     <div className="member-card">
-      <div className="member-card-info">
-        <UserProfileStatus
-          imgSrc={imgSrc}
-          status
-          width={60}
-          avatarRadius={17}
-        />
-        <div className="member-card-roles">
-          <span>{fullName}</span>
-          <SmallRoleList roleList={roleArray} />
-        </div>
-      </div>
+      <UserProfileStatus imgSrc={imgSrc} status width={60} avatarRadius={17} />
+      <span>{fullName}</span>
       <div className="member-card-controls">
         <MemberActionControls memberID="123456789" />
       </div>
