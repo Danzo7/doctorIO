43aa1778dffb107b6e664f8dac7c9b0e3eb173e6
Danzo7 <aymenmiro@gmail.com>
2022-06-21 00:21:10 +0100

    ✨ Add drag&drop roles(won't work with strictMode enable)

--- src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/roles_tab/role_list/RoleList.tsx
+++ src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/roles_tab/role_list/RoleList.tsx
@@ -2,6 +2,7 @@ import NewRole from './new_role';
 import RoleItem from './role_item';
 import { useState } from 'react';
 import './style/index.scss';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 interface RoleType {
   roleName: string;
@@ -18,22 +19,56 @@ export default function RoleList({
   defaultSelected = 0,
 }: RoleListProps) {
   const [selectedRole, setSelectedRole] = useState(defaultSelected);
+  const [itemList, setItemList] = useState(roleList);
+
+  const handleDrop = (droppedItem: any) => {
+    // Ignore drop outside droppable container
+    if (!droppedItem.destination) return;
+    const updatedList = [...itemList];
+    // Remove dragged item
+    const [reorderedItem] = updatedList.splice(droppedItem.source.index, 1);
+    // Add dropped item
+    updatedList.splice(droppedItem.destination.index, 0, reorderedItem);
+    // Update State
+    setItemList(updatedList);
+  };
   return (
-    <div className={`role-list `}>
+    <div className={`role-list`}>
       <NewRole />
-      <div className="content-list">
-        {roleList.map(({ roleName, linked }, index) => (
-          <RoleItem
-            onClick={() => {
-              setSelectedRole(index);
-            }}
-            roleName={roleName}
-            selected={index == selectedRole}
-            linked={linked}
-            key={index}
-          />
-        ))}
-      </div>
+      <DragDropContext onDragEnd={handleDrop}>
+        <Droppable droppableId="content-list">
+          {({ droppableProps, innerRef, placeholder }) => (
+            <div className="content-list" {...droppableProps} ref={innerRef}>
+              {itemList.map((item, index) => (
+                <Draggable
+                  key={item.roleName + index}
+                  draggableId={item.roleName + index}
+                  index={index}
+                >
+                  {(props) => (
+                    <div
+                      ref={props.innerRef}
+                      {...props.dragHandleProps}
+                      {...props.draggableProps}
+                    >
+                      <RoleItem
+                        onClick={() => {
+                          setSelectedRole(index);
+                        }}
+                        roleName={item.roleName}
+                        selected={index == selectedRole}
+                        linked={item.linked}
+                        key={index}
+                      />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
