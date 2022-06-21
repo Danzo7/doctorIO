f44567355d4b28642dc85d7f1743b4b4bf14e908
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-22 00:31:49 +0100

    ✨ open addDrugModal and implemnte add function

--- src/renderer/ui/layers/medical_session/pages/prescription_tab/PrescriptionTab.tsx
+++ src/renderer/ui/layers/medical_session/pages/prescription_tab/PrescriptionTab.tsx
@@ -1,17 +1,101 @@
 import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
 import Header from '@components/header';
+import AddDrugModal from '@containers/modals/add_drug_modal';
+import { useOverlay } from '@libs/overlay/useOverlay';
+import { useState } from 'react';
 import MedicamentTable from './medicament_table';
-
 import './style/index.scss';
+
+const dataList = [
+  {
+    drugName: 'aymen',
+    qts: 15,
+    dose: 3,
+    duration: '5 days',
+    comment: 'dont die',
+    id: 1,
+  },
+  {
+    drugName: 'aymen',
+    qts: 10,
+    dose: 3,
+    duration: '5 days',
+    comment: 'dont die',
+    id: 2,
+  },
+  {
+    drugName: 'aymen',
+    qts: 5,
+    dose: 3,
+    duration: '5 days',
+    comment: 'dont die',
+    id: 3,
+  },
+  {
+    drugName: 'aymen',
+    qts: 5,
+    dose: 3,
+    duration: '5 days',
+    comment: 'dont die',
+    id: 3,
+  },
+  {
+    drugName: 'aymen',
+    qts: 5,
+    dose: 3,
+    duration: '5 days',
+    comment: 'dont die',
+    id: 3,
+  },
+];
+
 interface PrescriptionTabProps {}
 export default function PrescriptionTab({}: PrescriptionTabProps) {
+  const { open } = useOverlay();
+  const [data, setData] = useState(dataList);
+
   return (
     <div className="prescription-tab">
       <Header
         title="Drug list"
-        buttonNode={<DarkLightCornerButton title="Add..." />}
+        buttonNode={
+          <DarkLightCornerButton
+            title="Add..."
+            onPress={() => {
+              open(
+                <AddDrugModal
+                  onAdd={(formData) => {
+                    console.log(formData);
+                    setData((prev) => {
+                      return [
+                        ...prev,
+                        {
+                          drugName: formData.drugName,
+                          qts: formData.qts,
+                          dose: 1, //TODO  we need to add input field to Add Drug for dose value
+                          comment: formData.comment,
+                          duration: formData.duration,
+                          id: Math.random() * 10,
+                        },
+                      ];
+                    });
+                    console.log(data);
+                  }}
+                />,
+                {
+                  closeOnClickOutside: true,
+                  isDimmed: true,
+                  clickThrough: false,
+                  position: { top: '30%' },
+                  width: '30%',
+                  closeBtn: 'inner',
+                },
+              );
+            }}
+          />
+        }
       />
-      <MedicamentTable editable />
+      <MedicamentTable editable dataList={data} />
     </div>
   );
 }
