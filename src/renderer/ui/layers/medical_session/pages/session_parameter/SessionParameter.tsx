346f34bdff5566d1035795df6c3a16e0f96174e8
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-21 20:06:29 +0100

    ✨ implement bottom controls of SessionParameter

--- src/renderer/ui/layers/medical_session/pages/session_parameter/SessionParameter.tsx
+++ src/renderer/ui/layers/medical_session/pages/session_parameter/SessionParameter.tsx
@@ -6,12 +6,24 @@ import Header from '@components/header';
 import Datepicker from '@components/inputs/datepicker';
 import Input from '@components/inputs/input';
 import MultipleCheckGroup from '@components/inputs/multiple_check_group';
+import EndSession from '@containers/modals/end_session';
+import { useOverlay } from '@libs/overlay/useOverlay';
 import { useState } from 'react';
 import './style/index.scss';
 interface SessionParameterProps {}
 export default function SessionParameter({}: SessionParameterProps) {
   const [selectedDate, setSelectedDate] = useState(new Date());
-
+  const { open, openTooltip } = useOverlay();
+  const openEndSessionModal = () => {
+    open(<EndSession />, {
+      closeOnClickOutside: true,
+      isDimmed: true,
+      clickThrough: false,
+      position: { top: '30%' },
+      width: '30%',
+      closeBtn: 'inner',
+    });
+  };
   const onDateChange = (date: Date) => {
     setSelectedDate(date);
   };
@@ -43,11 +55,32 @@ export default function SessionParameter({}: SessionParameterProps) {
       <Input type={'checkbox'} label="hand payment to assistant" />
       <div className="bottom-div">
         <div className="controls-div">
-          <DarkLightCornerButton title="Print..." isActive={true} />
+          <DarkLightCornerButton
+            title="Print..."
+            onPress={(e) => {
+              if (e)
+                openTooltip(
+                  [
+                    {
+                      text: 'Notice',
+                    },
+                    {
+                      text: 'Prescription',
+                    },
+                    {
+                      text: 'Both',
+                    },
+                  ],
+                  e?.currentTarget,
+                  true,
+                );
+            }}
+          />
           <TextButton
             text="Finish"
             backgroundColor={color.good_green}
             width={170}
+            onPress={openEndSessionModal}
           />
         </div>
       </div>
