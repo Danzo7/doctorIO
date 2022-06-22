e44b5d0a6bce0ff3b7849c0d3f649335f124242f
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-22 19:58:17 +0100

    ✨ open SessionPreviewModal on AppointmentHistoryItem

--- src/renderer/ui/components/appointment_history_panel/appointment_history_item/AppointmentHistoryItem.tsx
+++ src/renderer/ui/components/appointment_history_panel/appointment_history_item/AppointmentHistoryItem.tsx
@@ -1,22 +1,34 @@
 import SquareIconButton from '@components/buttons/square_icon_button/SquareIconButton';
 import PreviewWithControls from '@components/preview_with_controls';
+import SessionPreviewModal from '@containers/modals/session_preview_modal';
+import { useOverlay } from '@libs/overlay/useOverlay';
 import AppointmentHistoryIcon from 'toSvg/appointment_history.svg?icon';
 interface AppointmentHistoryItemProps {
   appointmentDate: string;
   appointmentDescription: string;
-  onPressHistory: () => void;
 }
 export default function AppointmentHistoryItem({
   appointmentDate,
   appointmentDescription,
-  onPressHistory,
 }: AppointmentHistoryItemProps) {
+  const { open } = useOverlay();
   return (
     <PreviewWithControls
       primaryText={appointmentDate}
       secondaryText={appointmentDescription}
     >
-      <SquareIconButton svg={AppointmentHistoryIcon} onPress={onPressHistory} />
+      <SquareIconButton
+        svg={AppointmentHistoryIcon}
+        onPress={() => {
+          open(<SessionPreviewModal />, {
+            closeOnClickOutside: true,
+            isDimmed: true,
+            clickThrough: false,
+            closeBtn: 'inner',
+            width: '30%',
+          });
+        }}
+      />
     </PreviewWithControls>
   );
 }
