4ff3655f0e1922feb204a250dcd45b9758f516ea
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-22 19:56:13 +0100

    🐛  make owner role props optional

--- src/renderer/ui/components/queue_controls/QueueControls.tsx
+++ src/renderer/ui/components/queue_controls/QueueControls.tsx
@@ -19,7 +19,7 @@ interface AssistanceRoleProps {
 }
 interface QueueControlsProps {
   role:
-    | { roleName: 'owner'; roleProps: OwnerRoleProps }
+    | { roleName: 'owner'; roleProps?: OwnerRoleProps }
     | { roleName: 'assistance'; roleProps?: AssistanceRoleProps };
 }
 export default function QueueControls({ role }: QueueControlsProps) {
@@ -49,7 +49,7 @@ export default function QueueControls({ role }: QueueControlsProps) {
                   closeBtn: 'inner',
                 },
               );
-              if (role.roleProps.onCallNext) role.roleProps?.onCallNext();
+              if (role.roleProps?.onCallNext) role.roleProps?.onCallNext();
             }}
           />
           <IconicButton
@@ -80,7 +80,7 @@ export default function QueueControls({ role }: QueueControlsProps) {
                   closeBtn: 'inner',
                 },
               );
-              if (role.roleProps.onPause) role.roleProps?.onPause();
+              if (role.roleProps?.onPause) role.roleProps?.onPause();
             }}
           />
         </div>
