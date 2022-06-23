936995bfd3d588795da5224c063d1fde8803c794
Danzo7 <aymenmiro@gmail.com>
2022-06-23 03:01:53 +0100

    ⚡️ add date format constant

---
+++ src/renderer/utils/constants/data_format.ts
@@ -0,0 +1,5 @@
+const DATE_ONLY = 'dd MMM yyyy';
+const DATE_NO_YEAR = 'EEEE, dd MMM';
+const TIME_ONLY = 'h:mm aa';
+const DATE_TIME = 'dd MMM yyyy, h:mm aa';
+export { DATE_ONLY, DATE_NO_YEAR, DATE_TIME, TIME_ONLY };
