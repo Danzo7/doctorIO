a576a55fc442305d16905e4cd1adc9f0fd13be79
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-24 00:45:17 +0100

    ➕ add more fake data

--- src/renderer/API/fake.ts
+++ src/renderer/API/fake.ts
@@ -1,4 +1,5 @@
 import { User } from '@models/local.models';
+import { Member } from '@models/server.models';
 
 const firstUser: User = {
   avatar: 'build/renderer/assets/9b4caf44c40506a102ec.png',
@@ -27,4 +28,21 @@ const firstUser: User = {
   userId: 2123,
   username: '',
 };
-export { firstUser };
+
+const member: Member = {
+  name: 'Aymen Daouadji',
+  avatar: 'build/renderer/assets/9b4caf44c40506a102ec.png',
+  memberStatus: true,
+  accessKey: '12346678',
+  addedBy: 'Brahim aymen',
+  age: 18,
+  gender: 'Men',
+  address: 'blida',
+  userId: 12346789,
+  phoneNumber: '054681349',
+  memberId: 123456789,
+  roles: [{ roleId: 1, roleName: 'gamer', roleDesc: 'gaming' }],
+  joinDate: new Date('2022-01-01'),
+};
+export default firstUser;
+export { firstUser, member };
