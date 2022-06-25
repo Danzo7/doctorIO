daf9776e9eba222fe1af86ebffb4eddb4451d6c3
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-25 22:52:36 +0100

    📝 declare PERMISSIONS

--- src/renderer/utils/constants/permissions.ts
+++ src/renderer/utils/constants/permissions.ts
@@ -6,9 +6,75 @@ export const PERMISSIONS: Permission[] = [
     name: 'Administrator',
     description: 'Can manage all aspects of the application',
   },
+
   {
     permKey: 'canAddMember',
     name: 'Add Member',
-    description: 'Can add new members to the application',
+    description: 'Can add new members to the clinic',
+  },
+  {
+    permKey: 'canManageRole',
+    name: 'Manage Roles',
+    description: 'Can manage clinic roles',
+  },
+  {
+    permKey: 'canAccessPatientsList',
+    name: 'Access patients list',
+    description: 'Can access patients list',
+  },
+  {
+    permKey: 'canAddPatients',
+    name: 'Add patients',
+    description: 'Can add patients ',
+  },
+  {
+    permKey: 'canManagePatients',
+    name: 'Manage patients',
+    description: 'Can manage patients ',
+  },
+  {
+    permKey: 'canHaveQueue',
+    name: 'Have a Queue',
+    description: 'Can Have a Queue list ',
+  },
+  {
+    permKey: 'canAddDrugs',
+    name: 'Add drugs',
+    description: 'can Add a drug to the drugs list ',
+  },
+  {
+    permKey: 'canViewClinicInsight',
+    name: 'View clinic insight',
+    description: 'can view clinic insight and analyses',
+  },
+  {
+    permKey: 'canManageDataCollection',
+    name: 'Manage Data Collections',
+    description: 'can Manage Data Collections',
+  },
+  {
+    permKey: 'canViewMedicalRecords',
+    name: 'View medical records',
+    description: 'can view medical records of patients',
+  },
+  {
+    permKey: 'canManageMembers',
+    name: 'Manage members',
+    description: 'can manage all the members of the clinic',
+  },
+  {
+    permKey: 'canRemoveMember',
+    name: 'Remove member',
+    description: 'can remove a selected member from the clinic ',
+  },
+  {
+    permKey: 'canManageClinic',
+    name: 'Manage clinic',
+    description: 'can manage the setting of the clinic',
+  },
+  {
+    permKey: 'canUseMessages',
+    name: 'Use messages',
+    description: 'allow member to send and receive messages ',
   },
 ];
