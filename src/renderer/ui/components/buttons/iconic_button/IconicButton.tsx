f1657240126549aea80604e721ff09fbf49a625f
Danzo7 <aymenmiro@gmail.com>
2022-06-26 17:34:15 +0100

    🎨 Match TextButton props

--- src/renderer/ui/components/buttons/iconic_button/IconicButton.tsx
+++ src/renderer/ui/components/buttons/iconic_button/IconicButton.tsx
@@ -1,27 +1,13 @@
 import colors from '@assets/styles/color';
-import { FunctionComponent, ReactNode, SVGProps } from 'react';
-import TextButton, { PressHandler } from '../text_button';
+import { ComponentProps, FunctionComponent, ReactNode, SVGProps } from 'react';
+import TextButton from '../text_button';
 
 interface IconicButtonProps {
   Icon: FunctionComponent<SVGProps<SVGSVGElement>> | ReactNode;
-  width?: number | string;
-  height?: number;
-  backgroundColor?: string;
-  afterBgColor?: string;
-  activeBgColor?: string;
-  borderColor?: string;
-  afterBorderColor?: string;
-  activeBorderColor?: string;
   iconColor?: string;
   iconAfterColor?: string;
   iconType?: 'stroke' | 'fill';
-  radius?: string | number;
-  onPress?: PressHandler;
   iconSize?: number | string;
-  padding?: number | string;
-  type?: 'button' | 'submit' | 'reset' | undefined;
-  disabled?: boolean;
-  blank?: true;
 }
 
 /**
@@ -29,26 +15,17 @@ interface IconicButtonProps {
  * @param {number} width -Size of button use ?icon query for dynamic icon size*/
 function IconicButton({
   Icon,
-
-  backgroundColor,
-  borderColor,
   radius = '100%',
-  afterBgColor,
-  afterBorderColor,
   iconColor,
   iconType,
   width = 40,
   height,
-  onPress,
   activeBgColor = colors.darker,
-  type,
-  activeBorderColor,
   iconAfterColor,
   iconSize,
   padding = 'unset',
-  disabled = false,
-  blank,
-}: IconicButtonProps) {
+  ...others
+}: IconicButtonProps & Omit<ComponentProps<typeof TextButton>, 'Icon'>) {
   const castIcon = () => {
     if ((Icon as FunctionComponent)?.prototype) {
       Icon = Icon as FunctionComponent<SVGProps<SVGSVGElement>>;
@@ -64,26 +41,18 @@ function IconicButton({
 
   return (
     <TextButton
-      blank={blank}
-      type={type}
-      backgroundColor={backgroundColor}
-      afterBgColor={afterBgColor}
       width={width}
       height={height ? height : width}
       padding={padding}
       radius={radius}
       activeBgColor={activeBgColor}
-      borderColor={borderColor}
-      activeBorderColor={activeBorderColor}
-      afterBorderColor={afterBorderColor}
-      onPress={onPress}
-      disabled={disabled}
       Icon={{
         svg: castIcon(),
         iconColor: iconColor,
         iconAfterColor: iconAfterColor,
         iconType: iconType,
       }}
+      {...others}
     />
   );
 }
