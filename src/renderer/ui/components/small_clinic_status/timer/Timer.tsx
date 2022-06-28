13bf27341bbd2332ab2a9361cde543e737600473
Danzo7 <aymenmiro@gmail.com>
2022-06-28 22:57:15 +0100

    ✨ Add functionality to Timer

--- src/renderer/ui/components/small_clinic_status/timer/Timer.tsx
+++ src/renderer/ui/components/small_clinic_status/timer/Timer.tsx
@@ -1,5 +1,8 @@
-import { patients } from '@api/fake';
+import { clinic, patients } from '@api/fake';
+import { timeToDate } from '@helpers/date.helper';
+import { differenceInSeconds } from 'date-fns';
 import { useCallback, useEffect, useRef } from 'react';
+import Countdown from 'react-countdown';
 import Clinic from 'toSvg/clinic.svg?icon';
 import { RenderTimer } from './render_timer_canvas';
 import './style/index.scss';
@@ -13,12 +16,20 @@ export default function Timer({ isActive }: TimerProps) {
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const requestIdRef = useRef<number>();
-
+  const closeTimeRef = useRef(timeToDate(clinic.timing.timeToClose));
+  const openTimeRef = useRef(timeToDate(clinic.timing.timeToOpen));
   const TimeRef = useRef({ ratio: 1 });
 
   const renderFrame = useCallback(() => {
-    if (TimeRef.current.ratio > 100) TimeRef.current.ratio = 0;
-    TimeRef.current.ratio += 0.1;
+    if (TimeRef.current.ratio < 100)
+      TimeRef.current.ratio = Number.parseInt(
+        (
+          100 -
+          (differenceInSeconds(closeTimeRef.current, new Date()) /
+            differenceInSeconds(closeTimeRef.current, openTimeRef.current)) *
+            100
+        ).toPrecision(),
+      );
     RenderTimer.call(canvasRef?.current?.getContext('2d'), {
       timeRatio: TimeRef.current.ratio,
       size,
@@ -58,9 +69,14 @@ export default function Timer({ isActive }: TimerProps) {
     >
       <canvas ref={canvasRef} width={size} height={size}></canvas>
       <div className="time-to-close">
-        <Clinic></Clinic>
-        <span>Time to close</span>
-        <span className="count-down">10h:50m</span>
+        <Clinic></Clinic> <span>Time to close</span>
+        <span className="count-down">
+          <Countdown
+            date={timeToDate(clinic.timing.timeToClose)}
+            daysInHours={true}
+            //onComplete={()}end of the day
+          />
+        </span>
       </div>
     </div>
   );
