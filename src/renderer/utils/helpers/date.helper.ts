a23c103e04872a6e1430daaf0d1d535d5ef9e395
Danzo7 <aymenmiro@gmail.com>
2022-06-28 22:56:46 +0100

    👷 add Helper methods

--- src/renderer/utils/helpers/date.helper.ts
+++ src/renderer/utils/helpers/date.helper.ts
@@ -1,10 +1,23 @@
-import { START_OF_TIME } from '@constants/data_format';
+import { format } from 'date-fns';
 
-export const timeToDate = (hour: number, min: number) => {
-  const time = new Date(START_OF_TIME);
-  time.setHours(hour, min);
+export const timeToDate = (props: { hour: number; min: number } | string) => {
+  const time = new Date();
+  time.setHours(0, 0, 0, 0);
+
+  if (typeof props === 'string') {
+    time.setHours(Number(props.split(':')[0]));
+    time.setMinutes(Number(props.split(':')[1]));
+  } else {
+    time.setHours(props.hour);
+    time.setMinutes(props.min);
+  }
   return time;
 };
+
 export const dateToTime = (date: Date) => {
   return date.getHours() + ':' + date.getMinutes();
+};
+export const diffrentInTime = (date1: Date, date2: Date) => {
+  const diff = date2.getTime() - date1.getTime();
+  return format(new Date(diff), "hh' h 'mm' min 'ss' sec'");
 };
