7e26d008c323ec4be9d314a65de5003d68a6e530
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-29 16:32:51 +0100

    ✨ use preferences type

--- src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/preferences_tab/PreferencesTab.tsx
+++ src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/preferences_tab/PreferencesTab.tsx
@@ -1,22 +1,22 @@
 import BorderSeparator from '@components/border_separator';
 import CheckboxTile from '@components/checkbox_tile';
+import { preferences } from '@constants/permissions';
+import { Fragment } from 'react';
 import './style/index.scss';
 interface PreferencesTabProps {}
 export default function PreferencesTab({}: PreferencesTabProps) {
   return (
     <div className="preferences-tab">
-      <CheckboxTile
-        editable
-        primaryText="Enable earning & growth"
-        secondaryText="by enabling this members that can create session or members linked to them will be able to determine a price for thier services"
-      />
-      <BorderSeparator direction="horizontal" />
-      <CheckboxTile
-        editable
-        primaryText="Allow outside messaging"
-        secondaryText="Allow members to connect with outsiders over the internet"
-      />
-      <BorderSeparator direction="horizontal" />
+      {preferences.map(({ name, permKey, description }) => (
+        <Fragment key={permKey}>
+          <CheckboxTile
+            editable
+            primaryText={name}
+            secondaryText={description}
+          />
+          <BorderSeparator direction="horizontal" />
+        </Fragment>
+      ))}
     </div>
   );
 }
