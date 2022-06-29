9740331c6e1684c578f8e48131c87985749fbfbd
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-06-29 17:02:27 +0100

    🎨 improve SnakeBar

--- src/renderer/ui/containers/modals/snake_bar/SnakeBar.tsx
+++ src/renderer/ui/containers/modals/snake_bar/SnakeBar.tsx
@@ -1,17 +1,23 @@
+import color from '@assets/styles/color';
 import { ReactNode } from 'react';
 import './style/index.scss';
 interface SnakeBarProps {
   description: string;
   type?: 'error' | 'info' | 'warning';
   children?: ReactNode;
+  backgroundColor?: string;
 }
 export default function SnakeBar({
   description,
   children,
   type = 'error',
+  backgroundColor = color.lighter_background,
 }: SnakeBarProps) {
   return (
-    <div className={`snake-bar ${type}`}>
+    <div
+      className={`snake-bar ${type}`}
+      css={{ backgroundColor: backgroundColor }}
+    >
       <span>{description}</span>
       {children}
     </div>
