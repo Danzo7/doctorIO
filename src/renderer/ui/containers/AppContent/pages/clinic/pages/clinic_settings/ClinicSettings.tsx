577940579dd78b3ed500705b3e581ce07843bf5d
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-07-03 00:33:39 +0100

    ✨ add WarningModal on disconnect clinic

--- src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/ClinicSettings.tsx
+++ src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/ClinicSettings.tsx
@@ -9,10 +9,14 @@ import MembersTab from './pages/members_tab';
 import TimingAndSchedule from './pages/timing_and_schedule';
 import PreferencesTab from './pages/preferences_tab';
 import AuditLogTab from './pages/audit_log_tab';
+import { useOverlay } from '@libs/overlay/useOverlay';
+import WarningModal from '@containers/modals/warning_modal';
+import TextButton from '@components/buttons/text_button';
+import { FIT_MODAL } from '@libs/overlay';
 interface ClinicSettingsProps {}
 export default function ClinicSettings({}: ClinicSettingsProps) {
   const navigate = useNavigate();
-
+  const { open, close } = useOverlay();
   return (
     <div className="clinic-settings">
       <div className="clinic-settings-header">
@@ -25,6 +29,26 @@ export default function ClinicSettings({}: ClinicSettingsProps) {
           text="disconnect"
           fontColor={colors.hot_red}
           isActive={true}
+          onPress={() => {
+            open(
+              <WarningModal
+                warningTitle="are you sure you want to disconnect ?"
+                warningDescription="you will be disconnected from the clinic after confirming"
+              >
+                <TextButton
+                  text="Confirm"
+                  backgroundColor={colors.good_green}
+                  width="100%"
+                  onPress={() => {
+                    //REDUX change the selected clinic
+                    close();
+                    navigate('/clinic/all');
+                  }}
+                />
+              </WarningModal>,
+              FIT_MODAL,
+            );
+          }}
         />
       </div>
       <NavTabMenu
