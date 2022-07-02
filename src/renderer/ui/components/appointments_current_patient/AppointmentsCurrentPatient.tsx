e5e4282afe6e2241d12707240fcc8d4601f9b640
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-07-03 00:50:27 +0100

    ♻️ refactor AppointmentsCurrentPatient

--- src/renderer/ui/components/appointments_current_patient/AppointmentsCurrentPatient.tsx
+++ src/renderer/ui/components/appointments_current_patient/AppointmentsCurrentPatient.tsx
@@ -1,3 +1,6 @@
+import { color } from '@assets/styles/color';
+import BorderSeparator from '@components/border_separator';
+import TextPair from '@components/text_pair/TextPair';
 import { formatDistance, subDays } from 'date-fns';
 import './style/index.scss';
 interface AppointmentsCurrentPatientProps {
@@ -12,14 +15,37 @@ export default function AppointmentsCurrentPatient({
 }: AppointmentsCurrentPatientProps) {
   return (
     <div className="appointments-current-patient">
-      <div className="patient-info">
-        <span>{patientName}</span>
-        <span>{formatDistance(arrivalTime, new Date())} ago</span>
-      </div>
-      <div className="number-container">
-        <span>Number</span>
-        <span>{position}</span>
-      </div>
+      <TextPair
+        maxWidth={200}
+        first={{
+          text: patientName,
+          fontColor: color.white,
+          fontWeight: '400',
+          fontSize: 17,
+        }}
+        second={{
+          text: formatDistance(arrivalTime, new Date()),
+          fontColor: color.text_gray,
+          fontSize: 10,
+          fontWeight: '400',
+        }}
+      />
+      <BorderSeparator direction="vertical" />
+      <TextPair
+        alignItems="center"
+        first={{
+          text: 'Number',
+          fontColor: color.white,
+          fontWeight: '400',
+          fontSize: 12,
+        }}
+        second={{
+          text: position,
+          fontColor: color.white,
+          fontSize: 21,
+          fontWeight: '400',
+        }}
+      />
     </div>
   );
 }
