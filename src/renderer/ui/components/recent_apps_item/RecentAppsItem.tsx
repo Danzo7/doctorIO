92fde8e17a3b4d9de0b506a9a48ae165c1dac1b6
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-07-02 19:36:22 +0100

    ✅ RecentAppsItem

--- src/renderer/ui/components/recent_apps_item/RecentAppsItem.tsx
+++ src/renderer/ui/components/recent_apps_item/RecentAppsItem.tsx
@@ -4,6 +4,7 @@ import PresentationItem from '@components/presentation_item';
 import { useOverlay } from '@libs/overlay/useOverlay';
 import DiagnosisModal from '@containers/modals/diagnosis_modal';
 import { Patient } from '@models/instance.model';
+import { DEFAULT_MODAL } from '@libs/overlay';
 
 export default function RecentAppsItem({
   firstName,
@@ -13,22 +14,14 @@ export default function RecentAppsItem({
   const { open } = useOverlay();
   return (
     <PresentationItem
-      primaryText={firstName + '' + lastName}
+      primaryText={firstName + ' ' + lastName}
       secondaryText={`# ${patId}`}
     >
       <TextButton
         text="Run diagnosis..."
         backgroundColor={color.cold_blue}
         radius={7}
-        onPress={() =>
-          open(<DiagnosisModal />, {
-            closeOnClickOutside: true,
-            isDimmed: true,
-            clickThrough: false,
-            width: '30%',
-            closeBtn: 'inner',
-          })
-        }
+        onPress={() => open(<DiagnosisModal />, DEFAULT_MODAL)}
       />
       <TextButton text="Add" backgroundColor={color.good_green} radius={7} />
     </PresentationItem>
