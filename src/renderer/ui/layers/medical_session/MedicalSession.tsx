bbe98cc4bd7c6cf8ae1a89d35c3f2e7f91ec8138
Danzo7 <aymenmiro@gmail.com>
2022-07-03 04:08:15 +0100

    💄 Add PatientSmallCard and bottom control

--- src/renderer/ui/layers/medical_session/MedicalSession.tsx
+++ src/renderer/ui/layers/medical_session/MedicalSession.tsx
@@ -1,7 +1,14 @@
 import { patients } from '@api/fake';
+import color from '@assets/styles/color';
 import BorderSeparator from '@components/border_separator';
-import { PatientSpecificsCard } from '@components/patient_card';
+import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
+import TextButton from '@components/buttons/text_button';
+import Header from '@components/header';
 import TabMenu from '@components/tab_menu';
+import EndSession from '@containers/modals/end_session';
+import { useOverlay } from '@libs/overlay/useOverlay';
+import { Patient } from '@models/instance.model';
+import PatientSmallCard from './components/patient_small_card';
 import MedicalSessionSideBar from './medical_session_side_bar';
 import NoticeTab from './pages/notice_tab';
 import PrescriptionTab from './pages/prescription_tab';
@@ -10,22 +17,36 @@ import './style/index.scss';
 interface MedicalSessionProps {
   patId: number;
 }
-//REDUX:search patient by id
 
 export default function MedicalSession({ patId }: MedicalSessionProps) {
-  const patient = patients.find((pat) => pat.patId == patId);
+  const patient = patients.find((pat) => pat.patId == patId) as Patient; //REDUX:get patient by id
+  const { open, openTooltip } = useOverlay();
+  const openEndSessionModal = () => {
+    open(<EndSession />, {
+      closeOnClickOutside: true,
+      isDimmed: true,
+      clickThrough: false,
+      position: { top: '30%' },
+      width: '30%',
+      closeBtn: 'inner',
+    });
+  };
   return (
     <div className="medical-session">
-      <MedicalSessionSideBar patient={patient!} />
+      <MedicalSessionSideBar patient={patient} />
       <div className="content-container">
-        <span>Session</span>
-        <PatientSpecificsCard
-          data={{
-            'first name': patient?.firstName,
-            'last name': patient?.lastName,
-            id: patient?.patId,
-            age: patient?.age,
-          }}
+        <Header
+          title="Session"
+          titleFontSize={20}
+          titleFontWeight={600}
+          buttonNode={
+            <PatientSmallCard
+              age={patient.age}
+              firstName={patient.firstName}
+              lastName={patient.lastName}
+              patId={patient.patId}
+            />
+          }
         />
         <TabMenu items={['prescription', 'notice']}>
           <PrescriptionTab />
@@ -33,6 +54,35 @@ export default function MedicalSession({ patId }: MedicalSessionProps) {
         </TabMenu>
         <BorderSeparator direction="horizontal" />
         <SessionParameter />
+        <div className="controls-div">
+          <DarkLightCornerButton
+            text="Print..."
+            onPress={(e) => {
+              if (e)
+                openTooltip(
+                  [
+                    {
+                      text: 'Notice',
+                    },
+                    {
+                      text: 'Prescription',
+                    },
+                    {
+                      text: 'Both',
+                    },
+                  ],
+                  e?.currentTarget,
+                  true,
+                );
+            }}
+          />
+          <TextButton
+            text="Finish"
+            backgroundColor={color.good_green}
+            width={170}
+            onPress={openEndSessionModal}
+          />
+        </div>
       </div>
     </div>
   );
