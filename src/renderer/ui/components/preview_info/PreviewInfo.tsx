40665ec3bb3910a7bb5486e69e27ea0e73af8483
Danzo7 <aymenmiro@gmail.com>
2022-07-03 04:50:44 +0100

    🐛 convert keys from camelCase to sentence

--- src/renderer/ui/components/preview_info/PreviewInfo.tsx
+++ src/renderer/ui/components/preview_info/PreviewInfo.tsx
@@ -6,17 +6,28 @@ interface PreviewInfoProps {
   buttonNode?: ReactNode;
   data: { [key: string]: any };
 }
+
 export default function PreviewInfo({
   title,
   buttonNode,
   data,
 }: PreviewInfoProps) {
+  const sentenceCase = (val: string) =>
+    val
+      .replace(/([A-Z])/g, ' $1')
+      .split(' ')
+      .map((str, index) =>
+        index == 0
+          ? str.replace(str.charAt(0), str.charAt(0).toUpperCase())
+          : str.toLowerCase(),
+      )
+      .join(' ');
   return (
     <PreviewList title={title} buttonNode={buttonNode} notScrollable>
       <div className="preview-list-container">
         {Object.entries(data).map(([key, value]) => (
           <div className="preview-list-row" key={key}>
-            <span>{key}</span>
+            <span>{sentenceCase(key)}</span>
             <span>{value}</span>
           </div>
         ))}
