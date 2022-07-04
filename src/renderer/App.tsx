19b7b417a809f51b36d2a02d988d1611ada8a588
Danzo7 <aymenmiro@gmail.com>
2022-07-04 18:29:20 +0100

    🚧 rely on user config to render main app

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,30 +1,35 @@
 import './App.scss';
-import AppMenu from '@containers/AppMenu';
-import AppContent from '@containers/AppContent';
-import AppSidebar from '@containers/AppSidebar';
 import { OverlayContainer } from '@libs/overlay';
 import { Route, Routes } from 'react-router-dom';
 import MedicalSession from '@layers/medical_session';
+import MainLayer from '@layers/main_layer';
+import { firstUser } from '@api/fake';
+import Clinic from '@containers/AppContent/pages/clinic';
 
 interface AppProps {}
 
 function App({}: AppProps) {
+  const user = firstUser; //REDUX load user
+
   return (
     <>
       <div className="app-container">
-        <Routes>
-          <Route path="session" element={<MedicalSession patId={1} />} />
-          <Route
-            path="*"
-            element={
-              <>
-                <AppMenu />
-                <AppContent />
-                <AppSidebar />
-              </>
-            }
-          />
-        </Routes>
+        {
+          user ? (
+            user.selectedClinic ? ( //FEATURE try to connect to selected clinic
+              <Routes>
+                <Route path="session" element={<MedicalSession patId={1} />} />
+                <Route path="*" element={<MainLayer />} />
+              </Routes>
+            ) : (
+              <Clinic />
+            )
+          ) : (
+            <div>
+              <h1>It seems like this is your first time using this app</h1>
+            </div>
+          ) //UI FOR FIRST TIME USER
+        }
       </div>
       <OverlayContainer />
     </>
--- src/renderer/ui/containers/AppContent/pages/clinic/Clinic.tsx
+++ src/renderer/ui/containers/AppContent/pages/clinic/Clinic.tsx
@@ -1,12 +1,17 @@
+import { firstUser } from '@api/fake';
 import { Routes, Route } from 'react-router-dom';
 import Clinics from './pages/clinics';
 import ClinicSettings from './pages/clinic_settings';
 interface ClinicProps {}
 export default function Clinic({}: ClinicProps) {
-  return (
+  const { selectedClinic } = firstUser; //REDUX get local data
+
+  return selectedClinic ? (
     <Routes>
       <Route path="all" element={<Clinics />} />
       <Route path="/*" element={<ClinicSettings />} />
     </Routes>
+  ) : (
+    <Clinics />
   );
 }
