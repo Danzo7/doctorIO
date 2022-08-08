42c5400d24c82cc2ea8a46b5bf724a1967155a42
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-08 22:33:02 +0100

    ✨dispatch addToQueue

--- src/renderer/ui/components/recent_apps_item/RecentAppsItem.tsx
+++ src/renderer/ui/components/recent_apps_item/RecentAppsItem.tsx
@@ -3,14 +3,18 @@ import { color } from '@colors';
 import PresentationItem from '@components/presentation_item';
 import { useOverlay } from '@libs/overlay/useOverlay';
 import DiagnosisModal from '@containers/modals/diagnosis_modal';
-import { Patient } from '@models/instance.model';
-import { DEFAULT_MODAL } from '@libs/overlay';
+import { AppointmentQueueItem, Patient } from '@models/instance.model';
+import { DEFAULT_MODAL, Overlay } from '@libs/overlay';
+import { useAppDispatch, useAppSelector } from '@store';
+import { addToQueue } from '@redux/instance/appointmentQueue/appointmentQueueSlice';
 
 export default function RecentAppsItem({
   firstName,
   lastName,
   patId,
 }: Pick<Patient, 'firstName' | 'lastName' | 'patId'>) {
+  const { appointments } = useAppSelector((state) => state.appointmentQueue);
+  const dispatch = useAppDispatch();
   const { open } = useOverlay();
   return (
     <PresentationItem
@@ -23,8 +27,22 @@ export default function RecentAppsItem({
         radius={7}
         onPress={() => open(<DiagnosisModal />, DEFAULT_MODAL)}
       />
-      <TextButton text="Add" backgroundColor={color.good_green} radius={7} />
+      <TextButton
+        text="Add"
+        backgroundColor={color.good_green}
+        radius={7}
+        onPress={() => {
+          //REDUX fetch patient info instead
+          const newApp: AppointmentQueueItem = {
+            patientId: patId,
+            patientName: firstName + ' ' + lastName,
+            date: new Date(),
+            position: appointments[appointments.length - 1].position + 1,
+          };
+          dispatch(addToQueue(newApp));
+          Overlay.close();
+        }}
+      />
     </PresentationItem>
-    //REDUX add patient to Queue
   );
 }
