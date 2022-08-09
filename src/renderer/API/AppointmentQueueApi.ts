83b692d893c26a6823f21ad22356e24036f368ce
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-09 21:59:58 +0100

    AppointmentQueueApi

---
+++ src/renderer/API/AppointmentQueueApi.ts
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+import { AppointmentQueueItem } from '../models/instance.model';
+
+export const AppointmentQueueApi = createApi({
+  reducerPath: 'AppointmentQueueApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'localhost:3000/queue' }),
+  endpoints: (builder) => ({
+    createQueue: builder.mutation({
+      query: (roleId: number) => ({ url: `/${roleId}`, method: 'POST' }),
+    }),
+    getQueueInfo: builder.query({ query: (roleId) => `/${roleId}` }),
+    deleteQueue: builder.mutation({
+      query: (roleId: number) => ({ url: `/${roleId}`, method: 'DELETE' }),
+    }),
+    getAppointments: builder.query({ query: (roleId) => `/${roleId}/item` }),
+    addAppointment: builder.mutation({
+      query: (data: { roleId: number; body: AppointmentQueueItem }) => {
+        const { roleId, body } = data;
+        return {
+          url: `/${roleId}/item`,
+          method: 'POST',
+          body: body,
+        };
+      },
+    }),
+    deleteAppointment: builder.mutation({
+      query: (data: { roleId: number; body: AppointmentQueueItem }) => {
+        const { roleId, body } = data;
+        return {
+          url: `/${roleId}/item`,
+          method: 'DELETE',
+          body: body,
+        };
+      },
+    }),
+    getQueueState: builder.query({ query: (roleId) => `/${roleId}/state` }),
+    changeQueueState: builder.mutation({
+      query: (data: { roleId: number; body: any }) => {
+        const { roleId, body } = data;
+        return { url: `/${roleId}/state`, method: 'PATCH', body: body };
+      },
+    }),
+  }),
+});
