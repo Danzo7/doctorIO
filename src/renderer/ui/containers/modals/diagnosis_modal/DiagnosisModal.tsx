a7dc557a95aa0bf48a27977cd885d207d46bda06
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-11 23:09:56 +0100

    ✨ useUpdateTestMutation

--- src/renderer/ui/containers/modals/diagnosis_modal/DiagnosisModal.tsx
+++ src/renderer/ui/containers/modals/diagnosis_modal/DiagnosisModal.tsx
@@ -3,29 +3,45 @@ import TextButton from '@components/buttons/text_button';
 import Input from '@components/inputs/input';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import ModalContainer from '@components/modal_container';
-
+import { useUpdateTestMutation } from '@redux/instance/appointmentQueue/AppointmentQueueApi';
+import { Test } from '../../../../models/instance.model';
+import { Overlay } from '@libs/overlay';
 interface Inputs {
   [label: string]: string;
 }
 interface DiagnosisModalProps {
   inputArray?: any[];
+  position: number;
 }
+
 export default function DiagnosisModal({
+  position,
   inputArray = [
-    { label: 'weight', type: 'number' },
-    { label: 'Tall', type: 'number' },
-    { label: 'Blood pressure', type: 'text' },
-    { label: 'Input', type: 'number' },
-    { label: 'Input', type: 'text' },
-    { label: 'Input', type: 'text' },
+    { label: 'weight', type: 'float' },
+    { label: 'height', type: 'number' },
+    { label: 'Blood pressure', type: 'number' },
+    { label: 'bloodType', type: 'text' },
+    { label: 'RH', type: 'number' },
   ],
 }: DiagnosisModalProps) {
+  const [updateTest] = useUpdateTestMutation();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    console.log(formData);
+    const newTest: Test = {
+      weight: Number(formData.weight),
+      height: Number(formData.height),
+      bloodPressure: Number(formData['Blood pressure']),
+      bloodType: 'A',
+      Rh: true,
+    };
+    updateTest({ roleId: 1, test: newTest, position: position });
+    Overlay.close();
+  };
   console.log(errors);
   return (
     <ModalContainer
