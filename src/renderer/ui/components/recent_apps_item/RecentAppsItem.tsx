0354dc756aa3211835bbeeea37e98abd0a460e1d
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-11 20:21:32 +0100

    ✨ useAddAppointmentMutation

--- src/renderer/ui/components/recent_apps_item/RecentAppsItem.tsx
+++ src/renderer/ui/components/recent_apps_item/RecentAppsItem.tsx
@@ -3,18 +3,17 @@ import { color } from '@colors';
 import PresentationItem from '@components/presentation_item';
 import { useOverlay } from '@libs/overlay/useOverlay';
 import DiagnosisModal from '@containers/modals/diagnosis_modal';
-import { AppointmentQueueItem, Patient } from '@models/instance.model';
+import { Patient } from '@models/instance.model';
 import { DEFAULT_MODAL, Overlay } from '@libs/overlay';
-import { useAppDispatch, useAppSelector } from '@store';
-import { addToQueue } from '@redux/instance/appointmentQueue/appointmentQueueSlice';
+import { useAddAppointmentMutation } from '@redux/instance/appointmentQueue/AppointmentQueueApi';
 
 export default function RecentAppsItem({
   firstName,
   lastName,
   patId,
 }: Pick<Patient, 'firstName' | 'lastName' | 'patId'>) {
-  const { appointments } = useAppSelector((state) => state.appointmentQueue);
-  const dispatch = useAppDispatch();
+  const [AddAppointment] = useAddAppointmentMutation();
+
   const { open } = useOverlay();
   return (
     <PresentationItem
@@ -32,14 +31,8 @@ export default function RecentAppsItem({
         backgroundColor={color.good_green}
         radius={7}
         onPress={() => {
-          //REDUX fetch patient info instead
-          const newApp: AppointmentQueueItem = {
-            patientId: patId,
-            patientName: firstName + ' ' + lastName,
-            date: new Date(),
-            position: appointments[appointments.length - 1].position + 1,
-          };
-          dispatch(addToQueue(newApp));
+          //REDUX get the right roleId
+          AddAppointment({ roleId: 1, body: patId });
           Overlay.close();
         }}
       />
