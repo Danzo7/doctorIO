2dea41fef8bacf3e25fd775f240948741e71986e
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-12 23:24:40 +0100

    ✅ PrescriptionTab

--- src/renderer/ui/layers/medical_session/pages/prescription_tab/PrescriptionTab.tsx
+++ src/renderer/ui/layers/medical_session/pages/prescription_tab/PrescriptionTab.tsx
@@ -1,18 +1,15 @@
-import { drugList } from '@api/fake';
 import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
 import Header from '@components/header';
 import AddDrugModal from '@containers/modals/add_drug_modal';
 import { DEFAULT_MODAL } from '@libs/overlay';
 import { useOverlay } from '@libs/overlay/useOverlay';
-import { Drug } from '@models/instance.model';
-import { useState } from 'react';
+import { useAppDispatch } from '@store';
 import MedicamentTable from './medicament_table';
 import './style/index.scss';
 
 interface PrescriptionTabProps {}
 export default function PrescriptionTab({}: PrescriptionTabProps) {
   const { open } = useOverlay();
-  const [data, setData] = useState<Drug[]>(drugList);
 
   return (
     <div className="prescription-tab">
@@ -22,33 +19,12 @@ export default function PrescriptionTab({}: PrescriptionTabProps) {
           <DarkLightCornerButton
             text="Add..."
             onPress={() => {
-              open(
-                <AddDrugModal
-                  onSubmitPress={(formData) => {
-                    console.log(formData);
-                    setData((prev) => {
-                      return [
-                        ...prev,
-                        {
-                          name: formData.name,
-                          qts: formData.qts,
-                          dosage: formData.dose,
-                          description: formData.description,
-                          duration: formData.duration,
-                          id: Math.random() * 10,
-                        },
-                      ];
-                    });
-                    console.log(data);
-                  }}
-                />,
-                DEFAULT_MODAL,
-              );
+              open(<AddDrugModal />, DEFAULT_MODAL);
             }}
           />
         }
       />
-      <MedicamentTable editable drugList={data} />
+      <MedicamentTable editable />
     </div>
   );
 }
