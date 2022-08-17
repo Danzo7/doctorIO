2d1a9035bdff92266060dea68a7e431a084c3d03
danzo7 <aymenmiro@gmail.com>
2022-08-17 14:44:32 +0100

    ✨ Add delete medical document fonctionality

--- src/renderer/ui/components/document_preview_panel/document_preview_item/DocumentPreviewItem.tsx
+++ src/renderer/ui/components/document_preview_panel/document_preview_item/DocumentPreviewItem.tsx
@@ -11,12 +11,46 @@ import WarningModal from '@containers/modals/warning_modal';
 import { DEFAULT_MODAL } from '@libs/overlay';
 import TextButton from '@components/buttons/text_button';
 import { color } from '@assets/styles/color';
-import { useDownloadDocumentMutation } from '@redux/instance/record/recordApi';
+import {
+  useDeleteDocumentMutation,
+  useDownloadDocumentMutation,
+} from '@redux/instance/record/recordApi';
 import { useRef } from 'react';
+//TODO move to file
+const DeleteDocumentModal = ({
+  fileName,
+  id,
+  onSucceed,
+}: {
+  fileName: string;
+  id: string;
+  onSucceed?: () => void;
+}) => {
+  const [deleting, { isLoading }] = useDeleteDocumentMutation();
+
+  return (
+    <WarningModal
+      warningTitle={`You are about to delete ${fileName} ? `}
+      warningDescription="the file will no longer be available after this action "
+    >
+      <TextButton
+        text={`${isLoading ? 'Deleting...' : 'Delete'}`}
+        backgroundColor={color.hot_red}
+        disabled={isLoading}
+        width="100%"
+        onPress={() => {
+          //REDUX delete file on the server
+          deleting({ id }).then(() => {
+            onSucceed?.();
+          });
+        }}
+      />
+    </WarningModal>
+  );
+};
 
 export default function DocumentPreviewItem({
   fileName,
-  fileType,
   id,
   status,
   date,
@@ -24,18 +58,19 @@ export default function DocumentPreviewItem({
   const { open, close } = useOverlay();
   const statusRef = useRef(status);
   const consumedError = useRef(false);
-
-  const [download, { isError, error }] = useDownloadDocumentMutation();
-  const err = isError ? ((error as any).error?.data as ServerError) : undefined;
-  if (err?.statusCode === 404) {
+  const [download, result] = useDownloadDocumentMutation();
+  const downError = result.isError
+    ? ((result.error as any).error?.data as ServerError)
+    : undefined;
+  if (downError?.statusCode === 404) {
     statusRef.current = 'LOST';
   }
-  if (err?.statusCode && !consumedError.current) {
+  if (downError?.statusCode && !consumedError.current) {
     consumedError.current = true;
     open(
       <WarningModal
-        warningTitle={`${err.error} (${err.statusCode})`}
-        warningDescription={err?.message as string}
+        warningTitle={`${downError.error} (${downError.statusCode})`}
+        warningDescription={downError?.message as string}
       />,
       {
         ...DEFAULT_MODAL,
@@ -71,20 +106,11 @@ export default function DocumentPreviewItem({
         Icon={TrashCan}
         onPress={() => {
           open(
-            <WarningModal
-              warningTitle={`You are about to delete ${fileName} ? `}
-              warningDescription="the file will no longer be available after this action "
-            >
-              <TextButton
-                text="Confirm"
-                backgroundColor={color.hot_red}
-                width="100%"
-                onPress={() => {
-                  //REDUX delete file on the server
-                  close();
-                }}
-              />
-            </WarningModal>,
+            <DeleteDocumentModal
+              id={id}
+              fileName={fileName}
+              onSucceed={close}
+            />,
             DEFAULT_MODAL,
           );
         }}
