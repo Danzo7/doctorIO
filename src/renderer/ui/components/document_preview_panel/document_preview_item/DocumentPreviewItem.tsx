9996b11e51345e6ac72bee9bd5a817c48c141d5f
danzo7 <aymenmiro@gmail.com>
2022-08-17 03:54:16 +0100

    ✨ Download functionality

--- src/renderer/ui/components/document_preview_panel/document_preview_item/DocumentPreviewItem.tsx
+++ src/renderer/ui/components/document_preview_panel/document_preview_item/DocumentPreviewItem.tsx
@@ -2,7 +2,8 @@ import SquareIconButton from '@components/buttons/square_icon_button/SquareIconB
 import PreviewWithControls from '@components/preview_with_controls';
 import TrashCan from 'toSvg/trash_can.svg?icon';
 import AppointmentHistoryIcon from 'toSvg/appointment_history.svg?icon';
-import { MedicalDocument } from '@models/instance.model';
+import damaged from 'toSvg/damaged.svg?icon';
+import { MedicalDocument, ServerError } from '@models/instance.model';
 import { format } from 'date-fns';
 import { DATE_ONLY } from '@constants/data_format';
 import { useOverlay } from '@libs/overlay/useOverlay';
@@ -10,6 +11,8 @@ import WarningModal from '@containers/modals/warning_modal';
 import { DEFAULT_MODAL } from '@libs/overlay';
 import TextButton from '@components/buttons/text_button';
 import { color } from '@assets/styles/color';
+import { useDownloadDocumentMutation } from '@redux/instance/record/recordApi';
+import { useRef } from 'react';
 
 export default function DocumentPreviewItem({
   fileName,
@@ -19,27 +22,57 @@ export default function DocumentPreviewItem({
   date,
 }: MedicalDocument) {
   const { open, close } = useOverlay();
+  const statusRef = useRef(status);
+  const consumedError = useRef(false);
+
+  const [download, { isError, error }] = useDownloadDocumentMutation();
+  const err = isError ? ((error as any).error?.data as ServerError) : undefined;
+  if (err?.statusCode === 404) {
+    statusRef.current = 'LOST';
+  }
+  if (err?.statusCode && !consumedError.current) {
+    consumedError.current = true;
+    open(
+      <WarningModal
+        warningTitle={`${err.error} (${err.statusCode})`}
+        warningDescription={err?.message as string}
+      />,
+      {
+        ...DEFAULT_MODAL,
+      },
+    );
+  }
+
   return (
     <PreviewWithControls
       primaryText={fileName}
       secondaryText={format(date, DATE_ONLY)}
     >
-      {status == 'NORMAL' ? (
-        <SquareIconButton
-          Icon={AppointmentHistoryIcon}
-          onPress={() => {
-            //REDUX download file or preview
-          }}
-        />
-      ) : (
-        <span>{status}</span> //UI improve span
-      )}
+      <SquareIconButton
+        Icon={statusRef.current == 'LOST' ? damaged : AppointmentHistoryIcon}
+        iconColor={statusRef.current == 'LOST' ? color.text_gray : undefined}
+        onPress={() => {
+          consumedError.current = false;
+          if (statusRef.current == 'LOST') {
+            open(
+              <WarningModal
+                warningTitle="Document Lost"
+                warningDescription="This document is lost. Please contact the administrator."
+              />,
+              {
+                ...DEFAULT_MODAL,
+              },
+            );
+          } else download({ id, name: fileName });
+        }}
+      />
+
       <SquareIconButton
         Icon={TrashCan}
         onPress={() => {
           open(
             <WarningModal
-              warningTitle={`You are about to delete ${fileName} (${fileType}) ? `}
+              warningTitle={`You are about to delete ${fileName} ? `}
               warningDescription="the file will no longer be available after this action "
             >
               <TextButton
