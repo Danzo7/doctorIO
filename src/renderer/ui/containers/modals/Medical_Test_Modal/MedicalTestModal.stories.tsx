3d76ef215e7cd56274704dd94efc53d99a76c58e
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-18 20:02:06 +0100

    ✨ MedicalTestModal functions implemented

--- src/renderer/ui/containers/modals/Medical_Test_Modal/MedicalTestModal.stories.tsx
+++ src/renderer/ui/containers/modals/Medical_Test_Modal/MedicalTestModal.stories.tsx
@@ -9,13 +9,4 @@ const Template: Story<ComponentProps<typeof MedicalTestModal>> = (args) => (
   <MedicalTestModal {...args} />
 );
 export const FirstStory = Template.bind({});
-FirstStory.args = {
-  inputArray: [
-    { label: 'weight', type: 'number' },
-    { label: 'Tall', type: 'number' },
-    { label: 'Blood pressure', type: 'text' },
-    { label: 'Input', type: 'number' },
-    { label: 'Input', type: 'text' },
-    { label: 'Input', type: 'text' },
-  ],
-};
+FirstStory.args = {};
--- src/renderer/ui/containers/modals/Medical_Test_Modal/MedicalTestModal.tsx
+++ src/renderer/ui/containers/modals/Medical_Test_Modal/MedicalTestModal.tsx
@@ -3,29 +3,39 @@ import TextButton from '@components/buttons/text_button';
 import Input from '@components/inputs/input';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import ModalContainer from '@components/modal_container';
-import { useUpdateTestMutation } from '@redux/instance/appointmentQueue/AppointmentQueueApi';
+import {
+  useAddAppointmentMutation,
+  useUpdateTestMutation,
+} from '@redux/instance/appointmentQueue/AppointmentQueueApi';
 import { Test } from '../../../../models/instance.model';
 import { Overlay } from '@libs/overlay';
-interface Inputs {
+import MultiOptionSwitcher from '@components/buttons/multi_option_switcher';
+import ToggleButton from '@components/buttons/toggle_button';
+import InputContainer from '@components/inputs/input_container';
+interface InputsArray {
   [label: string]: string;
 }
+interface Inputs {
+  weight: number;
+  height: number;
+  BloodPressure: number;
+  bloodType: 'A' | 'B' | 'AB' | 'O';
+  RH: boolean;
+}
 interface MedicalTestModalProps {
-  inputArray?: any[];
-  position: number;
+  position?: number;
+  patientId?: number;
 }
 
 export default function MedicalTestModal({
   position,
-  inputArray = [
-    { label: 'weight', type: 'float' },
-    { label: 'height', type: 'number' },
-    { label: 'Blood pressure', type: 'number' },
-    { label: 'bloodType', type: 'text' },
-    { label: 'RH', type: 'number' },
-  ],
+  patientId,
 }: MedicalTestModalProps) {
   const [updateTest] = useUpdateTestMutation();
+  const [addAppointment] = useAddAppointmentMutation();
+  const RhOptions = ['Positive', 'Negative'];
   const {
+    setValue,
     register,
     handleSubmit,
     formState: { errors },
@@ -35,14 +45,24 @@ export default function MedicalTestModal({
     const newTest: Test = {
       weight: Number(formData.weight),
       height: Number(formData.height),
-      bloodPressure: Number(formData['Blood pressure']),
-      bloodType: 'A',
-      Rh: true,
+      bloodPressure: Number(formData.BloodPressure),
+      bloodType: formData.bloodType,
+      Rh: formData.RH,
     };
-    updateTest({ roleId: 1, test: newTest, position: position });
+    if (position) updateTest({ roleId: 1, test: newTest, position: position });
+    else if (patientId) {
+      addAppointment({
+        roleId: 1,
+        body: { patientId: patientId, test: newTest },
+      });
+    }
     Overlay.close();
   };
-  console.log(errors);
+  const changeBloodType = (v: string) => {
+    if (v == 'A' || v == 'B' || v == 'AB' || v == 'O') {
+      setValue('bloodType', v);
+    }
+  };
   return (
     <ModalContainer
       title="Medical test"
@@ -50,7 +70,7 @@ export default function MedicalTestModal({
       controls={
         <>
           <TextButton
-            text="Add to queue"
+            text={position ? 'Add Test' : 'Add to queue'}
             backgroundColor={colors.good_green}
             radius={7}
             fontSize={14}
@@ -60,17 +80,64 @@ export default function MedicalTestModal({
         </>
       }
     >
-      {inputArray.map(({ label, type }, index) => (
-        <Input
-          key={index}
-          type={type}
-          label={label}
-          {...register(label, {
-            required: { value: true, message: `${label} is required` },
-          })}
-          errorMsg={errors[label]?.message}
+      <Input
+        errorMsg={errors.weight?.message}
+        type="number"
+        label="Weight"
+        {...register('weight', {
+          required: { value: true, message: 'weight is required' },
+        })}
+      />
+      <Input
+        errorMsg={errors.height?.message}
+        type="number"
+        label="Height"
+        {...register('height', {
+          required: { value: true, message: 'height is required' },
+        })}
+      />
+      <Input
+        errorMsg={errors.BloodPressure?.message}
+        type="number"
+        label="Blood pressure"
+        {...register('BloodPressure', {
+          required: { value: true, message: 'Blood pressure is required' },
+        })}
+      />
+      <Input
+        errorMsg={errors.bloodType?.message}
+        type={{
+          type: 'select',
+          options: ['A', 'B', 'AB', 'O'],
+        }}
+        label="Blood type"
+        {...register(
+          'bloodType',
+
+          {
+            required: { value: true, message: 'Blood type is required' },
+          },
+        )}
+        onChange={changeBloodType as any}
+        //     setValue={changeGender}
+      />
+      {/* <InputContainer label="RH" grow>
+        <ToggleButton
+          onChange={(checked) => {
+            setValue('RH', checked);
+          }}
         />
-      ))}
+        </InputContainer>  */}
+      {
+        <InputContainer label="RH" grow>
+          <MultiOptionSwitcher
+            textList={RhOptions}
+            onChange={(selected) => {
+              setValue('RH', RhOptions[selected] == 'Positive');
+            }}
+          />
+        </InputContainer>
+      }
     </ModalContainer>
   );
 }
