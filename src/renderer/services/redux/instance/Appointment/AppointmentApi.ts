50c8e1e2013a781c14d7052a45f9c83021446b4a
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-19 19:34:37 +0100

    ➕ add appointmentApi

---
+++ src/renderer/services/redux/instance/Appointment/AppointmentApi.ts
@@ -0,0 +1,47 @@
+import { Appointment_v2, BookedAppointment } from '@models/instance.model';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { parseISO } from 'date-fns';
+
+const appointmentApi = createApi({
+  reducerPath: 'AppointmentApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:3000/record/appointment',
+  }),
+  tagTypes: ['BookAppointment'],
+  endpoints: (builder) => ({
+    //GET
+    getBookedAppointment: builder.query<BookedAppointment[], null>({
+      query: () => `/book`,
+      providesTags: ['BookAppointment'],
+      transformResponse: (
+        response: (Omit<BookedAppointment, 'bookedFor'> & {
+          bookedFor: string;
+        })[],
+      ) => {
+        return response.map(({ bookedFor, ...other }) => ({
+          ...other,
+          bookedFor: parseISO(bookedFor),
+        }));
+      },
+    }),
+    bookAppointment: builder.mutation<
+      Appointment_v2,
+      { patientId: number; body: { date: Date; subject?: string } }
+    >({
+      query: ({ patientId, body }) => {
+        return {
+          url: `/book?patientId=${patientId}`,
+          method: 'POST',
+          body: { ...body },
+        };
+      },
+      invalidatesTags: ['BookAppointment'],
+    }),
+    //PATCH
+    // setAppointmentDone :builder.mutation<>
+  }),
+});
+
+export default appointmentApi;
+export const { useGetBookedAppointmentQuery, useBookAppointmentMutation } =
+  appointmentApi;
