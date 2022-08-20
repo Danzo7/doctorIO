24bd762521ff3599bad52c28106c698c5b6b2d34
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-20 23:37:11 +0100

    ✅ update AddSelectedToQueueModal

--- src/renderer/ui/containers/modals/add_selected_to_queue_modal/AddSelectedToQueueModal.tsx
+++ src/renderer/ui/containers/modals/add_selected_to_queue_modal/AddSelectedToQueueModal.tsx
@@ -2,13 +2,29 @@ import ModalContainer from '@components/modal_container';
 import RecentAppsItem from '@components/recent_apps_item';
 import { PatientBrief } from '@models/instance.model';
 import './style/index.scss';
+interface AddSelectedToQueueModalProps {
+  onAdd: () => void;
 
-export default function AddSelectedToQueueModal({ name, id }: PatientBrief) {
+  appointmentId?: number;
+}
+export default function AddSelectedToQueueModal({
+  name,
+  id,
+  onAdd,
+  appointmentId,
+}: PatientBrief & AddSelectedToQueueModalProps) {
   return (
     <ModalContainer
       gap={10}
       title="Add a Patient to appointment queue"
-      controls={<RecentAppsItem id={id} name={name} />}
+      controls={
+        <RecentAppsItem
+          id={id}
+          name={name}
+          onAdd={onAdd}
+          appointmentId={appointmentId}
+        />
+      }
     ></ModalContainer>
   );
 }
