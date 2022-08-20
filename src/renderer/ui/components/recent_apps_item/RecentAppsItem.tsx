fa0c192e07121baccc1cf0a3029e6a220858fe60
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-20 23:35:55 +0100

    ✅ update RecentAppsItem

--- src/renderer/ui/components/recent_apps_item/RecentAppsItem.tsx
+++ src/renderer/ui/components/recent_apps_item/RecentAppsItem.tsx
@@ -5,22 +5,29 @@ import { useOverlay } from '@libs/overlay/useOverlay';
 import MedicalTestModal from '@containers/modals/Medical_Test_Modal';
 import { PatientBrief } from '@models/instance.model';
 import { DEFAULT_MODAL, Overlay } from '@libs/overlay';
-import { useAddAppointmentMutation } from '@redux/instance/appointmentQueue/AppointmentQueueApi';
 
-export default function RecentAppsItem({ id, name }: PatientBrief) {
-  const [AddAppointment] = useAddAppointmentMutation();
-  // const { isSuccess, data, isFetching, error } = useGetPatientDetailQuery(id);
-  // console.log('error', error);
-
+interface RecentAppsItemProps {
+  onAdd: () => void;
+  appointmentId?: number;
+}
+export default function RecentAppsItem({
+  id,
+  name,
+  appointmentId,
+  onAdd,
+}: PatientBrief & RecentAppsItemProps) {
   const { open } = useOverlay();
   return (
     <PresentationItem primaryText={name} secondaryText={`# ${id}`}>
       <TextButton
-        text={'Run diagnosis...'} //REDUX change it to dynamic text
+        text={'Run Test...'} //REDUX change it to dynamic text
         backgroundColor={color.cold_blue}
         radius={7}
         onPress={() => {
-          open(<MedicalTestModal patientId={id} />, DEFAULT_MODAL);
+          open(
+            <MedicalTestModal patientId={id} appointmentId={appointmentId} />,
+            DEFAULT_MODAL,
+          );
         }}
       />
       <TextButton
@@ -28,8 +35,7 @@ export default function RecentAppsItem({ id, name }: PatientBrief) {
         backgroundColor={color.good_green}
         radius={7}
         onPress={() => {
-          //REDUX get the right roleId
-          AddAppointment({ roleId: 1, body: { patientId: id } });
+          onAdd();
           Overlay.close();
         }}
       />
