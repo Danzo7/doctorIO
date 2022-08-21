8af000dd0aad191b50063fcbf8ebcfec6cf75288
danzo7 <aymenmiro@gmail.com>
2022-08-21 18:55:02 +0100

    🚑 use new Input

--- src/renderer/ui/layers/medical_session/pages/session_parameter/SessionParameter.tsx
+++ src/renderer/ui/layers/medical_session/pages/session_parameter/SessionParameter.tsx
@@ -3,38 +3,55 @@ import Header from '@components/header';
 import Datepicker from '@components/inputs/datepicker';
 import Input from '@components/inputs/input';
 import MultipleCheckGroup from '@components/inputs/multiple_check_group';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-import { useState } from 'react';
 import './style/index.scss';
 interface SessionParameterProps {}
+interface Inputs {
+  bookAppointment: boolean;
+  bookDate: Date;
+  customCost: number;
+  payOutside: boolean;
+}
 export default function SessionParameter({}: SessionParameterProps) {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const { control, handleSubmit } = useForm<Inputs>({
+    defaultValues: {
+      bookAppointment: false,
+      bookDate: new Date(),
+      customCost: 0,
+      payOutside: false,
+    },
+  });
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data); //FEATURE save to redux
 
-  const onDateChange = (date: Date) => {
-    setSelectedDate(date);
-  };
   return (
     <div className="session-parameter">
       <Header
         title="Book an appointment"
         buttonNode={<ToggleButton isChecked={true} />}
       />
+      <form>
+        <Input type="date" control={control} name={'bookDate'} />
 
-      <Datepicker selected={selectedDate} onChange={onDateChange} />
-
-      <div className="payment-container">
-        <Header
-          title="Payment"
-          buttonNode={<ToggleButton isChecked={true} />}
+        <div className="payment-container">
+          <Header
+            title="Payment"
+            buttonNode={<ToggleButton isChecked={true} />}
+          />
+          <span>Earnings can be disables in clinic settings</span>
+        </div>
+        <div className="payment-input-container">
+          <MultipleCheckGroup items={['none', '1000', '2000', '3000']} />
+          <span>Custom :</span>
+          <Input type={'number'} control={control} name={'customCost'} />
+        </div>
+        <Input
+          type={'checkbox'}
+          label="hand payment to assistant"
+          control={control}
+          name="payOutside"
         />
-        <span>Earnings can be disables in clinic settings</span>
-      </div>
-      <div className="payment-input-container">
-        <MultipleCheckGroup items={['none', '1000', '2000', '3000']} />
-        <span>Custom :</span>
-        <Input type={'number'} />
-      </div>
-      <Input type={'checkbox'} label="hand payment to assistant" />
+      </form>
     </div>
   );
 }
