b48f89fc2d72e6dd35e437e98810189347df27a2
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-22 22:44:08 +0100

    ✅ update sessionSlice

--- src/renderer/services/redux/local/session/sessionSlice.ts
+++ src/renderer/services/redux/local/session/sessionSlice.ts
@@ -1,32 +1,73 @@
 import { Drug, Session } from '@models/instance.model';
 import { createSlice, nanoid, PayloadAction } from '@reduxjs/toolkit';
 
-const initialState: Session = { diagnosis: '', prescription: [] };
+interface SessionType {
+  sessionInfo: Session;
+  sessionParameter: {
+    bookAppointment: { isAllowed: boolean; date: Date };
+    payment: { isAllowed: boolean; value: number; handPayment: boolean };
+  };
+}
+
+const initialState: SessionType = {
+  sessionInfo: { diagnosis: '', prescription: [] },
+  sessionParameter: {
+    bookAppointment: { isAllowed: true, date: new Date() },
+    payment: { value: 0, handPayment: false, isAllowed: true },
+  },
+};
 const sessionSlice = createSlice({
   name: 'session',
   initialState: initialState,
   reducers: {
-    updateNotice: (state: Session, action: PayloadAction<string>) => {
-      state.diagnosis = action.payload;
+    updateNotice: (state: SessionType, action: PayloadAction<string>) => {
+      state.sessionInfo.diagnosis = action.payload;
     },
-    clearPrescription: (state: Session) => {
-      state.prescription = [];
+    clearPrescription: (state: SessionType) => {
+      state.sessionInfo.prescription = [];
     },
-    resetSession: (state: Session) => {
-      state.diagnosis = '';
-      state.prescription = [];
+    resetSession: (state: SessionType) => {
+      state.sessionInfo.diagnosis = '';
+      state.sessionInfo.prescription = [];
     },
-    addDrug: (state: Session, action: PayloadAction<Omit<Drug, 'id'>>) => {
-      state.prescription.push({ id: nanoid(), ...action.payload });
+    addDrug: (state: SessionType, action: PayloadAction<Omit<Drug, 'id'>>) => {
+      state.sessionInfo.prescription.push({ id: nanoid(), ...action.payload });
     },
     updatePrescription: (
-      state: Session,
+      state: SessionType,
       action: PayloadAction<{ index: number; drug: Omit<Drug, 'id'> }>,
     ) => {
-      state.prescription[action.payload.index] = {
-        id: state.prescription[action.payload.index].id,
+      state.sessionInfo.prescription[action.payload.index] = {
+        id: state.sessionInfo.prescription[action.payload.index].id,
         ...action.payload.drug,
       };
+    },
+    setBookAppointmentValue: (
+      state: SessionType,
+      action: PayloadAction<Date>,
+    ) => {
+      if (state.sessionParameter.bookAppointment.isAllowed)
+        state.sessionParameter.bookAppointment.date = action.payload;
+    },
+    updateBookAppointmentPermission: (state: SessionType) => {
+      state.sessionParameter.bookAppointment.isAllowed =
+        !state.sessionParameter.bookAppointment.isAllowed;
+    },
+    updatePaymentOptionPermission: (state: SessionType) => {
+      state.sessionParameter.payment.isAllowed =
+        !state.sessionParameter.payment.isAllowed;
+    },
+    updatePaymentValue: (
+      state: SessionType,
+      action: PayloadAction<{ value: 0; handPayment: false; isAllowed: true }>,
+    ) => {
+      if (state.sessionParameter.payment.isAllowed)
+        state.sessionParameter.payment = action.payload;
+    },
+    updateHandPaymentOption: (state: SessionType) => {
+      if (state.sessionParameter.payment.isAllowed)
+        state.sessionParameter.payment.handPayment =
+          !state.sessionParameter.payment.handPayment;
     },
   },
 });
@@ -36,6 +77,11 @@ export const {
   clearPrescription,
   updatePrescription,
   resetSession,
+  setBookAppointmentValue,
+  updateBookAppointmentPermission,
+  updatePaymentValue,
+  updateHandPaymentOption,
+  updatePaymentOptionPermission,
 } = sessionSlice.actions;
 
 export default sessionSlice;
