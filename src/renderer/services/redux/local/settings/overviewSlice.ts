d7ba22b72e8ec2b0459ae8f49dfef2765bc8f558
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-23 20:13:27 +0100

    ➕ add overviewSlice

---
+++ src/renderer/services/redux/local/settings/overviewSlice.ts
@@ -0,0 +1,31 @@
+import { Clinic } from '@models/server.models';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { clinic } from '@api/fake';
+const initialState: Clinic = clinic;
+
+const overviewSlice = createSlice({
+  name: 'overview',
+  initialState: initialState,
+  reducers: {
+    setOverviewInfo: (
+      state: Clinic,
+      action: PayloadAction<{
+        clinicName: string;
+        description: string;
+        clinicAddress: string;
+        phoneNumber: string;
+      }>,
+    ) => {
+      state.clinicName = action.payload.clinicName;
+      state.description = action.payload.description;
+      state.clinicAddress = action.payload.clinicAddress;
+      state.phoneNumber = action.payload.phoneNumber;
+    },
+    changeLogo: (state: Clinic, action: PayloadAction<string>) => {
+      state.avatar = action.payload;
+    },
+  },
+});
+export const { setOverviewInfo, changeLogo } = overviewSlice.actions;
+
+export default overviewSlice;
