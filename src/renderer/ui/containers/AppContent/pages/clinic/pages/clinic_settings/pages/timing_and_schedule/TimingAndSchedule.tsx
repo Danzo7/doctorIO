6848338577e3e55d48cf160aff9484d554dbd48e
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-25 00:42:02 +0100

    ✅ update TimingAndSchedule

--- src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/timing_and_schedule/TimingAndSchedule.tsx
+++ src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/timing_and_schedule/TimingAndSchedule.tsx
@@ -1,11 +1,17 @@
+import color from '@assets/styles/color';
 import BorderSeparator from '@components/border_separator';
+import TextButton from '@components/buttons/text_button';
 import CheckboxTile from '@components/checkbox_tile';
 import Header from '@components/header';
 import MultipleCheckGroup from '@components/inputs/multiple_check_group';
 import SmallClinicStatus from '@components/small_clinic_status';
-import PeriodTimePicker from '@components/time_picker';
+import PeriodTimePicker, { PeriodTimeInputs } from '@components/time_picker';
 import { rules } from '@constants/permissions';
-import { useAppSelector } from '@store';
+import SnakeBarActionsControls from '@containers/modals/snake_bar/snake_bar_actions_controls';
+import usePrompt from '@libs/HistoryBlocker';
+import { updateTimingAndSchedule } from '@redux/local/settings/settingsSlice';
+import store, { useAppDispatch, useAppSelector } from '@store';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import './style/index.scss';
 
 interface TimingAndScheduleProps {}
@@ -19,7 +25,63 @@ export default function TimingAndSchedule({}: TimingAndScheduleProps) {
     'friday',
     'saturday',
   ];
-  const clinicInfo = useAppSelector((state) => state.settings);
+
+  interface Values {
+    openingTime: PeriodTimeInputs;
+    break: PeriodTimeInputs;
+  }
+  // const clinicInfo = useAppSelector((state) => state.settings);
+  const clinicInfo = store.getState().settings;
+  console.log('clinicInfo', clinicInfo.timing.timeToClose);
+  const { control, getValues, setValue, watch } = useForm<Values>({
+    defaultValues: {
+      openingTime: {
+        startTime: clinicInfo.timing.timeToOpen,
+        endTime: clinicInfo.timing.timeToClose,
+      },
+      break: {
+        startTime: clinicInfo.timing.breakStart,
+        endTime: clinicInfo.timing.breakEnd,
+      },
+    },
+  });
+  const dispatch = useAppDispatch();
+  /* usePrompt(
+    'Careful : you have unsaved changes !',
+    ({ closeOverlay, dismiss }) => (
+      <SnakeBarActionsControls>
+        <TextButton
+          text="reset"
+          afterBgColor={color.darker}
+          onPress={() => {
+            closeOverlay();
+            reset();
+            dismiss();
+          }}
+        />
+        <TextButton
+          text="Save changes"
+          backgroundColor={color.good_green}
+          onPress={() => {
+            handleSubmit(onSubmit)();
+            closeOverlay();
+            dismiss();
+          }}
+        />
+      </SnakeBarActionsControls>
+    ),
+    isDirty,
+    true,
+  );  */
+  dispatch(
+    updateTimingAndSchedule({
+      timeToOpen: watch('openingTime.startTime'),
+      timeToClose: watch('openingTime.endTime'),
+      breakStart: watch('break.startTime'),
+      breakEnd: watch('break.endTime'),
+    }),
+  );
+  watch();
   return (
     <div className="timing-and-schedule">
       <SmallClinicStatus />
@@ -28,15 +90,13 @@ export default function TimingAndSchedule({}: TimingAndScheduleProps) {
         <Header title="Timing" />
         <PeriodTimePicker
           title="Opening time"
-          startTime={clinicInfo.timing.timeToOpen}
-          endTime={clinicInfo.timing.timeToClose}
-          onChange={(data) => console.log(data)} //REDUX:update settings
+          values={getValues('openingTime')}
+          onChange={(data) => setValue('openingTime', data)}
         />
         <PeriodTimePicker
           title="Break"
-          startTime="10:04"
-          endTime="14:15"
-          onChange={(data) => console.log(data)} //REDUX:update settings
+          values={getValues('break')}
+          onChange={(data) => setValue('openingTime', data)}
         />
         <span>Working days</span>
         <MultipleCheckGroup
