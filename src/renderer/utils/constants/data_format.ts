d31f7564a7eb15403331d44b18740aef70208518
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-25 00:41:43 +0100

    ➕ add TIME_ONLY_24H format

--- src/renderer/utils/constants/data_format.ts
+++ src/renderer/utils/constants/data_format.ts
@@ -3,4 +3,12 @@ const DATE_NO_YEAR = 'EEEE, dd MMM';
 const TIME_ONLY = 'h:mm aa';
 const DATE_TIME = 'dd MMM yyyy, h:mm aa';
 const START_OF_TIME = '2020-1-1';
-export { DATE_ONLY, DATE_NO_YEAR, DATE_TIME, TIME_ONLY, START_OF_TIME };
+const TIME_ONLY_24H = 'HH:mm';
+export {
+  DATE_ONLY,
+  DATE_NO_YEAR,
+  DATE_TIME,
+  TIME_ONLY,
+  START_OF_TIME,
+  TIME_ONLY_24H,
+};
