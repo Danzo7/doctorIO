1c8741482013b0d4aa4eb7172c291245f0ec92f6
danzo7 <aymenmiro@gmail.com>
2022-08-26 16:47:19 +0100

    ✨ New show networkModal when server is not responding

--- src/renderer/ui/layers/main_layer/MainLayer.tsx
+++ src/renderer/ui/layers/main_layer/MainLayer.tsx
@@ -1,12 +1,33 @@
 import AppContent from '@containers/AppContent';
 import AppMenu from '@containers/AppMenu';
 import AppSidebar from '@containers/AppSidebar';
+import { NetworkError } from '@containers/modals/warning_modal';
 import MedicalSession from '@layers/medical_session';
+import { STOP_MODAL } from '@libs/overlay';
+import { ModalPortal } from '@libs/overlay/OverlayContainer';
+import { useOverlay } from '@libs/overlay/useOverlay';
+import { StaticQueries } from '@redux/dynamic_queries';
+import { useAppSelector } from '@store';
+import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 interface MainLayerProps {}
 export default function MainLayer({}: MainLayerProps) {
-  return (
+  const { selectedClinic, clinic } = useAppSelector((state) => state.user);
+  const [isWorking, setIsWorking] = useState(false);
+  //check if url is alive
+  useEffect(() => {
+    if (selectedClinic && clinic) {
+      (async () => {
+        const res = await fetch(
+          'http://' + clinic[selectedClinic].serverLocation + '/status',
+        );
+        setIsWorking(res.ok);
+      })();
+    }
+  }, [clinic, selectedClinic]);
+
+  return isWorking ? (
     <Routes>
       <Route path="session" element={<MedicalSession />} />
       <Route
@@ -20,5 +41,9 @@ export default function MainLayer({}: MainLayerProps) {
         }
       />
     </Routes>
+  ) : (
+    <ModalPortal {...STOP_MODAL}>
+      <NetworkError errorMsg="The server is not responding. make sure the server is running" />
+    </ModalPortal>
   );
 }
