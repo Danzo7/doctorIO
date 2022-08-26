fc4f7a4bd763735b025318d6e8e9d9e7172293d0
danzo7 <aymenmiro@gmail.com>
2022-08-26 16:46:16 +0100

    🐛 reuse Clinics component

--- src/renderer/ui/layers/offline_layer/containers/joinclinic/Joinclinic.tsx
+++ src/renderer/ui/layers/offline_layer/containers/joinclinic/Joinclinic.tsx
@@ -1,5 +1,6 @@
+import Clinics from '@containers/AppContent/pages/clinic/pages/clinics';
 import './style/index.scss';
 interface JoinclinicProps {}
 export default function Joinclinic({}: JoinclinicProps) {
-  return <div className="joinclinic"></div>;
+  return <Clinics />;
 }
