73fb9b960e1dd7162e509989114827db189b0c57
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-08-29 19:51:16 +0100

    ✅ update MedicalSessionSideBar

--- src/renderer/ui/layers/medical_session/medical_session_side_bar/MedicalSessionSideBar.tsx
+++ src/renderer/ui/layers/medical_session/medical_session_side_bar/MedicalSessionSideBar.tsx
@@ -1,10 +1,12 @@
 import AppointmentHistoryPanel from '@components/appointment_history_panel';
 import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
+import LoadingSpinner from '@components/loading_spinner';
 import MedicalHistory from '@components/medical_history';
 import PreviewInfo from '@components/preview_info';
 import DiagnosisPreview from '@containers/modals/diagnosis_preview';
 import { DEFAULT_MODAL } from '@libs/overlay';
 import { useOverlay } from '@libs/overlay/useOverlay';
+import { useGetPatientDetailQuery } from '@redux/instance/record/patient_api';
 import './style/index.scss';
 
 interface MedicalSessionSideBarProps {
@@ -14,35 +16,38 @@ export default function MedicalSessionSideBar({
   patientId,
 }: MedicalSessionSideBarProps) {
   const { open } = useOverlay();
+  const { isSuccess, data, isFetching, isLoading, error } =
+    useGetPatientDetailQuery(patientId);
 
   return (
     <div className="medical-session-side-bar">
       <div className="medical-session-side-bar-content">
-        {/*    <PreviewInfo
-          title="Diagnosis"
-          data={patient.test}
-          buttonNode={
-            <DarkLightCornerButton
-              text="preview"
-              blend
-              onPress={() => {
-                open(<DiagnosisPreview data={patient.test} />, DEFAULT_MODAL);
-              }}
+        {isLoading || isFetching ? (
+          <LoadingSpinner />
+        ) : (
+          isSuccess &&
+          data.test && (
+            <PreviewInfo
+              title="Diagnosis"
+              data={data.test}
+              buttonNode={
+                <DarkLightCornerButton
+                  text="preview"
+                  blend
+                  onPress={() => {
+                    if (data.test)
+                      open(
+                        <DiagnosisPreview data={data.test} />,
+                        DEFAULT_MODAL,
+                      );
+                  }}
+                />
+              }
             />
-          }
-        />   */}
+          )
+        )}
         <MedicalHistory patientId={patientId} />
-        {/*   <AppointmentHistoryPanel
-          list={
-            patient.appointments
-              .filter(
-                (appointment) =>
-                  appointment.state == 'done' ||
-                  appointment.state == 'done-booked',
-              )
-              .slice(-3) as any
-          }
-        />  */}
+        <AppointmentHistoryPanel patientId={patientId} />
       </div>
     </div>
   );
