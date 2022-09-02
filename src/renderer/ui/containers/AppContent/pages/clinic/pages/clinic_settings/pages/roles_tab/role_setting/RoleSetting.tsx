201ce16467bec00e9d6ba8893b307da291429eb1
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-09-02 03:19:23 +0100

    ✅ update RoleSetting

--- src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/roles_tab/role_setting/RoleSetting.tsx
+++ src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/roles_tab/role_setting/RoleSetting.tsx
@@ -1,9 +1,6 @@
-import {
-  members as fakeMembers,
-  selectedRole as defaultSelected,
-  roles as fakeRoles,
-} from '@api/fake';
+import LoadingSpinner from '@components/loading_spinner';
 import TabMenu from '@components/tab_menu';
+import { useGetRoleByIdQuery } from '@redux/clinic/rbac/role/roleApi';
 import { useSearchParams } from 'react-router-dom';
 import PermissionList from './miniTabs/permission_list';
 import RoleSettingGeneral from './miniTabs/role_setting_general';
@@ -13,36 +10,34 @@ import './style/index.scss';
 interface RoleSettingProps {}
 export default function RoleSetting({}: RoleSettingProps) {
   const [searchParams] = useSearchParams();
-  const roleIdParam = searchParams.get('roleId')
-    ? searchParams.get('roleId')
-    : defaultSelected.id;
-  //REDUX fetch selected role
-  //FIXME
-  const { name, slaveRole, permissions, description } = fakeRoles.filter(
-    ({ id }) => id.toString() == roleIdParam,
-  )[0];
+  const roleIdParam = searchParams.get('roleId');
 
-  if (!permissions) return <div>NOoooooo</div>;
+  const { data, isFetching, isLoading, isSuccess, error } = useGetRoleByIdQuery(
+    Number(roleIdParam) as any,
+    { skip: roleIdParam == undefined },
+  );
+
+  if (roleIdParam == undefined)
+    return <div className="role-setting"> nothing </div>;
+  console.log('data :', data);
+  console.log('data :', error);
   return (
     <div className="role-setting">
-      <TabMenu items={['General', 'Permissions', 'Members']}>
-        <RoleSettingGeneral
-          name={name}
-          description={description}
-          slaveRole={slaveRole}
-        />
-        {
-          <PermissionList permissions={permissions} />
-          //FIXME types
-        }
-        <RoleSettingMembers
-          list={fakeMembers.filter(
-            ({ roles }) =>
-              roles.find(({ id: id }) => id.toString() == roleIdParam) !=
-              undefined,
-          )}
-        />
-      </TabMenu>
+      {isLoading || isFetching ? (
+        <LoadingSpinner />
+      ) : isSuccess ? (
+        <TabMenu items={['General', 'Permissions', 'Members']}>
+          <RoleSettingGeneral
+            name={data.name}
+            description={data.description}
+            slaveRole={data.slaveRole}
+          />
+          {<PermissionList permissions={data.permissions} />}
+          <RoleSettingMembers list={[]} />
+        </TabMenu>
+      ) : (
+        <div> mafihach </div>
+      )}
     </div>
   );
 }
