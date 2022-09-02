c299375bf600b61b8b75c4aabdb917e3de31e7c1
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-09-02 20:35:26 +0100

    ✅ update roleApi

--- src/renderer/services/redux/clinic/rbac/role/roleApi.ts
+++ src/renderer/services/redux/clinic/rbac/role/roleApi.ts
@@ -2,13 +2,13 @@
 import { PermKeys, Role, RoleBrief } from '@models/server.models';
 import { StaticQueries } from '@redux/dynamic_queries';
 import { createApi } from '@reduxjs/toolkit/dist/query/react';
+import memberApi from '../member/memberApi';
 
 const roleApi = createApi({
   reducerPath: 'roleApi',
   baseQuery: StaticQueries.roles.query,
   tagTypes: ['roles'],
   endpoints: (builder) => ({
-    //TODO change types to the correct ones
     getBriefRoles: builder.query<RoleBrief[], void>({
       query: () => '',
       providesTags: ['roles'],
@@ -45,11 +45,65 @@ const roleApi = createApi({
       invalidatesTags: ['roles'],
     }),
 
-    UpdateRole: builder.mutation<void, number>({
-      query: (body) => {
-        return { url: `?id=${body}`, method: 'PATCH' };
+    UpdateRole: builder.mutation<
+      void,
+      {
+        id: number;
+        body: {
+          name?: string;
+          description?: string;
+          permissions: number[];
+        };
+      }
+    >({
+      query: ({ id, body }) => {
+        return { url: `?id=${id}`, method: 'PATCH', body: { ...body } };
       },
       invalidatesTags: ['roles'],
+    }),
+    assignRole: builder.mutation<true, { memberId: number; roleId: number }>({
+      query: (body) => {
+        return { url: '/assign', method: 'PATCH', body: { ...body } };
+      },
+      invalidatesTags: ['roles'],
+      async onQueryStarted(id, { dispatch, queryFulfilled }) {
+        try {
+          await queryFulfilled;
+          dispatch(memberApi.util.invalidateTags(['members']));
+        } catch (err) {
+          //console.log(err);
+        }
+      },
+    }),
+    revokeRole: builder.mutation<true, { memberId: number; roleId: number }>({
+      query: (body) => {
+        return { url: '/revoke', method: 'PATCH', body: { ...body } };
+      },
+      invalidatesTags: ['roles'],
+      async onQueryStarted(id, { dispatch, queryFulfilled }) {
+        try {
+          //UTIL  just good stuff :   memberApi.util.resetApiState
+          await queryFulfilled;
+          dispatch(memberApi.util.invalidateTags(['members']));
+        } catch (err) {
+          //console.log(err);
+        }
+      },
+    }),
+    deleteRole: builder.mutation<true, number>({
+      query: (roleId) => {
+        return { url: `/${roleId}`, method: 'DELETE' };
+      },
+      invalidatesTags: ['roles'],
+      async onQueryStarted(id, { dispatch, queryFulfilled }) {
+        try {
+          //UTIL  just good stuff :   memberApi.util.resetApiState
+          await queryFulfilled;
+          dispatch(memberApi.util.invalidateTags(['members']));
+        } catch (err) {
+          //console.log(err);
+        }
+      },
     }),
   }),
 });
@@ -60,4 +114,7 @@ export const {
   useGetRolesQuery,
   useUpdateRoleMutation,
   useGetRoleByIdQuery,
+  useAssignRoleMutation,
+  useRevokeRoleMutation,
+  useDeleteRoleMutation,
 } = roleApi;
