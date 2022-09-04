ba164904993f1b079b01cac10eda487de7c246d0
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-09-04 21:42:51 +0100

    🐛 transformResponse of getMembers

--- src/renderer/services/redux/clinic/rbac/member/memberApi.ts
+++ src/renderer/services/redux/clinic/rbac/member/memberApi.ts
@@ -2,15 +2,24 @@
 import { Member, MemberBrief, PermKeys } from '@models/server.models';
 import { StaticQueries } from '@redux/dynamic_queries';
 import { createApi } from '@reduxjs/toolkit/dist/query/react';
+import { parseISO } from 'date-fns';
 
 const memberApi = createApi({
   reducerPath: 'memberApi',
   baseQuery: StaticQueries.members.query,
   tagTypes: ['members'],
   endpoints: (builder) => ({
-    getMembers: builder.query<MemberBrief, void>({
+    getMembers: builder.query<MemberBrief[], void>({
       query: () => '',
       providesTags: ['members'],
+      transformResponse: (
+        response: (Omit<MemberBrief, 'joinDate'> & { joinDate: string })[],
+      ) => {
+        return response.map(({ joinDate, ...other }) => ({
+          ...other,
+          joinDate: parseISO(joinDate),
+        }));
+      },
     }),
 
     getMemberById: builder.query<MemberBrief, number>({
