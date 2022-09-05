bd9307a13979a4dec28b4c0c6ca1799fedddf7e9
danzo7 <aymenmiro@gmail.com>
2022-09-05 16:50:42 +0100

    🗑️ cleanup

--- src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/members_tab/MembersTab.tsx
+++ src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/members_tab/MembersTab.tsx
@@ -4,15 +4,13 @@ import CreateInvitationModal from '@containers/modals/create_invitation_modal';
 import { DEFAULT_MODAL } from '@libs/overlay';
 import { useOverlay } from '@libs/overlay/useOverlay';
 import { useGetBriefRolesQuery } from '@redux/clinic/rbac/role/roleApi';
-import { clearAddedRoles } from '@redux/local/small_role_invSlice';
-import { useAppDispatch } from '@store';
+
 import RoleMembers from './role_members';
 import './style/index.scss';
 
 interface MembersTabProps {}
 export default function MembersTab({}: MembersTabProps) {
   const { open } = useOverlay();
-  const dispatch = useAppDispatch();
 
   const { data, isLoading, isSuccess, error } = useGetBriefRolesQuery();
   return (
@@ -20,7 +18,6 @@ export default function MembersTab({}: MembersTabProps) {
       <div className="add-member-wrapper">
         <DarkAddButton
           onPress={() => {
-            dispatch(clearAddedRoles());
             open(<CreateInvitationModal />, {
               ...DEFAULT_MODAL,
               position: { top: '30%' },
