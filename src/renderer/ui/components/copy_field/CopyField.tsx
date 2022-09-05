c1e1319875f15d5ffed6900fe4547c4fb723a402
danzo7 <aymenmiro@gmail.com>
2022-09-05 10:29:00 +0100

    ✨ add hint to copyfield

--- src/renderer/ui/components/copy_field/CopyField.tsx
+++ src/renderer/ui/components/copy_field/CopyField.tsx
@@ -1,28 +1,33 @@
 import { color } from '@assets/styles/color';
 import TextButton from '@components/buttons/text_button';
+import InputContainer from '@components/inputs/input_container';
 import InputWrapper from '@components/inputs/input_wrapper';
 
 interface CopyFieldProps {
   text: string;
+  hint?: string;
 }
-export default function CopyField({ text }: CopyFieldProps) {
+export default function CopyField({ text, hint }: CopyFieldProps) {
   return (
-    <InputWrapper
-      trailing={
-        <TextButton
-          text="Copy"
-          backgroundColor={color.cold_blue}
-          fontSize={13}
-          fontWeight={700}
-          alignSelf="center"
-          padding={5}
-          onPress={() => {
-            navigator.clipboard.writeText(text);
-          }}
-        />
-      }
-    >
-      <span css={{ userSelect: 'all', cursor: 'pointer' }}>{text}</span>
-    </InputWrapper>
+    <InputContainer hint={hint} grow={true}>
+      <InputWrapper
+        fillContainer={true}
+        trailing={
+          <TextButton
+            text="Copy"
+            backgroundColor={color.cold_blue}
+            fontSize={13}
+            fontWeight={700}
+            alignSelf="center"
+            padding={5}
+            onPress={() => {
+              navigator.clipboard.writeText(text);
+            }}
+          />
+        }
+      >
+        <span css={{ userSelect: 'all', cursor: 'pointer' }}>{text}</span>
+      </InputWrapper>
+    </InputContainer>
   );
 }
