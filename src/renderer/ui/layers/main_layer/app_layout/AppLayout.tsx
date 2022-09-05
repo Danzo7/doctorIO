d89a152b0fa854de6643f686c6f7ec442b44e0af
danzo7 <aymenmiro@gmail.com>
2022-09-05 20:52:28 +0100

    🔥 remove anoying context

--- src/renderer/ui/layers/main_layer/app_layout/AppLayout.tsx
+++ src/renderer/ui/layers/main_layer/app_layout/AppLayout.tsx
@@ -1,24 +1,24 @@
-import { AbilityContext } from '@ability/Ability';
 import { buildAbilityFor } from '@ability/utils';
 import LoadingSpinner from '@components/loading_spinner';
 import AppContent from '@containers/AppContent';
 import AppMenu from '@containers/AppMenu';
 import AppSidebar from '@containers/AppSidebar';
 import { useGetMyPermissionQuery } from '@redux/clinic/rbac/member/memberApi';
+import { useAbilityStore } from '@stores/abilityStore';
 interface AppLayoutProps {}
 export default function AppLayout({}: AppLayoutProps) {
   const { data, isLoading, isSuccess } = useGetMyPermissionQuery();
+  const setAbility = useAbilityStore((state) => state.set);
+  if (isSuccess) setAbility(buildAbilityFor(data.permissions, data.lvl == 0));
 
   return isLoading ? (
     <LoadingSpinner />
   ) : isSuccess ? (
-    <AbilityContext.Provider
-      value={buildAbilityFor(data.permissions, data.lvl == 0)}
-    >
+    <>
       <AppMenu />
       <AppContent />
       <AppSidebar />
-    </AbilityContext.Provider>
+    </>
   ) : (
     <div>error here</div> //REFACTOR unothorized handle error
   );
