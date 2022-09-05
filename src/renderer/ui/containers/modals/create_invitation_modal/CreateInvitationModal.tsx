28613bc93969f936d259dd7325f3185aeecc998d
danzo7 <aymenmiro@gmail.com>
2022-09-05 10:28:42 +0100

    ⚡️ use cached member list instead of API & improve error messages

--- src/renderer/ui/containers/modals/create_invitation_modal/CreateInvitationModal.tsx
+++ src/renderer/ui/containers/modals/create_invitation_modal/CreateInvitationModal.tsx
@@ -1,4 +1,4 @@
-import color from '@assets/styles/color';
+import { color } from '@assets/styles/color';
 import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
 import MultiOptionSwitcher from '@components/buttons/multi_option_switcher';
 import TextButton from '@components/buttons/text_button';
@@ -8,95 +8,97 @@ import LoadingSpinner from '@components/loading_spinner';
 import SmallRoleList from '@components/members_preview/small_role_list';
 import MemberMiniCard from '@components/member_mini_card';
 import ModalContainer from '@components/modal_container';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { Overlay } from '@libs/overlay';
-import { ServerError } from '@models/instance.model';
 import { useCreateInvitationMutation } from '@redux/clinic/invitation/invitationApi';
-import { useLazyGetMemberByIdQuery } from '@redux/clinic/rbac/member/memberApi';
-import { useGetBriefRolesQuery } from '@redux/clinic/rbac/role/roleApi';
-import { addRole, deleteRole } from '@redux/local/small_role_invSlice';
+import { useGetMembersQuery } from '@redux/clinic/rbac/member/memberApi';
+import {
+  addRole,
+  clearAddedRoles,
+  deleteRole,
+} from '@redux/local/small_role_invSlice';
 import { useAppDispatch, useAppSelector } from '@store';
-import { useRef, useState } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import search from 'toSvg/search.svg?icon';
-import { z } from 'zod';
 import './style/index.scss';
 
 interface Inputs {
   searchField: string;
 }
-const schema = z.object({
-  searchField: z.number({ required_error: 'write the member id' }).min(1),
-});
 interface CreateInvitationModalProps {}
 export default function CreateInvitationModal({}: CreateInvitationModalProps) {
-  const { control, handleSubmit, reset, getValues } = useForm<Inputs>({
-    mode: 'onSubmit',
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { isDirty },
+  } = useForm<Inputs>({
+    mode: 'onChange',
     defaultValues: { searchField: '' },
   });
   const invTypes = ['Join', 'Relink'];
   const [selectedType, setselectedType] = useState(invTypes[0]);
-
-  const searchRef = useRef<string>('');
-  const [GetMemberById, result] = useLazyGetMemberByIdQuery();
-
+  const { data, isLoading, error, isSuccess } = useGetMembersQuery();
   const addedRole = useAppSelector(
     (state) => state.smallRoleInvSlice.addedRole,
   );
   const dispatch = useAppDispatch();
   const [CreateInvitation] = useCreateInvitationMutation();
-
-  const [invKey, setInvKey] = useState<any>();
+  const [invResult, setInvResult] = useState<any>();
+  const filterMembers = (value: string) => {
+    return data?.filter(({ name, id }) =>
+      id != 1 && value.length > 0
+        ? RegExp(`^${value.trim().replace(/\s\s+/g, ' ')}`, 'i').test(name)
+        : false,
+    );
+  };
+  const filteredMembers = filterMembers(watch('searchField'));
+  const serverError: string | undefined = (error as any)?.data?.message;
   return (
     <ModalContainer
-      onSubmit={handleSubmit((value) => {
-        if (selectedType == invTypes[1]) {
-          if (searchRef.current != value.searchField) {
-            searchRef.current = value.searchField;
-            const memId = Number(searchRef.current);
-            if (!isNaN(memId)) GetMemberById(memId, false);
-          }
-        } else {
-          const addedRoleId = addedRole.map((role) => role.id);
-          console.log('addedRoleId JOIN :', addedRoleId);
-          CreateInvitation({
-            type: 'JOIN',
-            roles: addedRoleId,
-          }).then((res) => setInvKey(res));
-        }
+      onSubmit={handleSubmit(() => {
+        const addedRoleId = addedRole.map((role) => role.id);
+        if (addedRoleId.length == 0)
+          throw new Error('Please add at least one role'); //Catch
+        CreateInvitation({
+          type: 'JOIN',
+          roles: addedRoleId,
+        }).then((res) => {
+          dispatch(clearAddedRoles());
+          setInvResult(res);
+        });
       })}
-      title={invKey ? 'Invitation Key' : 'Create an invitation key'}
+      title={invResult ? 'Invitation Key' : 'Create an invitation key'}
       controls={
         <>
-          {!invKey ? (
+          {!invResult ? (
             selectedType == invTypes[1] ? (
               <div className="result-div">
-                {result.isLoading ? (
+                {isLoading ? (
                   <LoadingSpinner />
                 ) : (
-                  result.isSuccess && (
+                  isSuccess &&
+                  data &&
+                  filteredMembers?.map((member) => (
                     <MemberMiniCard
-                      fullName={result.data.name}
-                      memberId={result.data.id}
-                      status={result.data.status}
-                      avatar={result.data.avatar}
+                      fullName={member.name}
+                      memberId={member.id}
+                      status={member.status}
+                      avatar={member.avatar}
+                      key={member.id + 'member'}
                       buttonNode={
                         <DarkLightCornerButton
                           text="Select"
                           onPress={() => {
-                            const addedRoleId = result.data.roles.map(
-                              (role) => role.id,
-                            );
-                            console.log('addedRoleId :', addedRoleId);
                             CreateInvitation({
                               type: 'RELINK',
-                              roles: addedRoleId,
-                            }).then((res) => setInvKey(res));
+                              relinkToId: member.id,
+                            }).then((res) => setInvResult(res));
                           }}
                         />
                       }
                     />
-                  )
+                  ))
                 )}
               </div>
             ) : (
@@ -127,7 +129,7 @@ export default function CreateInvitationModal({}: CreateInvitationModalProps) {
         </>
       }
     >
-      {!invKey ? (
+      {!invResult ? (
         <div className="invitation-div">
           <MultiOptionSwitcher
             onChange={(selectedIndex) => {
@@ -141,10 +143,15 @@ export default function CreateInvitationModal({}: CreateInvitationModalProps) {
             <Input
               fillContainer
               placeholder="Select a member..."
-              trailing={<TextButton Icon={search} blank type="submit" />}
+              trailing={<TextButton Icon={search} />}
               type="search"
               name="searchField"
               control={control}
+              errorMessage={
+                isDirty && filteredMembers?.length == 0
+                  ? 'No member founds'
+                  : serverError
+              }
             />
           ) : (
             <div className="role-list-div">
@@ -160,10 +167,16 @@ export default function CreateInvitationModal({}: CreateInvitationModalProps) {
             </div>
           )}
         </div>
-      ) : invKey?.data ? (
-        <CopyField text={(invKey as any).data.key} />
+      ) : invResult?.data ? (
+        <CopyField
+          text={(invResult as any).data.key}
+          hint="The invitation key is valid for 1 minute"
+        />
       ) : (
-        <span> something went wrong </span>
+        <span>
+          {' '}
+          {invResult?.error?.data?.message ?? 'Something went wrong'}{' '}
+        </span>
       )}
     </ModalContainer>
   );
