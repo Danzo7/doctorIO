188809c99554941824acd3a8ef83c467a6643224
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-09-06 23:07:02 +0100

    ➕ add masterRole to RoleItem

--- src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/roles_tab/role_list/role_item/RoleItem.tsx
+++ src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/roles_tab/role_list/role_item/RoleItem.tsx
@@ -2,11 +2,18 @@ import './style/index.scss';
 import { RoleBrief } from '@models/server.models';
 import { useSearchParams } from 'react-router-dom';
 import roleApi from '@redux/clinic/rbac/role/roleApi';
+import Icon from 'toSvg/link.svg?icon';
 
 interface RoleItemProps {
   selected: boolean;
+  masterRole?: RoleBrief;
 }
-function RoleItem({ id, name, selected }: RoleBrief & RoleItemProps) {
+function RoleItem({
+  id,
+  name,
+  selected,
+  masterRole,
+}: RoleBrief & RoleItemProps) {
   const [_, setSearchParams] = useSearchParams();
   return (
     <div
@@ -17,15 +24,17 @@ function RoleItem({ id, name, selected }: RoleBrief & RoleItemProps) {
       className={`role-item${selected ? ' selected' : ''}`}
     >
       <span>{name}</span>
-      {/* <div
-        className="linked"
-        css={{ display: masterRole ? 'flex' : 'none!important' }}
-      >
-        <Icon />
-        <div className="linked-role-container">
-          <span>{masterRole?.roleName}</span>
-        </div> 
-      </div>*/}
+      {masterRole && (
+        <div
+          className="linked"
+          css={{ display: masterRole ? 'flex' : 'none!important' }}
+        >
+          <Icon />
+          <div className="linked-role-container">
+            <span>{masterRole?.name}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
