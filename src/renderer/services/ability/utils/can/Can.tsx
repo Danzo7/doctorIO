85a651b38ee9bdcfeb52f49da79c9fa1ea578546
Danzo7 <aymenmiro@gmail.com>
2022-09-06 17:11:22 +0100

    ✨ add allowed and or props

--- src/renderer/services/ability/utils/can/Can.tsx
+++ src/renderer/services/ability/utils/can/Can.tsx
@@ -1,13 +1,16 @@
-import { useAbilityStore } from '@stores/abilityStore';
+import { useAbility } from '@stores/abilityStore';
 import { ReactNode } from 'react';
 import { Action, Subjects } from '../ability.factory';
 interface CanProps {
   I: Action;
   a: Subjects;
-  children: ReactNode;
+  or?: Action[];
+  children: ((isAllowed: boolean) => ReactNode) | ReactNode;
 }
-export default function Can({ I, a, children }: CanProps) {
-  const abilities = useAbilityStore((state) => state.ability);
-
-  return abilities.can(I, a) ? <>{children}</> : null;
+export default function Can({ I, a, children, or }: CanProps) {
+  const abilities = useAbility();
+  if (typeof children == 'function') return <>children(abilities.can(I, a))</>;
+  return abilities.can(I, a) && (or ? or.every(Boolean) : true) ? (
+    <>{children}</>
+  ) : null;
 }
