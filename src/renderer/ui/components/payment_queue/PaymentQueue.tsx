3c79845ec6ff168381c54caf3bf21c6ed79ef42d
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-09-07 22:36:32 +0100

    ✅ update PaymentQueue

--- src/renderer/ui/components/payment_queue/PaymentQueue.tsx
+++ src/renderer/ui/components/payment_queue/PaymentQueue.tsx
@@ -6,55 +6,23 @@ import './style/index.scss';
 import SmallInfoCard from '@components/small_info_card';
 import { useScroller } from '@libs/hooks/useScroller';
 import PaymentItem from '@components/payment_item';
-
-const smallInfoCardData = {
-  'In queue': '30',
-  unpaid: '10',
-  urgent: '2',
-};
-const items = [
-  {
-    patientFullName: 'Daouadji Aymen',
-    paymentAmount: '9000 Da',
-  },
-  {
-    patientFullName: 'Daouadji Aymen',
-    paymentAmount: '9000 Da',
-  },
-  {
-    patientFullName: 'Daouadji Aymen',
-    paymentAmount: '9000 Da',
-  },
-  {
-    patientFullName: 'Daouadji Aymen',
-    paymentAmount: '9000 Da',
-  },
-  {
-    patientFullName: 'Daouadji Aymen',
-    paymentAmount: '9000 Da',
-  },
-  {
-    patientFullName: 'Daouadji Aymen',
-    paymentAmount: '9000 Da',
-  },
-  {
-    patientFullName: 'Daouadji Aymen',
-    paymentAmount: '9000 Da',
-  },
-  {
-    patientFullName: 'Daouadji Aymen',
-    paymentAmount: '9000 Da',
-  },
-];
+import { useGetPaymentsQuery } from '@redux/instance/Appointment/AppointmentApi';
+import LoadingSpinner from '@components/loading_spinner';
+import BorderSeparator from '@components/border_separator';
 
 interface PaymentQueueProps {}
 export default function PaymentQueue({}: PaymentQueueProps) {
   const { ref, gotoFirst, gotoLast, next, previous } = useScroller(10);
+  const { data, isSuccess, isLoading, error } = useGetPaymentsQuery();
+  const smallInfoCardData = {
+    'In queue': (isSuccess && data.length > 0 ? data.length : 0).toString(),
+    unpaid: '0', //TODO add unpaid value
+  };
   return (
     <div className="payment-queue">
       <div className="payment-queue-content">
         <SmallInfoCard data={smallInfoCardData} />
-        <div className="payment-queue-sep" />
+        <BorderSeparator direction="vertical" />
         <div className="payment-queue-list">
           <TextButton
             borderColor={colors.border_color}
@@ -65,19 +33,20 @@ export default function PaymentQueue({}: PaymentQueueProps) {
           >
             <Arrow css={{ transform: 'rotate(90deg)' }} />
           </TextButton>
-          {items.length > 0 ? (
+          {isLoading ? (
+            <LoadingSpinner />
+          ) : isSuccess && data.length > 0 ? (
             <ScrollView refs={ref} gap={10}>
-              {items.map(({ patientFullName, paymentAmount }, index) => (
-                <li key={patientFullName + index}>
-                  <PaymentItem
-                    patientFullName={patientFullName}
-                    paymentAmount={paymentAmount}
-                  />
+              {data.map(({ appointmentId, amount, name, date }, index) => (
+                <li key={appointmentId + index}>
+                  <PaymentItem patientFullName={name} amount={amount} />
                 </li>
               ))}
             </ScrollView>
           ) : (
-            <span>nothing...</span>
+            <div className="nothing-div">
+              <span>nothing...</span>
+            </div>
           )}
           <TextButton
             borderColor={colors.border_color}
