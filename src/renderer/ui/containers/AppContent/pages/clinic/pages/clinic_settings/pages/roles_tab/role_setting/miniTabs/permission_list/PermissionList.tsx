932ace919eb2d74c8eb172e18b3aac90392ad0e5
danzo7 <aymenmiro@gmail.com>
2022-09-07 15:32:21 +0100

    ✨ implement permissions

--- src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/roles_tab/role_setting/miniTabs/permission_list/PermissionList.tsx
+++ src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/roles_tab/role_setting/miniTabs/permission_list/PermissionList.tsx
@@ -1,10 +1,14 @@
 import PermissionItem from './Permission_item';
 import { PERMISSIONS } from '@constants/permissions';
 import './style/index.scss';
-import { useGetPermissionsSettings } from '@stores/roleSettingStore';
+import {
+  useGetPermissionsSettings,
+  useSetSettings,
+} from '@stores/roleSettingStore';
 
 export default function PermissionList() {
   const permissions = useGetPermissionsSettings();
+  const setSettings = useSetSettings();
   return (
     <div className="permission-list">
       {PERMISSIONS.map(({ description, permKey, name }) => (
@@ -14,6 +18,18 @@ export default function PermissionList() {
           description={description}
           editable={true}
           key={permKey}
+          onChange={(isChecked) => {
+            if (isChecked) {
+              setSettings({
+                permissions: [...permissions, permKey],
+              });
+            }
+            if (!isChecked) {
+              setSettings({
+                permissions: permissions.filter((p) => p !== permKey),
+              });
+            }
+          }}
         />
       ))}
     </div>
