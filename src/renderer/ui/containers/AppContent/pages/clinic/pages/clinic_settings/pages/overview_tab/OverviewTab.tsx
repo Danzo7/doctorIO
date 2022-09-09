8dac8932ded5772166f3bc938d7be9ad8087fd30
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-09-09 22:11:24 +0100

    ✅ update OverviewTab

--- src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/overview_tab/OverviewTab.tsx
+++ src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/overview_tab/OverviewTab.tsx
@@ -2,21 +2,47 @@ import ClinicOverviewCard from '@components/clinic_overview_card';
 import OverviewInfoForm from './overview_info_form';
 import './style/index.scss';
 import { useAppSelector } from '@store';
+import { useGetClinicQuery } from '@redux/clinic/clinicApi';
+import {
+  useOverViewInfo,
+  useSetDefaults,
+  useSetOverViewInfo,
+} from '@stores/overViewinfoStore';
+import { clinic } from '@api/fake';
+import LoadingSpinner from '@components/loading_spinner';
 
 interface OverviewTabProps {}
 export default function OverviewTab({}: OverviewTabProps) {
   //REDUX fetch clinic info
   const clinicInfo = useAppSelector((state) => state.settings);
+  // const setOverviewInfo = useSetOverViewInfo();
+
+  // const info = useOverViewInfo();
+  const setDefaults = useSetDefaults();
+  const info = useOverViewInfo();
+
+  const { data, error, isSuccess, isLoading } = useGetClinicQuery();
+  if (info.defaults == undefined && !isLoading) {
+    setDefaults({
+      name: isSuccess ? data.name : 'bs',
+      description: isSuccess ? data.description : 'bs',
+      address: isSuccess ? data.address : '',
+      phone: isSuccess ? data.phone : '',
+    });
+  }
   return (
-    <div className="overview-tab">
-      <ClinicOverviewCard {...clinicInfo} />
-      <div className="overview-tab-sep" />
-      <OverviewInfoForm
-        name={clinicInfo.name}
-        description={clinicInfo.description ?? ''}
-        address={clinicInfo.address}
-        phone={clinicInfo.phone ?? ''}
-      />
-    </div>
+    <>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        isSuccess && (
+          <div className="overview-tab">
+            <ClinicOverviewCard {...clinicInfo} />
+            <div className="overview-tab-sep" />
+            <OverviewInfoForm />
+          </div>
+        )
+      )}
+    </>
   );
 }
