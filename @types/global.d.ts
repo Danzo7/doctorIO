090e92b75f4786e14c506305f3cb821e78b80fab
danzo7 <aymenmiro@gmail.com>
2022-09-09 01:58:11 +0100

    🗑️ ckeanup

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -2,15 +2,6 @@
 /* Use this file to declare any custom file extensions for importing */
 /* Use this folder to also add/extend a package d.ts file, if needed. */
 
-interface Window {
-  api: {
-    send: (channel: string, data: any) => void;
-    receive: (channel: string, func: () => void) => void;
-    sayHello: () => Promise<string>;
-    // etc.
-  };
-}
-
 /* CSS MODULES */
 declare module '*.module.css' {
   const classes: { [key: string]: string };
