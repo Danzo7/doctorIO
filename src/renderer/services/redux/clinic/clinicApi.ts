2df62c99507a80c6571e54df021e5276c8b3655a
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-09-09 16:26:28 +0100

    ➕ add clinicApi

---
+++ src/renderer/services/redux/clinic/clinicApi.ts
@@ -0,0 +1,25 @@
+import { Clinic } from '@models/server.models';
+import { StaticQueries } from '@redux/dynamic_queries';
+import { createApi } from '@reduxjs/toolkit/dist/query/react';
+
+const clinicApi = createApi({
+  reducerPath: 'clinicApi',
+  baseQuery: StaticQueries.clinic.query,
+  tagTypes: ['clinic'],
+  endpoints: (builder) => ({
+    getClinic: builder.query<Clinic, void>({
+      query: () => '',
+      providesTags: ['clinic'],
+    }),
+    updateClinicOverview: builder.mutation<
+      boolean,
+      { name: string; description: string }
+    >({
+      query: (body) => {
+        return { url: '', body: { ...body }, method: 'PATCH' };
+      },
+    }),
+  }),
+});
+export default clinicApi;
+export const { useGetClinicQuery, useUpdateClinicOverviewMutation } = clinicApi;
