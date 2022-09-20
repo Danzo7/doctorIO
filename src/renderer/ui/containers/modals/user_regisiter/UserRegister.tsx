1fc3283edca33b80c5797a78a085e6e5048ff731
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2022-09-20 17:56:58 +0100

    ✅  update UserRegister

--- src/renderer/ui/containers/modals/user_regisiter/UserRegister.tsx
+++ src/renderer/ui/containers/modals/user_regisiter/UserRegister.tsx
@@ -13,13 +13,19 @@ const schema = z.object({
   firstName: z.string().min(3, 'first name is required'),
   lastName: z.string().min(3, 'last name is required'),
   email: z.string().email({ message: 'Invalid email address' }),
-  phoneNumber: z.string().min(8),
+  phoneNumber: z.string().min(8).optional(),
+  age: z.string().min(2),
+  address: z.string().min(3).optional(),
+  gender: z.enum(['male', 'female']),
 });
 interface Inputs {
   firstName: string;
   lastName: string;
+  gender: 'male' | 'female';
+  age: string;
   email: string;
   phoneNumber: string;
+  address: string;
 }
 interface UserRegisterProps {}
 
@@ -29,13 +35,27 @@ export default function UserRegister({}: UserRegisterProps) {
     defaultValues: {
       firstName: '',
       lastName: '',
+      gender: 'male',
       email: '',
       phoneNumber: '',
+      age: '',
+      address: '',
     },
   });
   const dispatch = useAppDispatch();
   const onSubmit: SubmitHandler<Inputs> = (formData) => {
-    dispatch(setUser(formData));
+    console.log('form data :', formData);
+    dispatch(
+      setUser({
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        age: formData.age,
+        email: formData.email,
+        gender: formData.gender,
+        phoneNumber: formData.phoneNumber,
+        address: formData.address,
+      }),
+    );
     Overlay.close();
   };
   return (
@@ -56,8 +76,18 @@ export default function UserRegister({}: UserRegisterProps) {
       <Inputix control={control}>
         <Input type="text" label="First name" name="firstName" />
         <Input type="text" label="last name" name="lastName" />
+        <Input
+          type={{
+            type: 'select',
+            options: ['male', 'female'],
+          }}
+          label="Gender"
+          name="gender"
+        />
+        <Input type="text" label="Age" name="age" />
         <Input type="email" label="Email" name="email" />
         <Input type="text" label="Phone number" name="phoneNumber" />
+        <Input type="text" label="Address" name="address" />
       </Inputix>
     </ModalContainer>
   );
