b1983396f92d3c7ed1922963b5b6d5dc2e34dc6e
Danzo7 <aymenmiro@gmail.com>
2022-11-02 22:48:26 +0100

    🔥 remove unused code

--- src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/roles_tab/role_list/RoleList.tsx
+++ src/renderer/ui/containers/AppContent/pages/clinic/pages/clinic_settings/pages/roles_tab/role_list/RoleList.tsx
@@ -1,36 +1,12 @@
 import NewRole from './new_role';
 import RoleItem from './role_item';
-import { useEffect, useState } from 'react';
 import './style/index.scss';
-import { Droppable, DroppableProps } from 'react-beautiful-dnd';
 import { useSearchParams } from 'react-router-dom';
 import { useGetBriefRolesQuery } from '@redux/clinic/rbac/role/roleApi';
 import LoadingSpinner from '@components/loading_spinner';
 import Can from '@ability/index';
 
-interface RoleListProps {
-  selected?: number;
-}
-const StrictModeDroppable = ({ children, ...props }: DroppableProps) => {
-  const [enabled, setEnabled] = useState(false);
-
-  useEffect(() => {
-    const animation = requestAnimationFrame(() => setEnabled(true));
-
-    return () => {
-      cancelAnimationFrame(animation);
-      setEnabled(false);
-    };
-  }, []);
-
-  if (!enabled) {
-    return null;
-  }
-
-  return <Droppable {...props}>{children}</Droppable>;
-};
-
-export default function RoleList({}: RoleListProps) {
+export default function RoleList() {
   const [searchParams] = useSearchParams();
 
   const { data, isLoading, isFetching, isSuccess, error } =
