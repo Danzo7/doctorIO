ac2e05a8c4743050d8a7f98d07d8c52ebcd315be
Danzo7 <aymenmiro@gmail.com>
2022-11-03 20:08:44 +0100

    ⚡️ get clinic information from query cache

--- src/renderer/ui/components/printed_layout/PrintedLayout.tsx
+++ src/renderer/ui/components/printed_layout/PrintedLayout.tsx
@@ -1,66 +1,71 @@
+import LoadingSpinner from '@components/loading_spinner';
 import { Drug } from '@models/instance.model';
+import { useGetClinicQuery } from '@redux/clinic/clinicApi';
 import { useRef } from 'react';
 import { useReactToPrint } from 'react-to-print';
 import './style/index.scss';
 interface PrintedLayoutProps {
-  clinicName: string;
-  ClinicAddress: string;
   doctorName: string;
   patientName: string;
   patientAge: number;
   drugList: Drug[];
 }
 export default function PrintedLayout({
-  clinicName,
-  ClinicAddress,
   doctorName,
   patientName,
   patientAge,
   drugList,
 }: PrintedLayoutProps) {
+  const { isSuccess, isLoading, data } = useGetClinicQuery();
   const componentRef = useRef<HTMLDivElement | null>();
   const handlePrint = useReactToPrint({
     content: () => componentRef.current as HTMLDivElement,
   });
   return (
     <div css={{ display: 'none' }}>
-      <div
-        ref={(e) => {
-          if (componentRef) componentRef.current = e;
-          handlePrint();
-        }}
-        className="printed-layout"
-      >
-        <div className="printed-layout-header">
-          <span>المؤسسة العمومية للصحة</span>
-          <span>{clinicName}</span>
-        </div>
-        <span>ORDONNACE</span>
-        <div className="info-container">
-          <span>
-            {ClinicAddress}, le : {Date.now()}
-          </span>
-          <span>Delivre par le Docteur : {doctorName}</span>
-          <div className="patient-info">
-            <span>a M. {patientName}</span>
-            <span>Age : {patientAge}</span>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : isSuccess ? (
+        <div
+          ref={(e) => {
+            if (componentRef) componentRef.current = e;
+            handlePrint();
+          }}
+          className="printed-layout"
+        >
+          <div className="printed-layout-header">
+            <span>المؤسسة العمومية للصحة</span>
+            <span>{data.name}</span>
+          </div>
+          <span>ORDONNACE</span>
+          <div className="info-container">
+            <span>
+              {data.address}, le : {Date.now()}
+            </span>
+            <span>Delivre par le Docteur : {doctorName}</span>
+            <div className="patient-info">
+              <span>a M. {patientName}</span>
+              <span>Age : {patientAge}</span>
+            </div>
+          </div>
+
+          <div className="drug-list">
+            {drugList.map(
+              ({ name, dosage, qts, duration, description }, index) => (
+                <span key={index}>
+                  {name} {dosage} CP /JOUR qsp {qts} JOURS {duration + ' '}
+                  {description}
+                </span>
+              ),
+            )}
+          </div>
+          <div className="bottom-info">
+            <span>بالقليل من الدم الذي تتبرعون به تنقذون حياة انسان</span>
           </div>
         </div>
-
-        <div className="drug-list">
-          {drugList.map(
-            ({ name, dosage, qts, duration, description }, index) => (
-              <span key={index}>
-                {name} {dosage} CP /JOUR qsp {qts} JOURS {duration + ' '}
-                {description}
-              </span>
-            ),
-          )}
-        </div>
-        <div className="bottom-info">
-          <span>بالقليل من الدم الذي تتبرعون به تنقذون حياة انسان</span>
-        </div>
-      </div>
+      ) : (
+        <div>Error</div>
+      )}
     </div>
   );
 }
