26c41c704336cc08719c7dd5ddf6a68f713df762
Danzo7 <aymenmiro@gmail.com>
2022-11-03 20:11:56 +0100

    ⚰️ remove unused code

--- src/renderer/services/redux/index.ts
+++ src/renderer/services/redux/index.ts
@@ -1,5 +1,2 @@
-import { useDispatch, TypedUseSelectorHook, useSelector } from 'react-redux';
-import { AppDispatch, RootState, store } from './store';
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+import { store } from './store';
 export default store;
