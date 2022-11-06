da6077a3de5bdd1453eb525aca731cf2fffeeef9
Danzo7 <aymenmiro@gmail.com>
2022-11-06 01:51:15 +0100

    ⚡️ Self handle actions

--- src/renderer/ui/components/recent_apps_item/RecentAppsItem.tsx
+++ src/renderer/ui/components/recent_apps_item/RecentAppsItem.tsx
@@ -2,21 +2,24 @@ import TextButton from '@components/buttons/text_button';
 import { color } from '@colors';
 import PresentationItem from '@components/presentation_item';
 import { useOverlay } from '@libs/overlay/useOverlay';
-import MedicalTestModal from '@containers/modals/Medical_Test_Modal';
 import { PatientBrief } from '@models/instance.model';
 import { DEFAULT_MODAL, Overlay } from '@libs/overlay';
+import AddMedicalTestModal from '@containers/modals/add_medical_test_modal';
+import { useAddQueueAppointmentMutation } from '@redux/instance/appointmentQueue/AppointmentQueueApi';
+import { useAssignAppointmentToQueueMutation } from '@redux/instance/Appointment/AppointmentApi';
 
 interface RecentAppsItemProps {
-  onAdd: () => void;
   appointmentId?: number;
 }
 export default function RecentAppsItem({
   id,
   name,
   appointmentId,
-  onAdd,
 }: PatientBrief & RecentAppsItemProps) {
   const { open } = useOverlay();
+  const [addAppointment] = useAddQueueAppointmentMutation();
+  const [AssignAppointmentToQueue] = useAssignAppointmentToQueueMutation();
+
   return (
     <PresentationItem primaryText={name} secondaryText={`# ${id}`}>
       <TextButton
@@ -25,7 +28,22 @@ export default function RecentAppsItem({
         radius={7}
         onPress={() => {
           open(
-            <MedicalTestModal patientId={id} appointmentId={appointmentId} />,
+            <AddMedicalTestModal
+              onSubmit={(data) => {
+                if (appointmentId)
+                  AssignAppointmentToQueue({
+                    appointmentId: appointmentId,
+                    test: data,
+                  });
+                else
+                  addAppointment({
+                    patientId: id,
+                    test: data,
+                  });
+
+                Overlay.close();
+              }}
+            />,
             DEFAULT_MODAL,
           );
         }}
@@ -35,7 +53,15 @@ export default function RecentAppsItem({
         backgroundColor={color.good_green}
         radius={7}
         onPress={() => {
-          onAdd();
+          if (appointmentId)
+            AssignAppointmentToQueue({
+              appointmentId: appointmentId,
+            });
+          else
+            addAppointment({
+              patientId: id,
+            });
+
           Overlay.close();
         }}
       />
