1321fb4a2798a6eb88ca0b50d1c748205975cfd4
daouadji506 <daouadji506@gmail.com>
2022-11-06 19:50:38 +0100

    ✏️ fix WelcomeBox message

--- src/renderer/ui/containers/AppContent/pages/home/Home.tsx
+++ src/renderer/ui/containers/AppContent/pages/home/Home.tsx
@@ -1,14 +1,27 @@
+import LoadingSpinner from '@components/loading_spinner';
 import MembersPanel from '@components/members_panel';
 import ShortStatsPanel from '@components/short_stats_panel';
 import SmallClinicStatus from '@components/small_clinic_status';
 import WelcomeBox from '@components/welcome_box';
+import { useGetMyMemberDetailQuery } from '@redux/clinic/rbac/member/memberApi';
 import './style/index.scss';
 interface HomeProps {}
 export default function Home({}: HomeProps) {
+  const { data, isSuccess, error, isLoading } = useGetMyMemberDetailQuery();
   return (
     <>
       <div>
-        <WelcomeBox message="Welcome Boy" />
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          isSuccess && (
+            <WelcomeBox
+              message={`Welcome ${data?.gender == 'male' ? 'mr' : 'ms'} ${
+                data.name
+              }`}
+            />
+          )
+        )}
         <ShortStatsPanel />
       </div>
       <div className="home-body">
