cd9ee0f54c22e129e90fd7cc1594295c5540f721
Daouadji <daouadji506@gmail.com>
2022-11-09 23:18:11 +0100

    💄 Alert comp

---
+++ src/renderer/ui/components/alert/Alert.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps } from 'react';
+import type { Story } from '@storybook/react';
+import Alert from './Alert';
+export default {
+  title: 'Modals/Alert',
+  component: Alert,
+};
+const Template: Story<ComponentProps<typeof Alert>> = (args) => (
+  <Alert {...args} />
+);
+export const FirstStory = Template;
+FirstStory.args = {
+  text: 'Something went wrong',
+};
---
+++ src/renderer/ui/components/alert/Alert.tsx
@@ -0,0 +1,27 @@
+import { color } from '@assets/styles/color';
+import StatusIcon from '@components/status_icon';
+import './style/index.scss';
+interface AlertProps {
+  status: 'Success' | 'warning' | 'error';
+  text: string;
+}
+export default function Alert({ status, text }: AlertProps) {
+  return (
+    <div
+      className="alert"
+      css={{
+        border: `1px solid  ${
+          status == 'Success'
+            ? color.good_green
+            : status == 'warning'
+            ? color.warm_orange
+            : color.hot_red
+        }`,
+      }}
+    >
+      <StatusIcon status={status} borderRadius={7} />
+
+      <span>{text} </span>
+    </div>
+  );
+}
