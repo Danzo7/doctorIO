ce6896aabe22e83d348c3ee135f768d982ac62d3
Daouadji <daouadji506@gmail.com>
2022-11-10 00:17:36 +0100

    🎨 make LoadingSpinner dynamic

--- src/renderer/ui/components/loading_spinner/LoadingSpinner.tsx
+++ src/renderer/ui/components/loading_spinner/LoadingSpinner.tsx
@@ -1,9 +1,25 @@
+import { color } from '@assets/styles/color';
 import './style/index.scss';
-interface LoadingSpinnerProps {}
-export default function LoadingSpinner({}: LoadingSpinnerProps) {
+interface LoadingSpinnerProps {
+  borderTopColor?: string;
+  borderColor?: string;
+  borderWidth?: number;
+}
+export default function LoadingSpinner({
+  borderTopColor = color.border_color,
+  borderColor = color.secondary_color,
+  borderWidth = 3,
+}: LoadingSpinnerProps) {
   return (
     <div className="loading-container">
-      <div className="loading-spinner" />
+      <div
+        className="loading-spinner"
+        css={{
+          borderWidth: borderWidth,
+          borderColor: borderColor,
+          borderTopColor: borderTopColor,
+        }}
+      />
     </div>
   );
 }
