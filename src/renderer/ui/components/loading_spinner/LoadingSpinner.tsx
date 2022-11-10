b69edaa0b474a902bb0c869416c77d3faa6317e1
Danzo7 <aymenmiro@gmail.com>
2022-11-10 18:20:34 +0100

    🎨 Use loading logo

--- src/renderer/ui/components/loading_spinner/LoadingSpinner.tsx
+++ src/renderer/ui/components/loading_spinner/LoadingSpinner.tsx
@@ -1,25 +1,13 @@
-import { color } from '@assets/styles/color';
 import './style/index.scss';
-interface LoadingSpinnerProps {
-  borderTopColor?: string;
-  borderColor?: string;
-  borderWidth?: number;
-}
-export default function LoadingSpinner({
-  borderTopColor = color.border_color,
-  borderColor = color.secondary_color,
-  borderWidth = 3,
-}: LoadingSpinnerProps) {
+import Loading from 'toSvg/loading_logo.svg?icon';
+
+interface LoadingSpinnerProps {}
+export default function LoadingSpinner({}: LoadingSpinnerProps) {
   return (
     <div className="loading-container">
-      <div
-        className="loading-spinner"
-        css={{
-          borderWidth: borderWidth,
-          borderColor: borderColor,
-          borderTopColor: borderTopColor,
-        }}
-      />
+      <div className="loading-spinner">
+        <Loading width={50} height={50} />
+      </div>
     </div>
   );
 }
