beec3e141ea60fedf55e7d0987c80a86fe71714b
Danzo7 <aymenmiro@gmail.com>
2022-11-11 00:15:31 +0100

    🐛 Fix story

--- src/renderer/ui/components/loading_modal/LoadingModal.stories.tsx
+++ src/renderer/ui/components/loading_modal/LoadingModal.stories.tsx
@@ -1,7 +1,6 @@
 import { ComponentProps } from 'react';
 import type { Story } from '@storybook/react';
 import LoadingModal from './LoadingModal';
-import GoodMark from 'toSvg/good_mark.svg?icon';
 export default {
   title: 'Modals/LoadingModal',
   component: LoadingModal,
@@ -11,7 +10,6 @@ const Template: Story<ComponentProps<typeof LoadingModal>> = (args) => (
 );
 export const FirstStory = Template;
 FirstStory.args = {
-  Icon: GoodMark,
   title: 'Reconnicting...',
   description: 'Trying to reconnect back to the server',
 };
