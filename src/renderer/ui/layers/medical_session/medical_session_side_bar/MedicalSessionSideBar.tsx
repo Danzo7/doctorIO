d93c503a7e291c6f967c906a16e5e76cb74007a1
Daouadji <daouadji506@gmail.com>
2022-11-12 23:09:50 +0100

    ✅ update MedicalSessionSideBar

--- src/renderer/ui/layers/medical_session/medical_session_side_bar/MedicalSessionSideBar.tsx
+++ src/renderer/ui/layers/medical_session/medical_session_side_bar/MedicalSessionSideBar.tsx
@@ -1,51 +1,22 @@
 import AppointmentHistoryPanel from '@components/appointment_history_panel';
-import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
-import LoadingSpinner from '@components/loading_spinner';
 import MedicalHistory from '@components/medical_history';
 import PreviewInfo from '@components/preview_info';
-import DiagnosisPreview from '@containers/modals/diagnosis_preview';
-import { DEFAULT_MODAL } from '@libs/overlay';
-import { useOverlay } from '@libs/overlay/useOverlay';
-import { useGetPatientDetailQuery } from '@redux/instance/record/patient_api';
+import { Test } from '@models/instance.model';
 import './style/index.scss';
 
 interface MedicalSessionSideBarProps {
   patientId: number;
+  test?: Test;
 }
 export default function MedicalSessionSideBar({
   patientId,
+  test,
 }: MedicalSessionSideBarProps) {
-  const { open } = useOverlay();
-  const { isSuccess, data, isFetching, isLoading, error } =
-    useGetPatientDetailQuery(patientId);
-
   return (
     <div className="medical-session-side-bar">
       <div className="medical-session-side-bar-content">
-        {isLoading || isFetching ? (
-          <LoadingSpinner />
-        ) : (
-          isSuccess &&
-          data.test && (
-            <PreviewInfo
-              title="Diagnosis"
-              data={data.test}
-              buttonNode={
-                <DarkLightCornerButton
-                  text="preview"
-                  blend
-                  onPress={() => {
-                    if (data.test)
-                      open(
-                        <DiagnosisPreview data={data.test} />,
-                        DEFAULT_MODAL,
-                      );
-                  }}
-                />
-              }
-            />
-          )
-        )}
+        {test && <PreviewInfo title="Diagnosis" data={test} />}
+
         <MedicalHistory patientId={patientId} />
         <AppointmentHistoryPanel patientId={patientId} />
       </div>
