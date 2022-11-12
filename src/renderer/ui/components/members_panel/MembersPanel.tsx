c47039439a43811872068879e392adee47fc772e
Daouadji <daouadji506@gmail.com>
2022-11-12 23:07:41 +0100

    ✨ add members filter

--- src/renderer/ui/components/members_panel/MembersPanel.tsx
+++ src/renderer/ui/components/members_panel/MembersPanel.tsx
@@ -1,16 +1,27 @@
 import Can from '@ability/index';
+import BorderSeparator from '@components/border_separator';
 import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
 import Header from '@components/header';
 import LoadingSpinner from '@components/loading_spinner';
 import MembersPreview from '@components/members_preview';
 import useNavigation from '@libs/hooks/useNavigation';
-import { useGetMembersQuery } from '@redux/clinic/rbac/member/memberApi';
+import { MemberBrief } from '@models/server.models';
+import {
+  useGetMembersQuery,
+  useGetMyMemberDetailQuery,
+} from '@redux/clinic/rbac/member/memberApi';
 import './style/index.scss';
 
 interface MembersPanelProps {}
 function MembersPanel({}: MembersPanelProps) {
   const { data, isLoading, isSuccess, error } = useGetMembersQuery();
   const { navigate } = useNavigation();
+  const onlineMembers = data?.filter((member) => member.status);
+  const offlineMembers = data?.filter((member) => !member.status);
+  const GetMyMemberDetailQuery = useGetMyMemberDetailQuery();
+  const assistants: MemberBrief[] = [];
+  const myRoles = GetMyMemberDetailQuery.data?.roles.map((role) => {});
+
   return (
     <div className="members-panel">
       <Header
@@ -32,9 +43,31 @@ function MembersPanel({}: MembersPanelProps) {
             <LoadingSpinner />
           ) : isSuccess ? (
             data.length > 0 ? (
-              data.map((member) => (
-                <MembersPreview {...member} key={member.id.toString()} />
-              ))
+              <>
+                {assistants.length > 0 && (
+                  <div className="members-filter-container">
+                    <span>Assistants</span>
+                    <BorderSeparator direction="horizontal" />
+                    {assistants?.map((member) => (
+                      <MembersPreview {...member} key={member.id.toString()} />
+                    ))}
+                  </div>
+                )}
+                <div className="members-filter-container">
+                  <span>Online</span>
+                  <BorderSeparator direction="horizontal" />
+                  {onlineMembers?.map((member) => (
+                    <MembersPreview {...member} key={member.id.toString()} />
+                  ))}
+                </div>
+                <div className="members-filter-container">
+                  <span>Offline</span>
+                  <BorderSeparator direction="horizontal" />
+                  {offlineMembers?.map((member) => (
+                    <MembersPreview {...member} key={member.id.toString()} />
+                  ))}
+                </div>
+              </>
             ) : (
               <div>No Members</div>
             )
