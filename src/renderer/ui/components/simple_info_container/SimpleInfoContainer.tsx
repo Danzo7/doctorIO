9b1ec7a4d63d79d875db2dfe49f215b82f15c4d9
Daouadji <daouadji506@gmail.com>
2022-11-13 20:22:45 +0100

    ✅ update SimpleInfoContainer

--- src/renderer/ui/components/simple_info_container/SimpleInfoContainer.tsx
+++ src/renderer/ui/components/simple_info_container/SimpleInfoContainer.tsx
@@ -1,15 +1,19 @@
+import { ReactNode } from 'react';
 import './style/index.scss';
 interface SimpleInfoContainerProps {
   text: string;
   alignSelf?: 'center' | 'flex-start' | 'flex-end' | 'stretch';
+  children?: ReactNode;
 }
 export default function SimpleInfoContainer({
   text,
   alignSelf,
+  children,
 }: SimpleInfoContainerProps) {
   return (
     <div className="simple-info-container" css={{ alignSelf: alignSelf }}>
       <span>{text}</span>
+      {children}
     </div>
   );
 }
