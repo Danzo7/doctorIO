6c422884d02d925fc4a6436b5b1a6b448e603fbf
Daouadji <daouadji506@gmail.com>
2022-11-14 21:20:30 +0100

    ✨ show VerticalPanel when No booked appointments

--- src/renderer/ui/components/booked_appointment_panel/BookedAppointmentPanel.tsx
+++ src/renderer/ui/components/booked_appointment_panel/BookedAppointmentPanel.tsx
@@ -1,10 +1,12 @@
 import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
 import PreviewList from '@components/preview_list';
+import VerticalPanel from '@components/vertical_panel';
 import AddSearchToBooked from '@containers/modals/add_search_to_booked';
 import { DEFAULT_MODAL } from '@libs/overlay';
 import { useOverlay } from '@libs/overlay/useOverlay';
 import { useGetBookedAppointmentQuery } from '@redux/instance/Appointment/AppointmentApi';
 import BookedAppointmentItem from './booked_appointment_item';
+import Schedule from 'toSvg/schedule.svg?icon';
 
 export default function BookedAppointmentPanel({}) {
   const { data, isSuccess } = useGetBookedAppointmentQuery();
@@ -23,10 +25,25 @@ export default function BookedAppointmentPanel({}) {
       }
       noBorder
     >
-      {isSuccess &&
+      {isSuccess && data.length > 0 ? (
         data.map((props, index) => (
           <BookedAppointmentItem {...props} key={index} />
-        ))}
+        ))
+      ) : (
+        <VerticalPanel
+          title="No booked appointments"
+          description="Start by booking an appointment. "
+          Icon={<Schedule width={'80%'} height="50%" />}
+          backgroundColor={'none'}
+          padding={'15px 0px 0 0px'}
+          action={{
+            text: 'Book appointment',
+            onClick() {
+              open(<AddSearchToBooked />, DEFAULT_MODAL);
+            },
+          }}
+        />
+      )}
     </PreviewList>
   );
 }
