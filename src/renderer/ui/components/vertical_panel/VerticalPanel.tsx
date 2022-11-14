2e8e00d14e5c59b84fe2ab26f190bf1d494f9546
Daouadji <daouadji506@gmail.com>
2022-11-14 18:29:22 +0100

    ✅ update VerticalPanel

--- src/renderer/ui/components/vertical_panel/VerticalPanel.tsx
+++ src/renderer/ui/components/vertical_panel/VerticalPanel.tsx
@@ -1,3 +1,4 @@
+import { color } from '@assets/styles/color';
 import { ReactNode } from 'react';
 import './style/index.scss';
 interface VerticalPanelProps {
@@ -6,6 +7,8 @@ interface VerticalPanelProps {
   description: string;
   action?: { text: string; onClick?: () => void };
   IconBtn?: ReactNode;
+  backgroundColor?: string;
+  padding?: number | string;
 }
 export default function VerticalPanel({
   Icon,
@@ -13,14 +16,24 @@ export default function VerticalPanel({
   description,
   action,
   IconBtn,
+  backgroundColor = color.secondary_color,
+  padding = 20,
 }: VerticalPanelProps) {
   return (
-    <div className="vertical-panel">
+    <div
+      className="vertical-panel"
+      css={{
+        background: backgroundColor,
+        padding: padding,
+        boxShadow:
+          backgroundColor == 'none' ? undefined : `0 0 5px 2px ${color.darker}`,
+      }}
+    >
       {Icon}
       <span>{title}</span>
       <div className="description-div">
         <span>
-          {description}{' '}
+          {description}
           {action && <span onClick={action.onClick}>{action.text}</span>}
         </span>
       </div>
