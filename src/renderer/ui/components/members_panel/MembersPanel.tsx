6c99a1c6bc0778e49da894d2a9bee532b2cfcdb1
Danzo7 <aymenmiro@gmail.com>
2022-11-17 16:36:31 +0100

    ✨Add List virtualization + hideBtn for offline

--- src/renderer/ui/components/members_panel/MembersPanel.tsx
+++ src/renderer/ui/components/members_panel/MembersPanel.tsx
@@ -1,27 +1,39 @@
 import Can from '@ability/index';
-import BorderSeparator from '@components/border_separator';
 import DarkLightCornerButton from '@components/buttons/dark_light_corner_button';
 import Header from '@components/header';
 import LoadingSpinner from '@components/loading_spinner';
 import MembersPreview from '@components/members_preview';
 import useNavigation from '@libs/hooks/useNavigation';
 import { MemberBrief } from '@models/server.models';
-import {
-  useGetMembersQuery,
-  useGetMyMemberDetailQuery,
-} from '@redux/clinic/rbac/member/memberApi';
+import { useGetMembersQuery } from '@redux/clinic/rbac/member/memberApi';
+import { useEffect, useRef, useState } from 'react';
+import { ViewportList } from 'react-viewport-list';
 import './style/index.scss';
-
+import UpArrow from 'toSvg/arrow.svg?icon';
+import { color } from '@assets/styles/color';
+import TextButton from '@components/buttons/text_button';
+import NotAButton from '@components/not_a_button';
 interface MembersPanelProps {}
 function MembersPanel({}: MembersPanelProps) {
-  const { data, isLoading, isSuccess, error } = useGetMembersQuery();
+  const { data, isLoading, isSuccess } = useGetMembersQuery();
   const { navigate } = useNavigation();
-  const onlineMembers = data?.filter((member) => member.status);
-  const offlineMembers = data?.filter((member) => !member.status);
-  const GetMyMemberDetailQuery = useGetMyMemberDetailQuery();
-  const assistants: MemberBrief[] = [];
-  const myRoles = GetMyMemberDetailQuery.data?.roles.map((role) => {});
+  const [showOffline, setShowOffline] = useState(false);
+  const [filteredMembers, setFilteredMembers] = useState<{
+    online: MemberBrief[];
+    offline: MemberBrief[];
+  }>({ online: [], offline: [] });
 
+  useEffect(() => {
+    if (data)
+      setFilteredMembers({
+        online: data.filter((member) => member.status),
+        offline: data.filter((member) => !member.status),
+      });
+    else return;
+  }, [data]);
+
+  const ref = useRef(null);
+  const listRef = useRef(null);
   return (
     <div className="members-panel">
       <Header
@@ -37,44 +49,68 @@ function MembersPanel({}: MembersPanelProps) {
           </Can>
         }
       />
-      <div className="members-list-container">
-        <div className="members-list">
-          {isLoading ? (
-            <LoadingSpinner />
-          ) : isSuccess ? (
-            data.length > 0 ? (
-              <>
-                {assistants.length > 0 && (
-                  <div className="members-filter-container">
-                    <span>Assistants</span>
-                    <BorderSeparator direction="horizontal" />
-                    {assistants?.map((member) => (
-                      <MembersPreview {...member} key={member.id.toString()} />
-                    ))}
+      <div className="members-list-container" ref={ref}>
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : isSuccess ? (
+          data.length > 0 ? (
+            <ViewportList
+              ref={listRef}
+              viewportRef={ref}
+              items={[
+                ...filteredMembers.online,
+                ...(showOffline ? filteredMembers.offline : []),
+              ]}
+            >
+              {(member: MemberBrief, index) => {
+                return (
+                  <div key={member.id}>
+                    {index === 0 && (
+                      <NotAButton
+                        text={'Online - ' + filteredMembers.online.length}
+                        fontSize={18}
+                        fontWeight={400}
+                        alignment="start"
+                        css={{
+                          borderBottom: '1px solid ' + color.border_color,
+                        }}
+                        radius={0}
+                      />
+                    )}
+
+                    <MembersPreview {...member} />
+                    {index === filteredMembers.online.length - 1 && (
+                      <TextButton
+                        fontSize={18}
+                        fontWeight={400}
+                        text={'Offline - ' + filteredMembers.offline.length}
+                        alignment="space-between"
+                        css={{
+                          borderBottom: '1px solid ' + color.border_color,
+                        }}
+                        radius={0}
+                        Icon={
+                          <UpArrow
+                            css={{ rotate: showOffline ? '180deg' : '0deg' }}
+                          />
+                        }
+                        itemsDirection="row-reverse"
+                        backgroundColor={
+                          !showOffline ? color.secondary_color : undefined
+                        }
+                        onPress={() => setShowOffline(!showOffline)}
+                      />
+                    )}
                   </div>
-                )}
-                <div className="members-filter-container">
-                  <span>Online</span>
-                  <BorderSeparator direction="horizontal" />
-                  {onlineMembers?.map((member) => (
-                    <MembersPreview {...member} key={member.id.toString()} />
-                  ))}
-                </div>
-                <div className="members-filter-container">
-                  <span>Offline</span>
-                  <BorderSeparator direction="horizontal" />
-                  {offlineMembers?.map((member) => (
-                    <MembersPreview {...member} key={member.id.toString()} />
-                  ))}
-                </div>
-              </>
-            ) : (
-              <div>No Members</div>
-            )
+                );
+              }}
+            </ViewportList>
           ) : (
-            <span>error occurs when getting members </span>
-          )}
-        </div>
+            <div>No Members</div>
+          )
+        ) : (
+          <span>error occurs when getting members </span>
+        )}
       </div>
     </div>
   );
