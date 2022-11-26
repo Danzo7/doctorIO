e8e925f1b55113fde692eba28aa1916835aca5fd
Daouadji <daouadji506@gmail.com>
2022-11-26 23:42:32 +0100

    ✨ new TabComponent

---
+++ src/renderer/ui/components/tab_component/TabComponent.stories.tsx
@@ -0,0 +1,33 @@
+import { ComponentProps } from 'react';
+import type { Story } from '@storybook/react';
+import TabComponent from './TabComponent';
+import color from '@assets/styles/color';
+export default {
+  title: 'TabComponent',
+  component: TabComponent,
+};
+const Template: Story<ComponentProps<typeof TabComponent>> = (args) => (
+  <TabComponent {...args} />
+);
+export const FirstStory = Template;
+FirstStory.args = {
+  editable: true,
+  items: [
+    {
+      label: 'tab 1',
+      content: <div css={{ backgroundColor: color.background }}>tab1</div>,
+    },
+    {
+      label: 'tab 1',
+      content: <div css={{ backgroundColor: color.darker }}>tab2</div>,
+    },
+    {
+      label: 'tab 1',
+      content: <div css={{ backgroundColor: color.hot_purple }}>tab3</div>,
+    },
+    {
+      label: 'tab 1',
+      content: <div css={{ backgroundColor: color.hot_red }}>tab4</div>,
+    },
+  ],
+};
