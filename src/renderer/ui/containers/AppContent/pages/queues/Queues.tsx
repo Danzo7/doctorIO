66a2d63f0d004ed698aeca4c4085594a84b347f4
Danzo7 <aymenmiro@gmail.com>
2022-11-29 04:01:09 +0100

    🐛 Fix when role edited while the page is open

--- src/renderer/ui/containers/AppContent/pages/queues/Queues.tsx
+++ src/renderer/ui/containers/AppContent/pages/queues/Queues.tsx
@@ -1,12 +1,15 @@
 import AppointmentsQueue from '@components/appointments_queue';
 import BookedAppointmentPanel from '@components/booked_appointment_panel';
+import ErrorPanel from '@components/error_panel';
 import PaymentQueue from '@components/payment_queue';
 import SmallClinicStatus from '@components/small_clinic_status';
+import { useAbility } from '@stores/abilityStore';
 import './style/index.scss';
 
 interface QueuesProps {}
 export default function Queues({}: QueuesProps) {
-  return (
+  const ability = useAbility();
+  return ability.can('have', 'queue') || ability.can('manage', 'queue') ? (
     <div className="queues">
       <span>Appointment Queue</span>
       <div className="queues-header">
@@ -18,5 +21,7 @@ export default function Queues({}: QueuesProps) {
         <SmallClinicStatus />
       </div>
     </div>
+  ) : (
+    <ErrorPanel />
   );
 }
