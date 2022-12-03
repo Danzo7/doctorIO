324726e2698a4c7331bea19b3c0f66f76e6d3e8a
Daouadji <daouadji506@gmail.com>
2022-12-03 21:29:13 +0100

    ✨ move sentenceCase to a helper function

---
+++ src/renderer/utils/helpers/string.helper.ts
@@ -0,0 +1,10 @@
+export const sentenceCase = (val: string) =>
+  val
+    .replace(/([A-Z])/g, ' $1')
+    .split(' ')
+    .map((str, index) =>
+      index == 0
+        ? str.replace(str.charAt(0), str.charAt(0).toUpperCase())
+        : str.toLowerCase(),
+    )
+    .join(' ');
