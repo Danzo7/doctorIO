069afa79c283bffd192743bec3a6326767d2d4a6
Daouadji <daouadji506@gmail.com>
2022-12-07 20:36:01 +0100

    ➕ add backgroundColor prop

--- src/renderer/ui/components/vital_item/VitalItem.tsx
+++ src/renderer/ui/components/vital_item/VitalItem.tsx
@@ -1,12 +1,19 @@
+import { color } from '@assets/styles/color';
 import './style/index.scss';
 interface VitalItemProps {
   name: string;
   value: string;
   unit?: string;
+  backgroundColor?: string;
 }
-export default function VitalItem({ name, value, unit }: VitalItemProps) {
+export default function VitalItem({
+  name,
+  value,
+  unit,
+  backgroundColor = color.darkersec_color,
+}: VitalItemProps) {
   return (
-    <div className="vital-item">
+    <div className="vital-item" css={{ backgroundColor: backgroundColor }}>
       <span>{name}</span>
       <div className="values-div">
         <span>{value}</span>
