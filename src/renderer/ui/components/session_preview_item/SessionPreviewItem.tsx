bb61bf6de4579994d90a0ca23eefad74076d3778
Daouadji <daouadji506@gmail.com>
2022-12-07 20:20:04 +0100

    ✅ update SessionPreviewItem

--- src/renderer/ui/components/session_preview_item/SessionPreviewItem.tsx
+++ src/renderer/ui/components/session_preview_item/SessionPreviewItem.tsx
@@ -7,9 +7,14 @@ import './style/index.scss';
 interface SessionPreviewItemProps {
   bookedBy: string;
   bookedIn: Date;
+  bookedFor?: Date;
   treatedBy?: string;
   treatedIn?: Date;
   subject?: string;
+  state: {
+    phase: 'done' | 'missed' | 'upcoming' | 'opened' | 'canceled' | 'in queue';
+    isBooked: boolean;
+  };
 }
 export default function SessionPreviewItem({
   bookedBy,
@@ -17,94 +22,128 @@ export default function SessionPreviewItem({
   bookedIn,
   treatedIn,
   subject,
+  state,
+  bookedFor,
 }: SessionPreviewItemProps) {
   return (
     <div className="session-preview-item">
-      <TextPair
-        gap={2}
-        first={{
-          text: 'Booked by',
-          fontSize: 12,
-          fontWeight: 600,
-          fontColor: color.text_gray,
-        }}
-        second={{
-          text: bookedBy,
-          fontSize: 15,
-          fontWeight: 600,
-          fontColor: color.white,
-        }}
-      />
-      <BorderSeparator direction="vertical" />
-      {treatedBy && (
-        <TextPair
-          gap={2}
-          first={{
-            text: 'Treated by',
-            fontSize: 12,
-            fontWeight: 600,
-            fontColor: color.text_gray,
-          }}
-          second={{
-            text: treatedBy,
-            fontSize: 15,
-            fontWeight: 600,
-            fontColor: color.white,
-          }}
-        />
-      )}
+      {state.phase == 'upcoming' ||
+      state.phase == 'in queue' ||
+      state.phase == 'opened' ? (
+        <>
+          <TextPair
+            gap={2}
+            first={{
+              text: 'Created by',
+              fontSize: 12,
+              fontWeight: 600,
+              fontColor: color.text_gray,
+            }}
+            second={{
+              text: bookedBy,
+              fontSize: 15,
+              fontWeight: 600,
+              fontColor: color.white,
+            }}
+          />
+          <BorderSeparator direction="vertical" />
+          <TextPair
+            gap={2}
+            first={{
+              text: 'Created in',
+              fontSize: 12,
+              fontWeight: 600,
+              fontColor: color.text_gray,
+            }}
+            second={{
+              text: format(bookedIn, DATE_ONLY),
+              fontSize: 15,
+              fontWeight: 600,
+              fontColor: color.white,
+            }}
+          />
+          <BorderSeparator direction="vertical" />
+          {bookedFor && (
+            <>
+              <TextPair
+                gap={2}
+                first={{
+                  text: 'Booked for',
+                  fontSize: 12,
+                  fontWeight: 600,
+                  fontColor: color.text_gray,
+                }}
+                second={{
+                  text: format(bookedFor, DATE_ONLY),
+                  fontSize: 15,
+                  fontWeight: 600,
+                  fontColor: color.white,
+                }}
+              />
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          {treatedBy && (
+            <>
+              <TextPair
+                gap={2}
+                first={{
+                  text: state.phase == 'done' ? 'Treated by' : 'Canceled by',
+                  fontSize: 12,
+                  fontWeight: 600,
+                  fontColor: color.text_gray,
+                }}
+                second={{
+                  text: treatedBy,
+                  fontSize: 15,
+                  fontWeight: 600,
+                  fontColor: color.white,
+                }}
+              />
+              <BorderSeparator direction="vertical" />
+            </>
+          )}
 
-      <TextPair
-        gap={2}
-        first={{
-          text: 'Booked in',
-          fontSize: 12,
-          fontWeight: 600,
-          fontColor: color.text_gray,
-        }}
-        second={{
-          text: format(bookedIn, DATE_ONLY),
-          fontSize: 15,
-          fontWeight: 600,
-          fontColor: color.white,
-        }}
-      />
-
-      <BorderSeparator direction="vertical" />
-      {treatedIn && (
-        <TextPair
-          gap={2}
-          first={{
-            text: 'Treated in',
-            fontSize: 12,
-            fontWeight: 600,
-            fontColor: color.text_gray,
-          }}
-          second={{
-            text: format(treatedIn, DATE_ONLY),
-            fontSize: 15,
-            fontWeight: 600,
-            fontColor: color.white,
-          }}
-        />
+          {treatedIn && (
+            <TextPair
+              gap={2}
+              first={{
+                text: state.phase == 'done' ? 'Treated in' : 'Canceled in',
+                fontSize: 12,
+                fontWeight: 600,
+                fontColor: color.text_gray,
+              }}
+              second={{
+                text: format(treatedIn, DATE_ONLY),
+                fontSize: 15,
+                fontWeight: 600,
+                fontColor: color.white,
+              }}
+            />
+          )}
+        </>
       )}
-      <BorderSeparator direction="vertical" />
       {subject && (
-        <TextPair
-          gap={2}
-          first={{
-            text: 'Subject',
-            fontSize: 12,
-            fontWeight: 600,
-            fontColor: color.text_gray,
-          }}
-          second={{
-            text: subject,
-            fontSize: 15,
-            fontWeight: 600,
-            fontColor: color.white,
-          }}
-        />
+        <>
+          <BorderSeparator direction="vertical" />
+          <TextPair
+            gap={2}
+            first={{
+              text: 'Subject',
+              fontSize: 12,
+              fontWeight: 600,
+              fontColor: color.text_gray,
+            }}
+            second={{
+              text: subject,
+              fontSize: 15,
+              fontWeight: 600,
+              fontColor: color.white,
+            }}
+          />
+        </>
       )}
     </div>
   );
