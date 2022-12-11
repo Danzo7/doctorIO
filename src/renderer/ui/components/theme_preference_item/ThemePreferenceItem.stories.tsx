7fa7c3dde5e442b4d3a774790d5619db8502d691
Danzo7 <aymenmiro@gmail.com>
2022-12-11 01:03:23 +0100

    ⚡️ Use radio input in ThemePreferenceItem

--- src/renderer/ui/components/theme_preference_item/ThemePreferenceItem.stories.tsx
+++ src/renderer/ui/components/theme_preference_item/ThemePreferenceItem.stories.tsx
@@ -2,6 +2,8 @@ import { ComponentProps } from 'react';
 import type { Story } from '@storybook/react';
 import ThemePreferenceItem from './ThemePreferenceItem';
 import Theme01 from 'toSvg/theme01.svg?icon';
+import Radio from '@components/inputs/radio';
+import { color } from '@assets/styles/color';
 export default {
   title: 'ThemePreferenceItem',
   component: ThemePreferenceItem,
@@ -10,4 +12,8 @@ const Template: Story<ComponentProps<typeof ThemePreferenceItem>> = (args) => (
   <ThemePreferenceItem {...args} />
 );
 export const FirstStory = Template;
-FirstStory.args = { label: 'Nighty (default)', Preview: Theme01 };
+FirstStory.args = {
+  negaBackground: color.background,
+  input: <Radio label="Coming soon..." group="theme" disabled />,
+  Preview: Theme01,
+};
--- src/renderer/ui/components/theme_preference_item/ThemePreferenceItem.tsx
+++ src/renderer/ui/components/theme_preference_item/ThemePreferenceItem.tsx
@@ -1,37 +1,22 @@
 import { color } from '@assets/styles/color';
 import BorderSeparator from '@components/border_separator';
-import { FunctionComponent, SVGProps } from 'react';
+import { FunctionComponent, ReactNode, SVGProps } from 'react';
 import './style/index.scss';
 interface ThemePreferenceItemProps {
-  label?: string;
   Preview: FunctionComponent<SVGProps<SVGSVGElement>>;
-  checked: boolean;
-  onSelect: () => void;
+  input: ReactNode;
+  negaBackground: string;
 }
 export default function ThemePreferenceItem({
-  label,
   Preview,
-  checked,
-  onSelect,
+  negaBackground,
+  input,
 }: ThemePreferenceItemProps) {
   return (
-    <div
-      className="theme-preference-item"
-      onClick={onSelect}
-      css={{ backgroundColor: checked ? color.good_black : color.border_color }}
-    >
+    <div className="theme-preference-item" css={{ background: negaBackground }}>
       <Preview width={160} height={100} />
       <BorderSeparator direction="horizontal" color={color.silver_gray} />
-      <div className="rounded-checkbox">
-        <div className="rounded-div">
-          <div
-            css={{
-              backgroundColor: checked ? color.good_green : color.silver_gray,
-            }}
-          ></div>
-        </div>
-        <span>{label}</span>
-      </div>
+      {input}
     </div>
   );
 }
