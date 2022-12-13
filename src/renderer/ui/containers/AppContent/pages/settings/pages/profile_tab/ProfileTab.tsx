8ac7ab2db796e29fc13281c30dd251db9acf68cb
Daouadji <daouadji506@gmail.com>
2022-12-13 19:50:05 +0100

    ✨ update logo feature done

--- src/renderer/ui/containers/AppContent/pages/settings/pages/profile_tab/ProfileTab.tsx
+++ src/renderer/ui/containers/AppContent/pages/settings/pages/profile_tab/ProfileTab.tsx
@@ -1,30 +1,53 @@
 import { color } from '@assets/styles/color';
 import IconicButton from '@components/buttons/iconic_button';
 import LoadingSpinner from '@components/loading_spinner';
+import LogoChanger from '@components/logo_changer';
 import TextPair from '@components/text_pair/TextPair';
-import UserProfileStatus from '@components/user_profile_status';
 import { DATE_ONLY } from '@constants/data_format';
-import ProfilePictureModal from '@containers/modals/profile_picture_modal';
-import { FIT_MODAL } from '@libs/overlay';
-import { useGetMyMemberDetailQuery } from '@redux/clinic/rbac/member/memberApi';
-import { modal } from '@stores/overlayStore';
+import CropPictureModal from '@containers/modals/crop_picture_modal';
+import { DEFAULT_MODAL } from '@libs/overlay';
+import {
+  useGetMyMemberDetailQuery,
+  useSetAvatarMutation,
+} from '@redux/clinic/rbac/member/memberApi';
+import { modal, Overlay_u } from '@stores/overlayStore';
 import { format } from 'date-fns';
 import Edit from 'toSvg/pencil.svg?icon';
 import './style/index.scss';
 interface ProfileTabProps {}
 export default function ProfileTab({}: ProfileTabProps) {
   const { data, isSuccess, isLoading } = useGetMyMemberDetailQuery();
+  const [setAvatar] = useSetAvatarMutation();
   return isLoading ? (
     <LoadingSpinner />
   ) : isSuccess ? (
     <div className="profile-tab">
-      <UserProfileStatus
+      {/* <UserProfileStatus
         imgSrc={data.avatar}
         status={data.status}
         width={100}
         alt={data.name}
         onClick={() => {
           modal(<ProfilePictureModal registration={false} />, FIT_MODAL).open();
+        }}
+      /> */}
+      <LogoChanger
+        width={100}
+        src={data.avatar}
+        alt={data.name}
+        onChange={(newSrc) => {
+          modal(
+            <CropPictureModal
+              src={newSrc}
+              onSave={(img) => {
+                setAvatar({ data: img }).then(() => {
+                  Overlay_u.close('avatarCropper');
+                });
+              }}
+            />,
+            DEFAULT_MODAL,
+            'avatarCropper',
+          ).open();
         }}
       />
       <div className="fullName-id-container">
