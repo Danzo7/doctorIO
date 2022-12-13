3f296ffe2a268ae3df0c18bdabee91cf2678b1b8
Danzo7 <aymenmiro@gmail.com>
2022-12-13 16:17:38 +0100

    🎨props

--- src/renderer/ui/components/inputs/slider/Slider.tsx
+++ src/renderer/ui/components/inputs/slider/Slider.tsx
@@ -1,5 +1,26 @@
 import './style/index.scss';
-interface SliderProps {}
-export default function Slider({}: SliderProps) {
-  return <div className="slider"></div>;
+interface SliderProps {
+  onChange?: (value: number) => void;
+  max?: number;
+  min?: number;
+  step?: number;
+  value?: number;
+}
+export default function Slider({
+  max,
+  min,
+  onChange,
+  step,
+  value,
+}: SliderProps) {
+  return (
+    <input
+      type="range"
+      min={min}
+      max={max}
+      step={step}
+      value={value}
+      onChange={(e) => onChange?.(Number(e.target.value))}
+    />
+  );
 }
