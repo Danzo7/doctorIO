f1cc1bb96559008c908742dc5897166b230adbd0
Danzo7 <aymenmiro@gmail.com>
2022-12-13 16:18:06 +0100

    ✨ cropper

---
+++ src/renderer/ui/components/cropper/Cropper.stories.tsx
@@ -0,0 +1,12 @@
+// import { ComponentProps } from 'react';
+// import type { Story } from '@storybook/react';
+// import Cropper from './Cropper';
+// export default {
+//   title: 'Cropper',
+//   component: Cropper,
+// };
+// const Template: Story<ComponentProps<typeof Cropper>> = (args) => (
+//   <Cropper {...args} />
+// );
+// export const FirstStory = Template
+// FirstStory.args = {};
