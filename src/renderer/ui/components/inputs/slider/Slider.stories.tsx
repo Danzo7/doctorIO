7ffe8a6d4f357bdc35017bd1ecc0d6ccd22ec998
Danzo7 <aymenmiro@gmail.com>
2022-12-13 16:06:13 +0100

    📝 Empty comp

---
+++ src/renderer/ui/components/inputs/slider/Slider.stories.tsx
@@ -0,0 +1,12 @@
+import { ComponentProps } from 'react';
+import type { Story } from '@storybook/react';
+import Slider from './Slider';
+export default {
+  title: 'Slider',
+  component: Slider,
+};
+const Template: Story<ComponentProps<typeof Slider>> = (args) => (
+  <Slider {...args} />
+);
+export const FirstStory = Template
+FirstStory.args = {};
---
+++ src/renderer/ui/components/inputs/slider/Slider.tsx
@@ -0,0 +1,5 @@
+import './style/index.scss';
+interface SliderProps {}
+export default function Slider({}: SliderProps) {
+  return <div className="slider"></div>;
+}
