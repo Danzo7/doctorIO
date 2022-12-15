af26908491e8fa4dc660806dec595d460c2132d5
Daouadji <daouadji506@gmail.com>
2022-12-15 17:04:45 +0100

    ✅ update ChangeSecretModal

--- src/renderer/ui/containers/modals/change_secret_modal/ChangeSecretModal.tsx
+++ src/renderer/ui/containers/modals/change_secret_modal/ChangeSecretModal.tsx
@@ -3,6 +3,9 @@ import TextButton from '@components/buttons/text_button';
 import Input, { Inputix } from '@components/inputs/input/Input';
 import ModalContainer from '@components/modal_container';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useUpdateMemberSecretMutation } from '@redux/clinic/rbac/member/memberApi';
+import { Overlay_u } from '@stores/overlayStore';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import './style/index.scss';
@@ -24,57 +27,83 @@ export default function ChangeSecretModal({}: ChangeSecretModalProps) {
       repeatSecretKey: '',
     },
   });
-
-  const onSubmit: SubmitHandler<Inputs> = (formData) => {
-    console.log(
-      '🚀 ~ file: ChangeSecretModal.tsx:77 ~ ChangeSecretModal ~ formData',
-      formData,
-    );
-    //TODO handle submit
+  const [UpdateMemberSecretMutation] = useUpdateMemberSecretMutation();
+  const [internalError, setError] = useState('');
+  const onSubmit: SubmitHandler<Inputs> = ({
+    oldSecretKey,
+    newSecretKey,
+    repeatSecretKey,
+  }) => {
+    if (newSecretKey == repeatSecretKey)
+      UpdateMemberSecretMutation({
+        oldSecret: oldSecretKey,
+        newSecret: newSecretKey,
+      }).then((result: any) => {
+        if (result.data) {
+          Overlay_u.close();
+        } else {
+          const castedErr = (result.error as any)?.data as ServerError;
+          if (castedErr?.errorCode == 1200) {
+            setError(castedErr?.message);
+          }
+        }
+      });
+    else {
+      setError("Secret Key confirmation doesn't match the password");
+    }
   };
   return (
-    <ModalContainer
-      title="Updating secret key"
-      onSubmit={handleSubmit(onSubmit)}
-      controls={
-        <div className="update-controls-div">
-          <TextButton
-            text="Update"
-            backgroundColor={color.good_green}
-            fontSize={14}
-            fontWeight={700}
-            width={'100%'}
-            blank
-            type="submit"
-          />
-          <span>
-            Note: updating the secret key will only affect the current clinic.
-          </span>
+    <div className="update-controls-div">
+      <ModalContainer
+        title="Updating secret key"
+        onSubmit={handleSubmit(onSubmit)}
+        controls={
+          <div className="update-controls-div">
+            <TextButton
+              text="Update"
+              backgroundColor={color.good_green}
+              fontSize={14}
+              fontWeight={700}
+              width={'100%'}
+              blank
+              type="submit"
+            />
+            <span>
+              Note: updating the secret key will only affect the current clinic.
+            </span>
+          </div>
+        }
+      >
+        <div className="update-input-div">
+          <Inputix control={control}>
+            <Input
+              type="text"
+              label="Old secret key"
+              name="oldSecretKey"
+              fillContainer
+            />
+            <Input
+              type="text"
+              label="New secret key"
+              name="newSecretKey"
+              fillContainer
+              onChange={() => {
+                if (internalError.length > 0) setError('');
+              }}
+            />
+            <Input
+              type="text"
+              label="Repeat secret key"
+              name="repeatSecretKey"
+              fillContainer
+              onChange={() => {
+                if (internalError.length > 0) setError('');
+              }}
+            />
+          </Inputix>
+          <span className="error-span">{internalError}</span>
         </div>
-      }
-    >
-      <div className="update-input-div">
-        <Inputix control={control}>
-          <Input
-            type="text"
-            label="Old secret key"
-            name="oldSecretKey"
-            fillContainer
-          />
-          <Input
-            type="text"
-            label="New secret key"
-            name="newSecretKey"
-            fillContainer
-          />
-          <Input
-            type="text"
-            label="Repeat secret key"
-            name="repeatSecretKey"
-            fillContainer
-          />
-        </Inputix>
-      </div>
-    </ModalContainer>
+      </ModalContainer>
+    </div>
   );
 }
