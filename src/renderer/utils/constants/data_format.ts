40d30ed07573484e62451d068d150b06f5e5ba52
Danzo7 <aymenmiro@gmail.com>
2022-12-16 01:34:38 +0100

    🔥 Remove constant formats

--- src/renderer/utils/constants/data_format.ts
+++ src/renderer/utils/constants/data_format.ts
@@ -1,14 +1,2 @@
-const DATE_ONLY = 'dd MMM yyyy'; //Variants: 'dd MMM yyyy',
-const DATE_DAY = 'EEEE, dd MMM';
-const TIME_ONLY = 'h:mm aa';
-const DATE_TIME = 'dd MMM yyyy, h:mm aa';
 const START_OF_TIME = '2020-1-1';
-const TIME_ONLY_24H = 'HH:mm';
-export {
-  DATE_ONLY,
-  DATE_DAY,
-  DATE_TIME,
-  TIME_ONLY,
-  START_OF_TIME,
-  TIME_ONLY_24H,
-};
+export { START_OF_TIME };
