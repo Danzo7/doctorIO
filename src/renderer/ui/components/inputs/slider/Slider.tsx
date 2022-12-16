d445544a5c0a8319fad2564b43fcd3a5efee3785
Danzo7 <aymenmiro@gmail.com>
2022-12-16 04:32:56 +0100

    💄 Improve styling

--- src/renderer/ui/components/inputs/slider/Slider.tsx
+++ src/renderer/ui/components/inputs/slider/Slider.tsx
@@ -1,5 +1,6 @@
 import './style/index.scss';
 import ImageIcon from 'toSvg/image.svg?icon';
+import color from '@assets/styles/color';
 interface SliderProps {
   onChange?: (value: number) => void;
   max?: number;
@@ -14,6 +15,7 @@ export default function Slider({
   step,
   value,
 }: SliderProps) {
+  const val = (((value ?? 1) - (min ?? 1)) / ((max ?? 1) - (min ?? 1))) * 100;
   return (
     <div className="slider-div">
       <ImageIcon width={15} height={15} />
@@ -24,6 +26,11 @@ export default function Slider({
         step={step}
         value={value}
         onChange={(e) => onChange?.(Number(e.target.value))}
+        css={{
+          '&::-webkit-slider-runnable-track': {
+            background: `linear-gradient(to right, ${color.hot_purple} 0%, ${color.hot_purple} ${val}%, ${color.text_gray} ${val}%,  ${color.text_gray} 100%)`,
+          },
+        }}
       />
       <ImageIcon width={20} height={20} />
     </div>
