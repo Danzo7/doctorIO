bc321e57611411866def92e5b81a5481ca6cd427
Daouadji <daouadji506@gmail.com>
2022-12-17 18:02:47 +0100

    ✨ add VerticalPanel on BookingTimeline (need icon)

--- src/renderer/ui/components/booking_timeline/BookingTimeline.tsx
+++ src/renderer/ui/components/booking_timeline/BookingTimeline.tsx
@@ -4,6 +4,7 @@ import PreviewList from '@components/preview_list';
 import { AppointmentBrief } from '@models/instance.model';
 import './style/index.scss';
 import TimelineItem from './timeline_item';
+import VerticalPanel from '@components/vertical_panel';
 
 interface BookingTimelineProps {
   patientId: number;
@@ -20,7 +21,10 @@ export default function BookingTimeline({
     <div className="booking-timeline">
       <div
         css={{
-          borderLeft: `5px solid ${color.cold_blue}`,
+          borderLeft:
+            appointments.length > 0
+              ? `5px solid ${color.cold_blue}`
+              : undefined,
           height: '100%',
           position: 'absolute',
           left: 2.5,
@@ -37,9 +41,13 @@ export default function BookingTimeline({
         gap={10}
         noBorder
       >
-        {appointments.map((app, index) => (
-          <TimelineItem key={index} {...app} patientId={patientId} /> //TODO show vertical panel when empty
-        ))}
+        {appointments.length > 0 ? (
+          appointments.map((app, index) => (
+            <TimelineItem key={index} {...app} patientId={patientId} />
+          ))
+        ) : (
+          <VerticalPanel title="No appointments" backgroundColor="none" /> //TODO add the correct icon to the vertical panel
+        )}
       </PreviewList>
     </div>
   );
