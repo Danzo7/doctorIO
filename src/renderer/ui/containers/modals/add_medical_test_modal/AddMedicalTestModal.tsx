5d610618228d27f765fbfaa75ebf31640f186372
Danzo7 <aymenmiro@gmail.com>
2022-12-23 02:43:50 +0100

    🚧 use VitalsStore (Need styling)

--- src/renderer/ui/containers/modals/add_medical_test_modal/AddMedicalTestModal.tsx
+++ src/renderer/ui/containers/modals/add_medical_test_modal/AddMedicalTestModal.tsx
@@ -1,18 +1,19 @@
-import colors from '@assets/styles/color';
+import colors, { color } from '@assets/styles/color';
 import TextButton from '@components/buttons/text_button';
 import Input from '@components/inputs/input';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import ModalContainer from '@components/modal_container';
 import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { Inputix } from '@components/inputs/input/Input';
 import { numericString } from '@helpers/zod.helper';
 import { BiometricScreening } from '@models/instance.model';
 import { titleCase } from '@helpers/string.helper';
-import { useAppSettingsStore } from '@stores/appSettingsStore';
 import { VitalField } from '@models/local.models';
 import IconicButton from '@components/buttons/iconic_button';
 import FieldIcon from 'toSvg/fields.svg?icon';
+import VerticalPanel from '@components/vertical_panel';
+import BorderSeparator from '@components/border_separator';
+import { useVitalFieldsStore } from '@stores/vitalFieldsStore';
 
 interface AddMedicalTestModalProps {
   onSubmit: SubmitHandler<BiometricScreening>;
@@ -38,20 +39,30 @@ function fieldsToSchema(fields: VitalField[]) {
     },
   );
 }
+function fieldsToSchemas(fields: VitalField[]) {
+  return fields.reduce(
+    (acc, field) => ({
+      ...acc,
+      [field.name]: field.display,
+    }),
+    {},
+  );
+}
 
 export default function AddMedicalTestModal({
   onSubmit,
 }: AddMedicalTestModalProps) {
-  const { schema, defaults, fields } = fieldsToSchema(
-    useAppSettingsStore.getState().vitalFields,
-  );
+  const { switchField, getActiveFields, vitalFields } = useVitalFieldsStore();
+  const { schema, defaults, fields } = fieldsToSchema(getActiveFields());
+  const dd = fieldsToSchemas(vitalFields);
   const {
     control,
     handleSubmit,
     formState: { touchedFields, dirtyFields },
-  } = useForm<z.infer<typeof schema>>({
-    resolver: zodResolver(z.object(schema)),
-    defaultValues: defaults,
+  } = useForm<z.infer<typeof schema>>();
+  //TODO: Move sidebars and hook to a separate component
+  const { control: con } = useForm({
+    defaultValues: dd,
   });
   return (
     <ModalContainer
@@ -87,19 +98,57 @@ export default function AddMedicalTestModal({
         </>
       }
     >
-      <Inputix control={control}>
-        {fields.map(
-          (field) =>
-            field.display && (
-              <Input
-                key={field.name}
-                type={{ type: 'numeric', unit: field.unit }}
-                label={titleCase(field.name)}
-                name={field.name}
-              />
-            ),
-        )}
-      </Inputix>
+      <div
+        className="content"
+        css={{ display: 'flex', flexDirection: 'row', flexGrow: 1, gap: 5 }}
+      >
+        {
+          //TODO move to comp ; Fix styling with scss
+          <div className="field-selection" css={{ flexGrow: 1 }}>
+            <Inputix control={con}>
+              {Object.entries(dd).map(([key]) => (
+                <Input
+                  type={'checkbox'}
+                  name={key}
+                  label={key}
+                  key={key}
+                  onChange={() => {
+                    switchField(key);
+                  }}
+                />
+              ))}
+            </Inputix>
+          </div>
+        }
+        <BorderSeparator direction="vertical" color={color.silver_gray} />
+        <div
+          className="fields"
+          css={{ flexGrow: 5, display: 'flex', flexWrap: 'wrap', gap: 5 }} //TODO: Fix styling with scss
+        >
+          {fields.length > 0 ? (
+            <Inputix control={control}>
+              {fields.map(
+                (field) =>
+                  field.display && (
+                    <Input
+                      key={field.name}
+                      type={{ type: 'numeric', unit: field.unit }}
+                      label={titleCase(field.name)}
+                      name={field.name}
+                      defaultValue={defaults[field.name].toString()}
+                    />
+                  ),
+              )}
+            </Inputix>
+          ) : (
+            <VerticalPanel
+              title="Please add a field first"
+              backgroundColor="none"
+              padding={0}
+            />
+          )}
+        </div>
+      </div>
     </ModalContainer>
   );
 }
