1dd16054b56794a1e0c6df21c84948b78111c8a4
Daouadji <daouadji506@gmail.com>
2022-12-24 22:01:45 +0100

    ✨ use useDeleteFieldMutation

--- src/renderer/ui/components/vital_field_item/VitalFieldItem.tsx
+++ src/renderer/ui/components/vital_field_item/VitalFieldItem.tsx
@@ -2,22 +2,24 @@ import './style/index.scss';
 import EyePassword from '@components/buttons/eye_password';
 import TextPair from '@components/text_pair/TextPair';
 import { color } from '@assets/styles/color';
-import IconicButton from '@components/buttons/iconic_button';
-import Edit from 'toSvg/pencil.svg?icon';
+import TrashCan from 'toSvg/trash_can.svg?icon';
+import SquareIconButton from '@components/buttons/square_icon_button/SquareIconButton';
+import { useDeleteFieldMutation } from '@redux/clinic/clinicApi';
+import { titleCase } from '@helpers/string.helper';
 interface VitalFieldItemProps {
   onChangeDisplay: () => void;
   display?: boolean;
   name: string;
   unit: string;
-  onEdit?: () => void;
 }
 export default function VitalFieldItem({
   name,
   unit,
-  onEdit,
+
   onChangeDisplay,
   display = false,
 }: VitalFieldItemProps) {
+  const [deleteField, res] = useDeleteFieldMutation();
   return (
     <div className="vital-field-item">
       <div className="vitals-info-div">
@@ -31,7 +33,7 @@ export default function VitalFieldItem({
             fontColor: color.text_gray,
           }}
           second={{
-            text: name,
+            text: titleCase(name),
             fontSize: 15,
             fontWeight: 500,
             fontColor: color.white,
@@ -56,14 +58,14 @@ export default function VitalFieldItem({
       </div>
       <div className="vitals-controls">
         <EyePassword onPress={onChangeDisplay} value={display} />
-        <IconicButton
-          tip="Edit"
-          onPress={onEdit}
-          blank
-          width={25}
-          radius={7}
-          afterBgColor={color.cold_blue}
-          Icon={<Edit width={12} height={12} />}
+        <SquareIconButton
+          disabled={res.isLoading}
+          Icon={TrashCan}
+          iconSize={15}
+          tip="Delete field"
+          onPress={() => {
+            deleteField(name);
+          }}
         />
       </div>
     </div>
