fb8982472c9f7e73d7e11903762bde975abddb2e
Daouadji <daouadji506@gmail.com>
2023-01-03 23:27:10 +0100

    ✅ update SettingOption

--- src/renderer/ui/components/setting_option/SettingOption.tsx
+++ src/renderer/ui/components/setting_option/SettingOption.tsx
@@ -2,11 +2,27 @@ import ToggleButton from '@components/buttons/toggle_button';
 import Header from '@components/header';
 import { ReactNode } from 'react';
 import './style/index.scss';
+import { color } from '@assets/styles/color';
 interface SettingOptionProps {
-  title: string;
-  description: string;
+  title:
+    | string
+    | {
+        text: string;
+        fontSize?: number;
+        fontColor?: string;
+        fontWeight?: string | number;
+      };
+  description:
+    | string
+    | {
+        text: string;
+        fontSize?: number;
+        fontColor?: string;
+        fontWeight?: string | number;
+      };
   gap?: number;
   controls?: ReactNode;
+  flexDirection?: 'column' | 'row';
   useToggleButton?: {
     isChecked?: boolean;
     onChange?: (isChecked: boolean) => void;
@@ -17,6 +33,7 @@ export default function SettingOption({
   description,
   controls,
   useToggleButton,
+  flexDirection = 'column',
   gap = 10,
 }: SettingOptionProps) {
   return (
@@ -24,6 +41,7 @@ export default function SettingOption({
       <Header
         title={title}
         padding={0}
+        flexGrow={1}
         buttonNode={
           useToggleButton && (
             <ToggleButton
@@ -33,8 +51,33 @@ export default function SettingOption({
           )
         }
       />
-      <span className="description-span">{description}</span>
-      {controls}
+      <div
+        className="setting-option-content"
+        css={{
+          flexDirection: flexDirection,
+          alignItems: flexDirection == 'column' ? 'flex-start' : 'center',
+          gap: flexDirection == 'column' ? 5 : undefined,
+          justifyContent: flexDirection == 'row' ? 'space-between' : undefined,
+        }}
+      >
+        {description && (
+          <span
+            css={{
+              fontSize:
+                typeof description != 'string' ? description.fontSize : 13,
+              color:
+                typeof description != 'string'
+                  ? description.fontColor
+                  : color.silver_gray,
+              fontWeight:
+                typeof description != 'string' ? description.fontWeight : 600,
+            }}
+          >
+            {typeof description == 'string' ? description : description.text}
+          </span>
+        )}
+        {controls}
+      </div>
     </div>
   );
 }
