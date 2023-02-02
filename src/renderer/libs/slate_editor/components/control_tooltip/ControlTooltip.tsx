30054604129b3eaa88f6463b7d53092baaac6d59
danzo7 <aymenmiro@gmail.com>
2023-02-02 02:41:58 +0100

    🐛 add direction to temporary fix  tooltips overriding

--- src/renderer/libs/slate_editor/components/control_tooltip/ControlTooltip.tsx
+++ src/renderer/libs/slate_editor/components/control_tooltip/ControlTooltip.tsx
@@ -2,10 +2,22 @@ import { ReactNode } from 'react';
 import './style/index.scss';
 interface ControlTooltipProps {
   children: ReactNode | ReactNode[];
+  direction?: 'top' | 'bottom';
 }
-export default function ControlTooltip({ children }: ControlTooltipProps) {
+export default function ControlTooltip({
+  children,
+  direction,
+}: ControlTooltipProps) {
   return (
-    <span className="control-tooltip" contentEditable={false}>
+    <span
+      className="control-tooltip"
+      contentEditable={false}
+      css={
+        direction == 'top'
+          ? { bottom: 'calc(100% + 5px)' }
+          : { top: 'calc(100% + 5px)' }
+      }
+    >
       {children}
     </span>
   );
