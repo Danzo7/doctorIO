a66d19ee47228143d7836fb59d4b386878f2ba2b
danzo7 <aymenmiro@gmail.com>
2023-02-25 00:31:43 +0100

    ✨ Add swap position option for snakebar

--- src/renderer/ui/containers/modals/snake_bar/SnakeBar.tsx
+++ src/renderer/ui/containers/modals/snake_bar/SnakeBar.tsx
@@ -1,6 +1,10 @@
 import { color } from '@assets/styles/color';
 import { ReactNode } from 'react';
 import './style/index.scss';
+import Arrow from 'toSvg/arrow.svg?icon';
+import IconicButton from '@components/buttons/iconic_button';
+import { SETTINGS, useAppSettingsStore } from '@stores/appSettingsStore';
+
 interface SnakeBarProps {
   description: string;
   type?: 'error' | 'info' | 'warning';
@@ -13,11 +17,36 @@ export default function SnakeBar({
   type = 'error',
   backgroundColor = color.lighter_background,
 }: SnakeBarProps) {
+  const promptPosition = SETTINGS.promptPosition;
   return (
     <div
       className={`snake-bar ${type}`}
       css={{ backgroundColor: backgroundColor }}
+      key={+new Date()} // To force animation replay when rerender  (https://stackoverflow.com/questions/52231320/how-to-replay-a-css3-animation-in-reactjs)
     >
+      <div
+        className="position-control"
+        css={promptPosition == 'bottom' ? { top: 0 } : { bottom: 0 }}
+      >
+        <IconicButton
+          Icon={
+            <Arrow
+              css={{
+                rotate: promptPosition == 'bottom' ? '180deg' : undefined,
+              }}
+            />
+          }
+          radius={5}
+          width={20}
+          iconSize={10}
+          afterBgColor={color.light}
+          onPress={() => {
+            useAppSettingsStore
+              .getState()
+              .setPromptPosition(promptPosition == 'top' ? 'bottom' : 'top');
+          }}
+        />
+      </div>
       <span>{description}</span>
       {children}
     </div>
