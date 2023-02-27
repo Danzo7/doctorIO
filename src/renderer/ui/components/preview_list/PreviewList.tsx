4f381df5ee76431dd06a06588a18c815766b8ed6
danzo7 <aymenmiro@gmail.com>
2023-02-27 02:13:09 +0100

    ⚡️ handle isLoading and isFetching

--- src/renderer/ui/components/preview_list/PreviewList.tsx
+++ src/renderer/ui/components/preview_list/PreviewList.tsx
@@ -2,7 +2,8 @@ import { color } from '@assets/styles/color';
 import Header from '@components/header';
 import { ReactNode, useRef } from 'react';
 import './style/index.scss';
-import ViewportList from 'react-viewport-list';
+import { ViewportList } from 'react-viewport-list';
+import Shimmer from '@components/shimmers/shimmer';
 interface PreviewListProps {
   title: string;
   buttonNode?: ReactNode;
@@ -13,6 +14,9 @@ interface PreviewListProps {
   overflow?: 'hidden' | 'visible' | 'auto' | 'scroll';
   flexGrow?: true;
   width?: number | string;
+  isLoading?: boolean;
+  isFetching?: boolean;
+  shimmer?: ReactNode;
 }
 export default function PreviewList({
   title,
@@ -24,6 +28,9 @@ export default function PreviewList({
   overflow = 'auto',
   width,
   flexGrow,
+  isFetching,
+  isLoading,
+  shimmer,
 }: PreviewListProps) {
   const containerRef = useRef(null);
   const listRef = useRef(null);
@@ -39,17 +46,50 @@ export default function PreviewList({
         maxHeight: maxHeight ? maxHeight : undefined,
         flexGrow: flexGrow ? 1 : 0,
         width: width ? width : undefined,
+        pointerEvents: isFetching || isLoading ? 'none' : undefined,
+        cursor: isFetching || isLoading ? 'not-allowed' : undefined,
+        '>.header':
+          isFetching || isLoading
+            ? {
+                button: isFetching
+                  ? { opacity: 0.3 }
+                  : {
+                      background: `linear-gradient(90deg,${color.silver_gray}, ${color.light},${color.border_color})`,
+                      backgroundSize: '400% 400%',
+                      animation: 'shimmer 2s ease infinite',
+                      '*': {
+                        visibility: 'hidden',
+                      },
+                    },
+              }
+            : {},
       }}
     >
       <Header title={title} buttonNode={buttonNode} />
       <div
         className="preview-list-wrapper"
-        css={{ gap: gap }}
+        css={{
+          gap: gap,
+          ...(isFetching && !isLoading
+            ? { filter: 'blur(2px) brightness(0.8)' }
+            : {}),
+        }}
         ref={containerRef}
       >
-        <ViewportList ref={listRef} viewportRef={containerRef} items={items}>
-          {(item) => item}
-        </ViewportList>
+        {isLoading ? (
+          shimmer ?? (
+            <Shimmer
+              height={30}
+              borderRadius={5}
+              length={3}
+              direction="column"
+            />
+          )
+        ) : (
+          <ViewportList ref={listRef} viewportRef={containerRef} items={items}>
+            {(item) => item}
+          </ViewportList>
+        )}
       </div>
     </div>
   );
