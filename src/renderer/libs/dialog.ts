e15dee117f003bffe3ae79b69406bd6ba35c6aa0
danzo7 <aymenmiro@gmail.com>
2023-02-28 03:09:57 +0100

    ✨ Dialog lib

---
+++ src/renderer/libs/dialog.ts
@@ -0,0 +1,35 @@
+import modal from './overlay/stores/modal';
+import { FIT_MODAL } from './overlay';
+import ConfirmModal from '@containers/modals/dialog_modal/components/ConfirmModal';
+export type ConfirmOptions = {
+  description?: string;
+  onCancel?: () => void;
+  onConfirm?: () => void;
+  status?: 'warning' | 'error' | 'success';
+};
+export const Confirm = (
+  title: string,
+  { description, onCancel, onConfirm }: ConfirmOptions,
+) => {
+  return new Promise<boolean>((resolve) => {
+    modal(
+      ({ close }) =>
+        ConfirmModal({
+          title,
+          status: 'warning',
+          description,
+          onConfirm: () => {
+            onConfirm?.();
+            resolve(true);
+            close();
+          },
+          onCancel: () => {
+            onCancel?.();
+            resolve(false);
+            close();
+          },
+        }),
+      { ...FIT_MODAL, onClose: () => resolve(false) },
+    ).open();
+  });
+};
