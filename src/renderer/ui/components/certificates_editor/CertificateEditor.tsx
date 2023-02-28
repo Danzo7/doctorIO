a7e77c9d945a61415a3f2869f4cd4922bb1ae8fd
danzo7 <aymenmiro@gmail.com>
2023-02-28 17:44:12 +0100

    ✨ add reusable certificate template editor

--- src/renderer/ui/components/certificates_editor/CertificateEditor.tsx
+++ src/renderer/ui/components/certificates_editor/CertificateEditor.tsx
@@ -15,6 +15,8 @@ import { format } from 'date-fns';
 import { SETTINGS } from '@stores/appSettingsStore';
 import { createEditor } from '@libs/slate_editor/createEditor';
 import { CommonEditor } from '@libs/slate_editor/commons/CommonEditor';
+import { withDynamicAttributes } from '@libs/slate_editor/slate-dynamic-attributes/withDynamicAttributes';
+import EditorToolbar from '@libs/slate_editor/components/editor_toolbar';
 interface CertificateEditorProps {
   onChange?: (value: Descendant[]) => void;
   mentions?: string[];
@@ -33,12 +35,11 @@ export default function CertificateEditor({
   const editor = useMemo(
     () =>
       withSuggestion(createEditor(), {
-        suggestions: ['at', 'me', ...mentions],
+        suggestions: ['at', ...mentions],
         keyword: '@',
         insertOnSelect(selected, prev) {
           const mappedText: { [key: string]: string } = {
             at: format(new Date(), SETTINGS.dateFormat),
-            me: 'Dr. John Doe',
           };
           return {
             type: 'autofill',
@@ -78,6 +79,69 @@ export default function CertificateEditor({
           onChanged?.(value);
         }}
       >
+        <div className="editable-container">
+          <Editable
+            className="editor"
+            placeholder="Write something..."
+            renderLeaf={renderLeaf}
+            renderElement={renderElement}
+            autoFocus
+            readOnly={readonly}
+          />
+        </div>
+      </Slate>
+      {error && (
+        <div className="error-message">
+          <span>
+            {error ??
+              (defaultValue && !CommonEditor.isValidDesendants(defaultValue)
+                ? 'Invalid value'
+                : '')}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
+export function CertificateModelsEditor({
+  onChange: onChanged,
+  error,
+  defaultValue,
+  readonly,
+}: CertificateEditorProps) {
+  const editor = useMemo(
+    () => withDynamicAttributes(createEditor()),
+
+    [],
+  );
+  const renderLeaf = useCallback(
+    (props: RenderLeafProps) => <EditorLeaf {...props} />,
+    [],
+  );
+  const renderElement = useCallback(
+    (props: RenderElementProps) => <EditorElement {...props} />,
+    [],
+  );
+
+  return (
+    <div className={`certificate-editor ${error ? 'error' : ''}`}>
+      <Slate
+        editor={editor}
+        value={
+          defaultValue && CommonEditor.isValidDesendants(defaultValue)
+            ? defaultValue
+            : [
+                {
+                  type: 'p',
+                  children: [{ text: '', color: color.coldBlack }],
+                },
+              ]
+        }
+        onChange={(value) => {
+          onChanged?.(value);
+        }}
+      >
+        <EditorToolbar items={['size', 'format', 'align', 'dynamic']} />
         <div className="editable-container">
           <Editable
             className="editor"
