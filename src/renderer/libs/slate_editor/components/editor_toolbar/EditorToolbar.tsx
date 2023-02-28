00c98a8d3bbc6b53cde492a6f6814e59092e2fa3
danzo7 <aymenmiro@gmail.com>
2023-02-28 17:45:09 +0100

    🐛 Fix modals closing each others

--- src/renderer/libs/slate_editor/components/editor_toolbar/EditorToolbar.tsx
+++ src/renderer/libs/slate_editor/components/editor_toolbar/EditorToolbar.tsx
@@ -37,168 +37,211 @@ import { ImageEditor } from '@libs/slate_editor/slate-image/ImageEditor';
 import { createImage } from '@helpers/image.helper';
 import { DEFAULT_AUTO_REF } from '@libs/slate_editor/constants';
 
-interface EditorToolbarProps {}
+interface EditorToolbarProps {
+  items?: ('insert' | 'format' | 'align' | 'dynamic' | 'size')[];
+}
 
-export default function EditorToolbar({}: EditorToolbarProps) {
+export default function EditorToolbar({
+  items = ['size', 'format', 'align', 'insert', 'dynamic'],
+}: EditorToolbarProps) {
   const editor = useSlate();
   return (
     <div className="editor-toolbar">
-      <div className="block">
-        <SquareIconButton
-          iconSize={15}
-          onMouseDown={(event) => {
-            event?.preventDefault();
-            setFontSize(editor, 'increase');
-          }}
-          unFocusable
-          iconColor={color.white}
-          Icon={Increase}
-        />
-        <InputWrapper height={'auto'} radius={0} padding={2}>
-          <AutoSizeInput
-            type="number"
-            value={getFontSize(editor)}
-            onChange={(e) => {
-              if (
-                isNaN(Number(e.target.value)) ||
-                Number(e.target.value) > 128 ||
-                Number(e.target.value) < 8
-              )
-                return e;
-              else Editor.addMark(editor, 'fontSize', Number(e.target.value));
-            }}
-          />
-        </InputWrapper>
-        <SquareIconButton
-          iconSize={10}
-          onMouseDown={(event) => {
-            event?.preventDefault();
-            setFontSize(editor, 'decrease');
-          }}
-          unFocusable
-          iconColor={color.white}
-          Icon={Decrease}
-        />
-      </div>
-      <div className="block">
-        <SquareIconButton
-          onMouseDown={(event) => {
-            event?.preventDefault();
-            toggleMark(editor, 'bold');
-          }}
-          unFocusable
-          iconColor={isMarkActive(editor, 'bold') ? color.white : color.light}
-          Icon={BoldIcon}
-        />
-        <SquareIconButton
-          onMouseDown={(event) => {
-            event?.preventDefault();
-            toggleMark(editor, 'italic');
-          }}
-          unFocusable
-          iconColor={isMarkActive(editor, 'italic') ? color.white : color.light}
-          Icon={ItalicIcon}
-        />
-        <SquareIconButton
-          onMouseDown={(event) => {
-            event?.preventDefault();
-            toggleMark(editor, 'underline');
-          }}
-          unFocusable
-          iconColor={
-            isMarkActive(editor, 'underline') ? color.white : color.light
-          }
-          Icon={UnderlineIcon}
-        />
-      </div>
-      <div className="block">
-        <SquareIconButton
-          onMouseDown={(event) => {
-            event?.preventDefault();
-            toggleAlign(editor, 'left');
-          }}
-          unFocusable
-          iconColor={isAlignActive(editor, 'left') ? color.white : color.light}
-          Icon={AlignLeft}
-        />
-        <SquareIconButton
-          onMouseDown={(event) => {
-            event?.preventDefault();
-            toggleAlign(editor, 'center');
-          }}
-          unFocusable
-          iconColor={
-            isAlignActive(editor, 'center') ? color.white : color.light
-          }
-          Icon={AlignCenter}
-        />
-        <SquareIconButton
-          onMouseDown={(event) => {
-            event?.preventDefault();
-            toggleAlign(editor, 'right');
-          }}
-          unFocusable
-          iconColor={isAlignActive(editor, 'right') ? color.white : color.light}
-          Icon={AlignRight}
-        />
-      </div>
-      <div className="block">
-        <SquareIconButton
-          tip="Insert Table"
-          onMouseDown={(event) => {
-            event?.preventDefault();
-            TablesEditor.insertTable(editor);
-          }}
-          unFocusable
-          iconColor={color.silver_gray}
-          Icon={TableIcon}
-        />
-        <SquareIconButton
-          tip="Insert Image"
-          iconColor={color.silver_gray}
-          onPress={() => {
-            modal(
-              ({ close }) => (
-                <ImageGallery
-                  onSelect={async (image) => {
-                    const img = await createImage(image);
-                    const width = 250;
-                    const height = Math.floor((250 * img.height) / img.width);
-                    ImageEditor.insertImage(editor, image, { width, height });
+      {items.map((item) => {
+        switch (item) {
+          case 'size':
+            return (
+              <div className="block">
+                <SquareIconButton
+                  iconSize={15}
+                  onMouseDown={(event) => {
+                    event?.preventDefault();
+                    setFontSize(editor, 'increase');
+                  }}
+                  unFocusable
+                  iconColor={color.white}
+                  Icon={Increase}
+                />
+                <InputWrapper height={'auto'} radius={0} padding={2}>
+                  <AutoSizeInput
+                    type="number"
+                    value={getFontSize(editor)}
+                    onChange={(e) => {
+                      if (
+                        isNaN(Number(e.target.value)) ||
+                        Number(e.target.value) > 128 ||
+                        Number(e.target.value) < 8
+                      )
+                        return e;
+                      else
+                        Editor.addMark(
+                          editor,
+                          'fontSize',
+                          Number(e.target.value),
+                        );
+                    }}
+                  />
+                </InputWrapper>
+                <SquareIconButton
+                  iconSize={10}
+                  onMouseDown={(event) => {
+                    event?.preventDefault();
+                    setFontSize(editor, 'decrease');
+                  }}
+                  unFocusable
+                  iconColor={color.white}
+                  Icon={Decrease}
+                />
+              </div>
+            );
+          case 'format':
+            return (
+              <div className="block">
+                <SquareIconButton
+                  onMouseDown={(event) => {
+                    event?.preventDefault();
+                    toggleMark(editor, 'bold');
+                  }}
+                  unFocusable
+                  iconColor={
+                    isMarkActive(editor, 'bold') ? color.white : color.light
+                  }
+                  Icon={BoldIcon}
+                />
+                <SquareIconButton
+                  onMouseDown={(event) => {
+                    event?.preventDefault();
+                    toggleMark(editor, 'italic');
+                  }}
+                  unFocusable
+                  iconColor={
+                    isMarkActive(editor, 'italic') ? color.white : color.light
+                  }
+                  Icon={ItalicIcon}
+                />
+                <SquareIconButton
+                  onMouseDown={(event) => {
+                    event?.preventDefault();
+                    toggleMark(editor, 'underline');
+                  }}
+                  unFocusable
+                  iconColor={
+                    isMarkActive(editor, 'underline')
+                      ? color.white
+                      : color.light
+                  }
+                  Icon={UnderlineIcon}
+                />
+              </div>
+            );
+          case 'align':
+            return (
+              <div className="block">
+                <SquareIconButton
+                  onMouseDown={(event) => {
+                    event?.preventDefault();
+                    toggleAlign(editor, 'left');
+                  }}
+                  unFocusable
+                  iconColor={
+                    isAlignActive(editor, 'left') ? color.white : color.light
+                  }
+                  Icon={AlignLeft}
+                />
+                <SquareIconButton
+                  onMouseDown={(event) => {
+                    event?.preventDefault();
+                    toggleAlign(editor, 'center');
+                  }}
+                  unFocusable
+                  iconColor={
+                    isAlignActive(editor, 'center') ? color.white : color.light
+                  }
+                  Icon={AlignCenter}
+                />
+                <SquareIconButton
+                  onMouseDown={(event) => {
+                    event?.preventDefault();
+                    toggleAlign(editor, 'right');
+                  }}
+                  unFocusable
+                  iconColor={
+                    isAlignActive(editor, 'right') ? color.white : color.light
+                  }
+                  Icon={AlignRight}
+                />
+              </div>
+            );
+          case 'insert':
+            return (
+              <div className="block">
+                <SquareIconButton
+                  tip="Insert Table"
+                  onMouseDown={(event) => {
+                    event?.preventDefault();
+                    TablesEditor.insertTable(editor);
+                  }}
+                  unFocusable
+                  iconColor={color.silver_gray}
+                  Icon={TableIcon}
+                />
+                <SquareIconButton
+                  tip="Insert Image"
+                  iconColor={color.silver_gray}
+                  onPress={() => {
+                    modal(
+                      ({ close }) => (
+                        <ImageGallery
+                          onSelect={async (image) => {
+                            const img = await createImage(image);
+                            const width = 250;
+                            const height = Math.floor(
+                              (250 * img.height) / img.width,
+                            );
+                            ImageEditor.insertImage(editor, image, {
+                              width,
+                              height,
+                            });
 
-                    close();
+                            close();
+                          }}
+                        />
+                      ),
+                      { ...DEFAULT_MODAL, width: '90%', height: '90%' },
+                    ).open();
+                  }}
+                  unFocusable
+                  Icon={AddImage}
+                />
+              </div>
+            );
+          case 'dynamic':
+            return (
+              <div className="end-block">
+                <TextButton
+                  text="Auto"
+                  Icon={TagIcon}
+                  afterBgColor={color.silver_gray}
+                  onPress={() => {
+                    modal(
+                      ({ close }) => (
+                        <InsertAttributesModal
+                          close={close}
+                          elements={DEFAULT_AUTO_REF}
+                          onChange={(value) => {
+                            AttributeEditor.insertAttribute(editor, value);
+                          }}
+                        />
+                      ),
+                      DEFAULT_MODAL,
+                      'Insert Attributes',
+                    ).open({ previousBehavior: 'keep' });
                   }}
                 />
-              ),
-              { ...DEFAULT_MODAL, width: '90%', height: '90%' },
-            ).open();
-          }}
-          unFocusable
-          Icon={AddImage}
-        />
-      </div>
-      <div className="end-block">
-        <TextButton
-          text="Auto"
-          Icon={TagIcon}
-          afterBgColor={color.silver_gray}
-          onPress={() => {
-            modal(
-              ({ close }) => (
-                <InsertAttributesModal
-                  close={close}
-                  elements={DEFAULT_AUTO_REF}
-                  onChange={(value) => {
-                    AttributeEditor.insertAttribute(editor, value);
-                  }}
-                />
-              ),
-              DEFAULT_MODAL,
-              'Insert Attributes',
-            ).open();
-          }}
-        />
-      </div>
+              </div>
+            );
+        }
+      })}
     </div>
   );
 }
