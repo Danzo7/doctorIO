0b6ea23a5ae74dacd4f316281b3929221044fa98
Daouadji <daouadji506@gmail.com>
2023-03-07 14:45:44 +0100

    ➕ add props to LoadingSpinner

--- src/renderer/ui/components/loading_spinner/LoadingSpinner.tsx
+++ src/renderer/ui/components/loading_spinner/LoadingSpinner.tsx
@@ -1,10 +1,16 @@
 import './style/index.scss';
 import Loading from 'toSvg/loading_logo.svg?icon';
 
-interface LoadingSpinnerProps {}
-export default function LoadingSpinner({}: LoadingSpinnerProps) {
+interface LoadingSpinnerProps {
+  width?: number | string;
+  height?: number | string;
+}
+export default function LoadingSpinner({
+  width,
+  height = 50,
+}: LoadingSpinnerProps) {
   return (
-    <div className="loading-container">
+    <div className="loading-container" css={{ width: width, height: height }}>
       <div className="loading-spinner">
         <Loading width={50} height={50} />
       </div>
