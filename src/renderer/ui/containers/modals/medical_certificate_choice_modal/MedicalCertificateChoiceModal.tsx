42fd6a8fc8e350ad7fc3ca8a95d6878d06d27d4a
Daouadji <daouadji506@gmail.com>
2023-03-10 21:22:55 +0100

    ✅ update the supplied data in the props

--- src/renderer/ui/containers/modals/medical_certificate_choice_modal/MedicalCertificateChoiceModal.tsx
+++ src/renderer/ui/containers/modals/medical_certificate_choice_modal/MedicalCertificateChoiceModal.tsx
@@ -2,14 +2,19 @@ import ModalContainer from '@components/modal_container';
 import './style/index.scss';
 import TextButton from '@components/buttons/text_button';
 import { color } from '@assets/styles/color';
-import CertificateEditorModal from '../certificate_editor_modal';
 import { DEFAULT_MODAL, modal } from '@libs/overlay';
+import { useGetCertificateTemplatesQuery } from '@redux/clinic/templates/templatesApi';
+import RefetchPanel from '@components/refetch_panel';
+import CertificateEditorModal from '../certificate_editor_modal';
 interface MedicalCertificateChoiceModalProps {}
 export default function MedicalCertificateChoiceModal({}: MedicalCertificateChoiceModalProps) {
+  const { isSuccess, isLoading, data, refetch } =
+    useGetCertificateTemplatesQuery();
   return (
     <div className="medical-certificate-choice-modal">
       <ModalContainer
         title="Choose medical certificate type"
+        isLoading={isLoading}
         controlsPosition="end"
         controls={
           <TextButton
@@ -25,24 +30,40 @@ export default function MedicalCertificateChoiceModal({}: MedicalCertificateChoi
         }
       >
         <div className="medical-certificate-choice-options">
-          <TextButton
-            text="Create certificate from template"
-            backgroundColor={color.cold_blue}
-            fontSize={14}
-            fontWeight={700}
-            onPress={() => {
-              //TODO open certificates dropdown
-            }}
-          />
-          <TextButton
-            text="Create new one"
-            backgroundColor={color.cold_blue}
-            fontSize={14}
-            fontWeight={700}
-            onPress={() => {
-              modal(<CertificateEditorModal />, DEFAULT_MODAL).open();
-            }}
-          />
+          {isSuccess ? (
+            data.length > 0 ? (
+              data.map(({ id, title }, index) => (
+                <TextButton
+                  key={index}
+                  text={title}
+                  backgroundColor={color.cold_blue}
+                  fontSize={14}
+                  fontWeight={700}
+                  onPress={() => {
+                    modal(
+                      <CertificateEditorModal
+                        defaultValue={{ id: id.toString(), title: title }}
+                      />,
+                      { ...DEFAULT_MODAL, height: '90%', width: '90%' },
+                      'certificateModal',
+                    ).open();
+                  }}
+                />
+              ))
+            ) : (
+              <TextButton
+                text="Create new one"
+                backgroundColor={color.cold_blue}
+                fontSize={14}
+                fontWeight={700}
+                onPress={() => {
+                  modal(<CertificateEditorModal />, DEFAULT_MODAL).open();
+                }}
+              />
+            )
+          ) : (
+            <RefetchPanel action={refetch} />
+          )}
         </div>
       </ModalContainer>
     </div>
--- src/renderer/ui/layers/medical_session/pages/cetificates_tab/CertificatesTab.tsx
+++ src/renderer/ui/layers/medical_session/pages/cetificates_tab/CertificatesTab.tsx
@@ -9,6 +9,7 @@ import KeywordFieldItem from '@components/keyword_field_item';
 import VerticalPanel from '@components/vertical_panel';
 import { DEFAULT_MODAL, modal, tooltip } from '@libs/overlay';
 import CertificateEditorModal from '@containers/modals/certificate_editor_modal';
+import MedicalCertificateChoiceModal from '@containers/modals/medical_certificate_choice_modal';
 
 interface CertificatesTabProps {}
 export default function CertificatesTab({}: CertificatesTabProps) {
@@ -44,14 +45,12 @@ export default function CertificatesTab({}: CertificatesTabProps) {
                       text: 'Use a template',
                       onPress: () => {
                         modal(
-                          () => <CertificateEditorModal />,
+                          () => <MedicalCertificateChoiceModal />,
                           {
                             closeOnClickOutside: true,
                             closeOnBlur: false,
                             isDimmed: true,
                             clickThrough: false,
-
-                            width: '50%',
                           },
                           'certificateModal',
                         ).open();
@@ -74,7 +73,7 @@ export default function CertificatesTab({}: CertificatesTabProps) {
               key={index}
               onEdit={() => {
                 modal(
-                  <CertificateEditorModal defaultValue={certificate} />,
+                  <CertificateEditorModal updateValue={certificate} />,
                   { ...DEFAULT_MODAL, height: '90%', width: '90%' },
                   'certificateModal',
                 ).open();
@@ -111,7 +110,7 @@ export default function CertificatesTab({}: CertificatesTabProps) {
                       text: 'Use a template',
                       onPress: () => {
                         modal(
-                          () => <CertificateEditorModal />,
+                          () => <MedicalCertificateChoiceModal />,
                           {
                             closeOnClickOutside: true,
                             closeOnBlur: false,
