2dee07813d440c45c9aca0c32887f5baff812da4
Daouadji <daouadji506@gmail.com>
2023-03-18 00:51:20 +0100

    ✅ update Statistics page

--- src/renderer/ui/containers/AppContent/pages/statistics/Statistics.tsx
+++ src/renderer/ui/containers/AppContent/pages/statistics/Statistics.tsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './style/index.scss';
+import { NavTabMenu } from '@components/tab_menu';
+import GeneralStatisticTab from './pages/general_statistic_tab';
 interface StatisticsProps {}
 export default function Statistics({}: StatisticsProps) {
-  return <div className="statistics"></div>;
+  return (
+    <div className="statistics">
+      <span>Growth & Earning</span>
+      <NavTabMenu
+        items={[
+          { name: 'General', route: { to: '', include: 'GeneralStatistics' } },
+        ]}
+      />
+      <Routes>
+        <Route path="" element={<GeneralStatisticTab />} />
+        <Route path="GeneralStatistics" element={<GeneralStatisticTab />} />
+      </Routes>
+    </div>
+  );
 }
