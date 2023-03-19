50f75cc70914118bf3d7d0d13729bbdfe9545495
Daouadji <daouadji506@gmail.com>
2023-03-19 16:17:15 +0100

    ✅ update MemberStatisticItem comp

--- src/renderer/ui/components/member_statistic_item/MemberStatisticItem.tsx
+++ src/renderer/ui/components/member_statistic_item/MemberStatisticItem.tsx
@@ -2,12 +2,17 @@ import NotAButton from '@components/not_a_button';
 import './style/index.scss';
 import { color } from '@assets/styles/color';
 import UserProfileStatus from '@components/user_profile_status';
+import King from 'toSvg/king.svg?icon';
 interface MemberStatisticItemProps {
   score: number;
   name: string;
   avatar: string;
   status: boolean;
   rank: number;
+  numOfAppointments: number;
+  finishedSessions: number;
+  numOfPat: number;
+  revenue: number;
 }
 export default function MemberStatisticItem({
   name,
@@ -15,16 +20,21 @@ export default function MemberStatisticItem({
   status,
   score,
   rank,
+  numOfAppointments,
+  finishedSessions,
+  numOfPat,
+  revenue,
 }: MemberStatisticItemProps) {
-  //TODO add rank 1 logo instead of number
   return (
     <div className="member-statistic-item">
       <div className="left-container">
         <NotAButton
-          text={rank.toString()}
+          text={rank != 1 ? rank.toString() : undefined}
           backgroundColor={color.warm_orange}
-          width={25}
-          height={25}
+          width={30}
+          height={30}
+          padding={0}
+          Icon={rank == 1 ? <King width={15} height={15} /> : undefined}
         />
         <div className="member-Info">
           <UserProfileStatus
@@ -40,10 +50,10 @@ export default function MemberStatisticItem({
         </div>
       </div>
       <div className="right-container">
-        <span>31000</span>
-        <span>20</span>
-        <span>20</span>
-        <span>0</span>
+        <span>{numOfAppointments}</span>
+        <span>{finishedSessions}</span>
+        <span>{numOfPat}</span>
+        <span>{revenue} </span>
       </div>
     </div>
   );
